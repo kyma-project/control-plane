86a71f9b8710b87b65d4aed8a5b18ffa6c32c8c2
Karol Furgoł <furgolkarol@gmail.com>
2020-06-29 13:03:11 +0200

    fix keb

--- components/kyma-environment-broker/internal/provisioner/client.go
+++ components/kyma-environment-broker/internal/provisioner/client.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"reflect"
 
-	"github.com/kyma-incubator/compass/components/kyma-environment-broker/internal/httputil"
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/httputil"
 
 	schema "github.com/kyma-incubator/compass/components/provisioner/pkg/gqlschema"
 	gcli "github.com/machinebox/graphql"
