e4136fa5f1c8230cb36d72544b5c3e84b9a081ac
Tomasz Gorgol <t.gorgol@sap.com>
2020-07-08 11:56:56 +0200

    Change hardcoded occurrences of compass-system namespace

--- tests/e2e/provisioning/internal/director/config.go
+++ tests/e2e/provisioning/internal/director/config.go
@@ -2,6 +2,6 @@ package director
 
 type Config struct {
 	URL                        string `envconfig:"default=http://compass-director.compass-system.svc.cluster.local:3000/graphql"`
-	Namespace                  string `envconfig:"default=compass-system"`
-	OauthCredentialsSecretName string `envconfig:"default=compass-broker-registration"`
+	Namespace                  string `envconfig:"default=kcp-system"`
+	OauthCredentialsSecretName string `envconfig:"default=kcp-broker-registration"`
 }
