36abfbaf9ea66a1bb795ee77cf4c4b80847603c3
Benjamin Somhegyi <benjamin.somhegyi@sap.com>
2020-07-08 10:00:01 +0200

    Data model minor changes

--- components/kyma-environment-broker/internal/orchestration/core/model.go
+++ components/kyma-environment-broker/internal/orchestration/core/model.go
@@ -9,35 +9,36 @@ import (
 
 // Runtime is the data type which captures the needed SKR specific attributes to perform reconciliations on a given runtime.
 type Runtime struct {
+	// Valid values: InstanceID, RuntimeID, GlobalAccountID, SubAccountID
 	internal.Instance
 
-	ShootName              string
+	// The corresponding shoot cluster's .metadata.name value
+	ShootName string
+	// The corresponding shoot cluster's .spec.maintenance.timeWindow.Begin value, in "HHMMSS+[TZ HHMM]" format, e.g. "040000+0000"
 	MaintenanceWindowBegin string
-	MaintenanceWindowEnd   string
+	// The corresponding shoot cluster's .spec.maintenance.timeWindow.End value, in "HHMMSS+[TZ HHMM]" format, e.g. "040000+0000"
+	MaintenanceWindowEnd string
 }
 
+// TargetAll all SKRs provisioned successfully and not deprovisioning
 const TargetAll = "all"
 
 // RuntimeTarget captures a specification of SKR targets to resolve for an orchestration.
 // When a RuntimeTarget defines multiple fields, all should match to any given runtime to be selected (i.e. the terms are AND-ed).
-// Options:
-//   - Target: all                                      // All SKRs provisioned successfully and not deprovisioning
-//   - GlobalAccount: CA50125541TID000000000741207136   // regex pattern to match against, could also be CA.*
-//   - SubAccount: 0d20e315-d0b4-48a2-9512-49bc8eb03cd1 // regex pattern to match against
-//   - Region: "europe|eu-"                             // regex pattern to match against
 type RuntimeTarget struct {
-	Target        string
+	// Valid values: "all"
+	Target string
+	// Regex pattern to match against the runtime's GlobalAccount field. E.g. CA50125541TID000000000741207136, CA.*
 	GlobalAccount string
-	SubAccount    string
-	Region        string
+	// Regex pattern to match against the runtime's SubAccount field. E.g. 0d20e315-d0b4-48a2-9512-49bc8eb03cd1
+	SubAccount string
+	// Regex pattern to match against the shoot cluster's Region field (not SCP platform-region). E.g. "europe|eu-"
+	Region string
 }
 
 // RuntimeResolver interface includes the Resolve() method, which, given an input slice of target specs to include and exclude,
 // returns back a list of unique Runtime objects. The Runtime object contains attributes necessary for the executors and operations
-// to perform reconcile on the given SKR. Such attributes include:
-//   - Identifiers, like instance ID, global account, subaccount, (gardener shoot name)
-//   - Shoot maintenance window
-//
+// to perform reconcile on the given SKR.
 type RuntimeResolver interface {
 	Resolve(include []RuntimeTarget, exclude []RuntimeTarget) ([]Runtime, error)
 }
@@ -45,12 +46,10 @@ type RuntimeResolver interface {
 // RuntimeReconciler encapsulates a Runtime, and the logic to perform checking the actual vs. desired state
 // of the specific operation use-case (e.g. Kyma release, shoot config) on the given runtime,
 // as well as the logic to reconcile the SKR according to the specific use-case.
-// The interface includes:
-//   - Reconcile() gets the actual state, compares with desired state, and performs the operation on the specific SKR
-//     when the actual state needs to be converged with the desired state.
-//   - Runtime() returns the assigned runtime for this RuntimeReconciler
 type RuntimeReconciler interface {
+	// Reconcile gets the actual state, compares with desired state, and performs the operation on the specific SKR when the actual state needs to be converged with the desired state.
 	Reconcile() error
+	// Runtime returns the assigned runtime for this RuntimeReconciler
 	Runtime() Runtime
 }
 
@@ -59,15 +58,15 @@ type RuntimeReconciler interface {
 // while the ManagedOrchestration holds the desired state as well as methods for interacting with the specific custom resource object.
 type NewRuntimeReconciler = func(r Runtime, o ManagedOrchestration) RuntimeReconciler
 
-// ManagedOrchestration The various custom resources for orchestrating RuntimeReconcilers on SKRs implement this interface. It exposes the following:
-//   - RuntimeTargetSpec() gets the runtime targets spec from the custom resource object
-//   - OrchestrationSpec() gets the orchestration config spec from the custom resource object
-//   - ReconcilerStatus() gets the current reconciler specific block of the custom resource status:
-//   - UpdateReconcilerStatus is called by the OrchestrationReconciler implementation to update the reconciliation status inside the custom resource's status: attribute
+// ManagedOrchestration The various custom resources for orchestrating RuntimeReconcilers on SKRs implement this interface.
 type ManagedOrchestration interface {
+	// RuntimeTargetSpec gets the runtime targets spec from the custom resource object
 	RuntimeTargetSpec() []RuntimeTarget
+	// OrchestrationSpec gets the orchestration config spec from the custom resource object
 	OrchestrationSpec() OrchestrationSpec
+	// ReconcilerStatus gets the current reconciler specific block of the custom resource status:
 	ReconcilerStatus() *ReconcilerStatus
+	// UpdateReconcilerStatus is called by the OrchestrationReconciler implementation to update the reconciliation status inside the custom resource's status: attribute
 	UpdateReconcilerStatus() error
 }
 
@@ -98,11 +97,17 @@ type ParallelOrchestrationSpec struct {
 	Workers int
 }
 
-// OrchestrationSpec is the spec part common for all ManagedOrchestration Custom Resources
+// OrchestrationSpec is the .spec.orchestration part common for all ManagedOrchestration Custom Resources
 type OrchestrationSpec struct {
 	Strategy OrchestrationStrategyType
 	Schedule OrchestrationScheduleType
 	Parallel ParallelOrchestrationSpec
+}
+
+// TargetSpec is the .spec.targets part common for all ManagedOrchestration Custom Resources
+type TargetSpec struct {
+	Include []RuntimeTarget
+	Exclude []RuntimeTarget
 }
 
 // ReconcilerRuntimeStatistics
@@ -141,7 +146,7 @@ type OrchestrationCondition struct {
 	LastUpdateTime time.Time
 }
 
-// OrchestrationStatus is the status part common for all ManagedOrchestration Custom Resources
+// OrchestrationStatus is the .status part common for all ManagedOrchestration Custom Resources
 type OrchestrationStatus struct {
 	ObservedGeneration int
 	Conditions         []OrchestrationCondition
