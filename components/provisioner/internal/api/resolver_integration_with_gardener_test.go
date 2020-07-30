33ff00cebad5496ab6fe73174180dd371479878c
Rafal Potempa <rafal.potem@gmail.com>
2020-07-30 12:21:58 +0200

    Adjust test

--- components/provisioner/internal/api/resolver_integration_with_gardener_test.go
+++ components/provisioner/internal/api/resolver_integration_with_gardener_test.go
@@ -237,7 +237,9 @@ func TestProvisioning_ProvisionRuntimeWithDatabase(t *testing.T) {
 
 			// then
 			assert.Equal(t, runtimeID, shoot.Annotations["kcp.provisioner.kyma-project.io/runtime-id"])
+			assert.Equal(t, runtimeID, shoot.Annotations["compass.provisioner.kyma-project.io/runtime-id"])
 			assert.Equal(t, *provisionRuntime.ID, shoot.Annotations["kcp.provisioner.kyma-project.io/operation-id"])
+			assert.Equal(t, *provisionRuntime.ID, shoot.Annotations["compass.provisioner.kyma-project.io/operation-id"])
 			assert.Equal(t, auditLogTenant, shoot.Annotations["custom.shoot.sapcloud.io/subaccountId"])
 			assert.Equal(t, subAccountId, shoot.Labels[model.SubAccountLabel])
 
@@ -249,6 +251,7 @@ func TestProvisioning_ProvisionRuntimeWithDatabase(t *testing.T) {
 			shoot, err = shootInterface.Get(shoot.Name, metav1.GetOptions{})
 			require.NoError(t, err)
 			assert.Equal(t, runtimeID, shoot.Annotations["kcp.provisioner.kyma-project.io/runtime-id"])
+			assert.Equal(t, runtimeID, shoot.Annotations["compass.provisioner.kyma-project.io/runtime-id"])
 
 			// when checking Runtime Status
 			runtimeStatusProvisioned, err := resolver.RuntimeStatus(ctx, *provisionRuntime.RuntimeID)
@@ -336,6 +339,7 @@ func TestProvisioning_ProvisionRuntimeWithDatabase(t *testing.T) {
 			// then
 			require.NoError(t, err)
 			assert.Equal(t, runtimeID, shoot.Annotations["kcp.provisioner.kyma-project.io/runtime-id"])
+			assert.Equal(t, runtimeID, shoot.Annotations["compass.provisioner.kyma-project.io/runtime-id"])
 
 			//when Deprovisioning
 			shoot = removeFinalizers(t, shootInterface, shoot)
@@ -364,7 +368,8 @@ func TestProvisioning_ProvisionRuntimeWithDatabase(t *testing.T) {
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "shoot-with-unknown-id",
 				Annotations: map[string]string{
-					"kcp.provisioner.kyma-project.io/runtime-id": "fbed9b28-473c-4b3e-88a3-803d94d38785",
+					"kcp.provisioner.kyma-project.io/runtime-id":     "fbed9b28-473c-4b3e-88a3-803d94d38785",
+					"compass.provisioner.kyma-project.io/runtime-id": "fbed9b28-473c-4b3e-88a3-803d94d38785",
 				},
 			},
 			Spec: gardener_types.ShootSpec{},
