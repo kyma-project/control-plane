c6302c753a06fcf0714cceee724c3afec8772906
Benjamin Somhegyi <benjamin.somhegyi@sap.com>
2020-10-05 15:15:37 +0200

    Changes after review

--- components/kubeconfig-service/pkg/client/client_test.go
+++ components/kubeconfig-service/pkg/client/client_test.go
@@ -17,6 +17,17 @@ type FakeTokenSource string
 
 var fixToken FakeTokenSource = "fake-token-1234"
 
+const (
+	testTenant     = "test-tenant-id-0001"
+	testRuntime    = "test-runtime-id-0001"
+	testKubeConfig = `---
+yaml-like: true
+multi-line: |-
+  lorem impsum
+  donor
+special-chars-to-escape: "abcdefgh<0123456789>ijkl*:_mnopqrst"`
+)
+
 func (t FakeTokenSource) Token() (*oauth2.Token, error) {
 	return &oauth2.Token{
 		AccessToken: string(t),
@@ -25,12 +36,7 @@ func (t FakeTokenSource) Token() (*oauth2.Token, error) {
 }
 func TestClient_GetKubeConfig(t *testing.T) {
 	// given
-	testTenant := "test-tenant-id-0001"
-	testRuntime := "test-runtime-id-0001"
-	testKubeConfig := `---
-yaml-like: true
-multi-line: "also true"
-special-chars-to-escape: "abcdefgh<0123456789>ijkl*:_mnopqrst"`
+
 	called := false
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, http.MethodGet, r.Method)
