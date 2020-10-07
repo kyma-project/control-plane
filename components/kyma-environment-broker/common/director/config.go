e572fd21cf73f43dfb35e175daf75d883bd1bad7
Kamil Sputo <sputo.kamil@gmail.com>
2020-10-07 09:42:38 +0200

    gofmt files

--- components/kyma-environment-broker/common/director/config.go
+++ components/kyma-environment-broker/common/director/config.go
@@ -1,10 +1,10 @@
 package director
 
 type Config struct {
-	URL                  string `envconfig:"default=http://compass-director.compass-system.svc.cluster.local:3000/graphql"`
-	Namespace            string `envconfig:"default=kcp-system"`
-	OauthTokenURL        string `envconfig:"default=https://oauth.domain.com/oauth/token"`
-	OauthClientID        string `envcondif:"default=directorId"`
-	OauthClientSecret    string `envconfig:"default=directorSecret"`
-	OauthScope           string `envconfig:"default=runtime:read runtime:write"`
+	URL               string `envconfig:"default=http://compass-director.compass-system.svc.cluster.local:3000/graphql"`
+	Namespace         string `envconfig:"default=kcp-system"`
+	OauthTokenURL     string `envconfig:"default=https://oauth.domain.com/oauth/token"`
+	OauthClientID     string `envcondif:"default=directorId"`
+	OauthClientSecret string `envconfig:"default=directorSecret"`
+	OauthScope        string `envconfig:"default=runtime:read runtime:write"`
 }
