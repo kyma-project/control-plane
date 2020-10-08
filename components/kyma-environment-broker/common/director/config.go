75c4a1412ceaada71eeb11601522509650c4baf4
Kamil Sputo <sputo.kamil@gmail.com>
2020-10-08 16:14:41 +0200

    Fix typo in director config

--- components/kyma-environment-broker/common/director/config.go
+++ components/kyma-environment-broker/common/director/config.go
@@ -3,7 +3,7 @@ package director
 type Config struct {
 	URL               string `envconfig:"default=http://compass-director.compass-system.svc.cluster.local:3000/graphql"`
 	OauthTokenURL     string `envconfig:"default=https://oauth.domain.com/oauth/token"`
-	OauthClientID     string `envcondif:"default=directorId"`
+	OauthClientID     string `envconfig:"default=directorId"`
 	OauthClientSecret string `envconfig:"default=directorSecret"`
 	OauthScope        string `envconfig:"default=runtime:read runtime:write"`
 }
