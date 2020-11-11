4a6a614a3cad747eb407f8e51d69eb55f9adc3d8
Benjamin Somhegyi <benjamin.somhegyi@sap.com>
2020-11-11 06:40:26 +0100

    Orchestration fixes (#320)
    * Orchestration fixes
    
    * bump KEB image
    
    * Dummy change to trigger PR

--- components/kyma-environment-broker/internal/orchestration/strategy_parallel_test.go
+++ components/kyma-environment-broker/internal/orchestration/strategy_parallel_test.go
@@ -1,6 +1,7 @@
 package orchestration
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -8,19 +9,54 @@ import (
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/util/rand"
 )
 
-func TestNewParallelOrchestrationStrategy(t *testing.T) {
+type testExecutor struct {
+	mux      sync.Mutex
+	opCalled map[string]bool
+}
 
-	s := NewParallelOrchestrationStrategy(&testExecutor{}, logrus.New())
+func (t *testExecutor) Execute(opID string) (time.Duration, error) {
+	t.mux.Lock()
+	called := t.opCalled[opID]
+	t.opCalled[opID] = true
+	t.mux.Unlock()
 
-	startTime, err := time.Parse(maintenanceWindowFormat, "220000+0000")
-	require.NoError(t, err)
+	if !called {
+		time.Sleep(1 * time.Second)
+		return 1 * time.Second, nil
+	} else {
+		return 0, nil
+	}
+}
 
-	n := time.Now()
-	start := time.Date(n.Year(), n.Month(), n.Day(), startTime.Hour(), startTime.Minute(), startTime.Second(), startTime.Nanosecond(), &time.Location{})
+func TestNewParallelOrchestrationStrategy_Immediate(t *testing.T) {
+	// given
+	executor := &testExecutor{opCalled: map[string]bool{}}
+	s := NewParallelOrchestrationStrategy(executor, logrus.New())
+
+	ops := make([]internal.RuntimeOperation, 3)
+	for i := range ops {
+		ops[i] = internal.RuntimeOperation{
+			Operation: internal.Operation{ID: rand.String(5)},
+		}
+	}
+
+	// when
+	id, err := s.Execute(ops, orchestration.StrategySpec{Schedule: orchestration.Immediate, Parallel: orchestration.ParallelStrategySpec{Workers: 2}})
+
+	// then
+	assert.NoError(t, err)
+	s.Wait(id)
+}
+
+func TestNewParallelOrchestrationStrategy_MaintenanceWindow(t *testing.T) {
+	// given
+	executor := &testExecutor{opCalled: map[string]bool{}}
+	s := NewParallelOrchestrationStrategy(executor, logrus.New())
+
+	start := time.Now().Add(5 * time.Second)
 
 	ops := make([]internal.RuntimeOperation, 3)
 	for i := range ops {
@@ -30,15 +66,10 @@ func TestNewParallelOrchestrationStrategy(t *testing.T) {
 		}
 	}
 
-	_, err = s.Execute(ops, orchestration.StrategySpec{Schedule: orchestration.Immediate})
+	// when
+	id, err := s.Execute(ops, orchestration.StrategySpec{Schedule: orchestration.MaintenanceWindow, Parallel: orchestration.ParallelStrategySpec{Workers: 2}})
+
+	// then
 	assert.NoError(t, err)
-
-	_, err = s.Execute(ops, orchestration.StrategySpec{Schedule: orchestration.MaintenanceWindow})
-	assert.NoError(t, err)
-}
-
-type testExecutor struct{}
-
-func (t *testExecutor) Execute(opID string) (time.Duration, error) {
-	return 0, nil
+	s.Wait(id)
 }
