a16fb0ed066474fadb379d948e0276967b0ac440
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2020-11-12 12:55:35 +0100

    remove log messgaes

--- components/provisioner/internal/operations/stages/shootupgrade/wait_for_shoot_new_version.go
+++ components/provisioner/internal/operations/stages/shootupgrade/wait_for_shoot_new_version.go
@@ -37,21 +37,15 @@ func (s *WaitForShootNewVersionStep) TimeLimit() time.Duration {
 func (s *WaitForShootNewVersionStep) Run(cluster model.Cluster, operation model.Operation, logger logrus.FieldLogger) (operations.StageResult, error) {
 
 	gardenerConfig := cluster.ClusterConfig
-	logger.Warnf("gardenerConfig.Name: %v", gardenerConfig.Name)
 
 	shoot, err := s.gardenerClient.Get(context.Background(), gardenerConfig.Name, v1.GetOptions{})
 	if err != nil {
 		return operations.StageResult{}, err
 	}
-	logger.Warnf("shoot.Status.ObservedGeneration: %v", shoot.Status.ObservedGeneration)
-	logger.Warnf("shoot.ObjectMeta.Generation: %v", shoot.ObjectMeta.Generation)
 
 	if shoot.Status.ObservedGeneration == shoot.ObjectMeta.Generation {
 		return operations.StageResult{Stage: s.nextStep, Delay: 0}, nil
 	}
-	logger.Warnf("shoot.Status.LastOperation.State: %v", shoot.Status.LastOperation.State)
-	logger.Warnf("gardencorev1beta1.LastOperationStateFailed: %v", gardencorev1beta1.LastOperationStateFailed)
-	logger.Warnf("shoot.Status.LastOperation.Description: %v", shoot.Status.LastOperation.Description)
 
 	if shoot.Status.LastOperation.State == gardencorev1beta1.LastOperationStateFailed {
 		err := fmt.Errorf(fmt.Sprintf("Gardener Shoot cluster upgrade failed. Last Shoot state: %s, Shoot description: %s", shoot.Status.LastOperation.State, shoot.Status.LastOperation.Description))
