d4d0dd1e4afa3552285d77d1c4a1160a6db7ca19
Michal Kempski <michal.kempski@sap.com>
2020-11-25 19:57:07 +0100

    Generate a random azure zone (#343)

--- components/kyma-environment-broker/internal/provider/common.go
+++ components/kyma-environment-broker/internal/provider/common.go
@@ -1,5 +1,11 @@
 package provider
 
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
 func updateString(toUpdate *string, value *string) {
 	if value != nil {
 		*toUpdate = *value
@@ -10,4 +16,24 @@ func updateSlice(toUpdate *[]string, value []string) {
 	if value != nil {
 		*toUpdate = value
 	}
+}
+
+func generateDefaultAzureZones() []string {
+	return []string{generateRandomAzureZone()}
+}
+
+func generateRandomAzureZone() string {
+	rand.Seed(time.Now().UnixNano())
+
+	const (
+		min = 1
+		max = 3
+	)
+
+	// generates random number from 1-3 range
+	getRandomNumber := func() int {
+		return rand.Intn(max-min+1) + min
+	}
+
+	return strconv.Itoa(getRandomNumber())
 }
