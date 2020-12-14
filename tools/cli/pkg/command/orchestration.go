1be9ca9ff28348bbd3756a4a68e397a91bf36ddb
Benjamin Somhegyi <benjamin.somhegyi@sap.com>
2020-12-14 16:35:05 +0100

    Address docs comments

--- tools/cli/pkg/command/orchestration.go
+++ tools/cli/pkg/command/orchestration.go
@@ -145,8 +145,8 @@ The command has the following modes:
   - Without specifying an orchestration ID as an argument. In this mode, the command lists all orchestrations, or orchestrations matching the --state option, if provided.
   - When specifying an orchestration ID as an argument. In this mode, the command displays details about the specific orchestration.
       If the optional --operation flag is provided, it displays details of the specified Runtime operation within the orchestration.
-  - When specifying an orchestration ID, and ` + "`operations` or `ops`" + ` as arguments. In this mode, the command displays the Runtime operations for the given orchestration.
-  - When specifying an orchestration ID, and ` + "`cancel`" + ` as arguments. In this mode, the command cancels the orchestration and all pending Runtime operations.`,
+  - When specifying an orchestration ID and ` + "`operations` or `ops`" + ` as arguments. In this mode, the command displays the Runtime operations for the given orchestration.
+  - When specifying an orchestration ID and ` + "`cancel`" + ` as arguments. In this mode, the command cancels the orchestration and all pending Runtime operations.`,
 		Example: `  kcp orchestrations --state inprogress                                   Display all orchestrations which are in progress.
   kcp orchestration 0c4357f5-83e0-4b72-9472-49b5cd417c00                  Display details about a specific orchestration.
   kcp orchestration 0c4357f5-83e0-4b72-9472-49b5cd417c00 --operation OID  Display details of the specified Runtime operation within the orchestration.
