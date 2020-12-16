7c3e3e84e1bdd7efe8928f90885f60c178c910b3
Jack Cheng <jack.cheng01@sap.com>
2020-12-16 13:25:37 +0800

    Revert changes to kcp cli before server side code is merged

--- tools/cli/pkg/command/runtime.go
+++ tools/cli/pkg/command/runtime.go
@@ -40,10 +40,6 @@ var tableColumns = []printer.Column{
 		FieldSpec: "{.ProviderRegion}",
 	},
 	{
-		Header:    "PLAN",
-		FieldSpec: "{.ServicePlanName}",
-	},
-	{
 		Header:         "CREATED AT",
 		FieldFormatter: runtimeCreatedAt,
 	},
@@ -76,7 +72,6 @@ The command supports filtering Runtimes based on various attributes. See the lis
 	cobraCmd.Flags().StringSliceVarP(&cmd.params.SubAccountIDs, "subaccount", "s", nil, "Filter by subaccount ID. You can provide multiple values, either separated by a comma (e.g. SAID1,SAID2), or by specifying the option multiple times.")
 	cobraCmd.Flags().StringSliceVarP(&cmd.params.RuntimeIDs, "runtime-id", "i", nil, "Filter by Runtime ID. You can provide multiple values, either separated by a comma (e.g. ID1,ID2), or by specifying the option multiple times.")
 	cobraCmd.Flags().StringSliceVarP(&cmd.params.Regions, "region", "r", nil, "Filter by provider region. You can provide multiple values, either separated by a comma (e.g. westeurope,northeurope), or by specifying the option multiple times.")
-	cobraCmd.Flags().StringSliceVarP(&cmd.params.Plans, "plan", "p", nil, "Filter by service plan name. You can provide multiple values, either separated by a comma (e.g. azure,trial), or by specifying the option multiple times.")
 
 	return cobraCmd
 }
