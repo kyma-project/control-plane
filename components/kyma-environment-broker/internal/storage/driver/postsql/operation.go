cbc390f49ef497e729e0d13beaad1cbb60a10e91
michal.kempski@sap.com <michal.kempski@sap.com>
2021-01-05 17:22:55 +0100

    Fix operations unmarshal

--- components/kyma-environment-broker/internal/storage/driver/postsql/operation.go
+++ components/kyma-environment-broker/internal/storage/driver/postsql/operation.go
@@ -693,7 +693,7 @@ func (s *operations) toOperation(op *dbmodel.OperationDTO) (internal.Operation, 
 
 	err := s.decryptBasicAuth(&pp)
 	if err != nil {
-		return internal.Operation{}, errors.Wrap(err, "while encrypting basic auth")
+		return internal.Operation{}, errors.Wrap(err, "while decrypting basic auth")
 	}
 
 	return internal.Operation{
@@ -732,13 +732,13 @@ func (s *operations) toProvisioningOperation(op *dbmodel.OperationDTO) (*interna
 	}
 	var operation internal.ProvisioningOperation
 	var err error
-	operation.Operation, err = s.toOperation(op)
-	if err != nil {
-		return nil, err
-	}
 	err = json.Unmarshal([]byte(op.Data), &operation)
 	if err != nil {
 		return nil, errors.New("unable to unmarshall provisioning data")
+	}
+	operation.Operation, err = s.toOperation(op)
+	if err != nil {
+		return nil, err
 	}
 
 	return &operation, nil
@@ -765,13 +765,13 @@ func (s *operations) toDeprovisioningOperation(op *dbmodel.OperationDTO) (*inter
 	}
 	var operation internal.DeprovisioningOperation
 	var err error
-	operation.Operation, err = s.toOperation(op)
-	if err != nil {
-		return nil, err
-	}
 	err = json.Unmarshal([]byte(op.Data), &operation)
 	if err != nil {
 		return nil, errors.New("unable to unmarshall provisioning data")
+	}
+	operation.Operation, err = s.toOperation(op)
+	if err != nil {
+		return nil, err
 	}
 
 	return &operation, nil
@@ -798,13 +798,13 @@ func (s *operations) toUpgradeKymaOperation(op *dbmodel.OperationDTO) (*internal
 	}
 	var operation internal.UpgradeKymaOperation
 	var err error
-	operation.Operation, err = s.toOperation(op)
-	if err != nil {
-		return nil, err
-	}
 	err = json.Unmarshal([]byte(op.Data), &operation)
 	if err != nil {
 		return nil, errors.New("unable to unmarshall provisioning data")
+	}
+	operation.Operation, err = s.toOperation(op)
+	if err != nil {
+		return nil, err
 	}
 	operation.RuntimeOperation.ID = op.ID
 	if op.OrchestrationID.Valid {
