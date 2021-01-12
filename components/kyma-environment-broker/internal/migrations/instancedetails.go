add8530d6318d5dba6faaa2f60f78765849d94d8
Kamil Sputo <sputo.kamil@gmail.com>
2021-01-12 17:10:43 +0100

    wip

--- components/kyma-environment-broker/internal/migrations/instancedetails.go
+++ components/kyma-environment-broker/internal/migrations/instancedetails.go
@@ -1,6 +1,8 @@
 package migrations
 
 import (
+	"encoding/json"
+
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/storage"
 
 	"github.com/pkg/errors"
@@ -27,7 +29,12 @@ func (m *InstanceDetailsMigration) Migrate() error {
 	m.log.Infof("Performing instance details migration of %d operations", len(upgradeOperations))
 
 	for _, op := range upgradeOperations {
-		m.log.Infof("Existing upgradeKyma operation: %+v", op)
+		m.log.Infof("Existing upgradeKyma operation %s", op.Operation.ID)
+		pretty, err := json.MarshalIndent(op, "", "    ")
+		if err != nil {
+			m.log.Fatal("Failed to generate json", err)
+		}
+		m.log.WithField("operationID", op.Operation.ID).Infof("Instance details: %s", string(pretty))
 		if op.InstanceDetails.RuntimeID != "" {
 			m.log.Infof("InstanceDetails were found in operation %s, skipping", op.Operation.ID)
 			continue
@@ -36,13 +43,26 @@ func (m *InstanceDetailsMigration) Migrate() error {
 		if err != nil {
 			return errors.Wrap(err, "while listing operations")
 		}
+		m.log.Infof("Last provisioningOperation %s", lastProvOp.Operation.ID)
+		if lastProvOp.InstanceDetails.RuntimeID == "" {
+			m.log.Warnf("Empty InstanceDetails for provisioninOperation: %s", lastProvOp.Operation.ID)
+		}
+		pretty, err = json.MarshalIndent(lastProvOp, "", "    ")
+		if err != nil {
+			m.log.Fatal("Failed to generate json", err)
+		}
+		m.log.WithField("upgradeKymaOperationID", op.Operation.ID).Infof("provisioningOperation: %s", string(pretty))
 		op.InstanceDetails = lastProvOp.InstanceDetails
 		//_, err = m.operations.UpdateUpgradeKymaOperation(op)
 		//if err != nil {
 		//	return errors.Wrap(err, "while updating operation parameters")
 		//}
 		m.log.Infof("Operation %s was migrated", op.Operation.ID)
-		m.log.Infof("New upgradeKyma operation: %+v", op)
+		pretty, err = json.MarshalIndent(op, "", "    ")
+		if err != nil {
+			m.log.Fatal("Failed to generate json", err)
+		}
+		m.log.WithField("upgradeKymaOperationID", op.Operation.ID).Infof("Migrated operation: %s", string(pretty))
 	}
 
 	m.log.Info("Instance details migration end up successfully")
