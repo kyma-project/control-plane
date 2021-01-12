52b1ab6986e79b80ea8c23fbb08bd73f80781d42
Kamil Sputo <sputo.kamil@gmail.com>
2021-01-12 13:32:29 +0100

    go fmt files

--- components/kyma-environment-broker/internal/migrations/instancedetails_test.go
+++ components/kyma-environment-broker/internal/migrations/instancedetails_test.go
@@ -14,48 +14,48 @@ import (
 )
 
 func TestInstanceDetailsMigration_Migrate(t *testing.T) {
-		t.Run("should migrate InstanceDetails from existing ProvisioningOperation", func(t *testing.T) {
-			s := storage.NewMemoryStorage()
-			log := logrus.New()
+	t.Run("should migrate InstanceDetails from existing ProvisioningOperation", func(t *testing.T) {
+		s := storage.NewMemoryStorage()
+		log := logrus.New()
 
-			// given
-			err := s.Provisioning().InsertProvisioningOperation(fixProvisioningOperation())
-			require.NoError(t, err)
-			err = s.Operations().InsertUpgradeKymaOperation(internal.UpgradeKymaOperation{
-				Operation:        internal.Operation{
-					InstanceDetails:        internal.InstanceDetails{},
-					ID:                     "upgrade-op-id",
-					CreatedAt:              time.Now().Add(5*time.Minute),
-					UpdatedAt:              time.Now().Add(6*time.Minute),
-					InstanceID:             "instance-id",
-					State:                  orchestration.Canceled,
-					ProvisioningParameters: internal.ProvisioningParameters{},
-					OrchestrationID:        "orch-id",
-				},
-				RuntimeOperation: orchestration.RuntimeOperation{},
-				InputCreator:     nil,
-			})
-			require.NoError(t, err)
-
-			err = migrations.NewInstanceDetailsMigration(s.Operations(), log).Migrate()
-			require.NoError(t, err)
-
+		// given
+		err := s.Provisioning().InsertProvisioningOperation(fixProvisioningOperation())
+		require.NoError(t, err)
+		err = s.Operations().InsertUpgradeKymaOperation(internal.UpgradeKymaOperation{
+			Operation: internal.Operation{
+				InstanceDetails:        internal.InstanceDetails{},
+				ID:                     "upgrade-op-id",
+				CreatedAt:              time.Now().Add(5 * time.Minute),
+				UpdatedAt:              time.Now().Add(6 * time.Minute),
+				InstanceID:             "instance-id",
+				State:                  orchestration.Canceled,
+				ProvisioningParameters: internal.ProvisioningParameters{},
+				OrchestrationID:        "orch-id",
+			},
+			RuntimeOperation: orchestration.RuntimeOperation{},
+			InputCreator:     nil,
 		})
-	}
+		require.NoError(t, err)
+
+		err = migrations.NewInstanceDetailsMigration(s.Operations(), log).Migrate()
+		require.NoError(t, err)
+
+	})
+}
 
 func fixProvisioningOperation() internal.ProvisioningOperation {
 	return internal.ProvisioningOperation{
-		Operation:       internal.Operation{
-			InstanceDetails:        internal.InstanceDetails{
-				Lms:          internal.LMS{
-					TenantID:    "lms-tenant",
+		Operation: internal.Operation{
+			InstanceDetails: internal.InstanceDetails{
+				Lms: internal.LMS{
+					TenantID: "lms-tenant",
 				},
 				SubAccountID: "subacc-id",
 				RuntimeID:    "runtime-id",
 				ShootName:    "shoot-name",
 				ShootDomain:  "shoot-domain",
-				XSUAA:        internal.XSUAAData{
-					Instance:  internal.ServiceManagerInstanceInfo{
+				XSUAA: internal.XSUAAData{
+					Instance: internal.ServiceManagerInstanceInfo{
 						BrokerID:              "broker-id",
 						ServiceID:             "service-id",
 						PlanID:                "plan-id",
@@ -67,12 +67,12 @@ func fixProvisioningOperation() internal.ProvisioningOperation {
 					BindingID: "binding-id",
 				},
 			},
-			ID:                     "prov-op-id",
-			Version:                0,
-			CreatedAt:              time.Now(),
-			UpdatedAt:              time.Now().Add(1*time.Minute),
-			InstanceID:             "instance-id",
-			State:                  domain.Succeeded,
+			ID:         "prov-op-id",
+			Version:    0,
+			CreatedAt:  time.Now(),
+			UpdatedAt:  time.Now().Add(1 * time.Minute),
+			InstanceID: "instance-id",
+			State:      domain.Succeeded,
 		},
 	}
 }
