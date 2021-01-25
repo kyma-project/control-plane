4ac83c8953a8badb7675c7bfa9b8b6f6cbf043fa
Rakesh Garimella <rakesh.garimella@sap.com>
2021-01-25 22:04:56 +0100

    Fix

--- components/kyma-environment-broker/internal/auditlog/config.go
+++ components/kyma-environment-broker/internal/auditlog/config.go
@@ -6,5 +6,5 @@ type Config struct {
 	Password      string `envconfig:"APP_AUDITLOG_PASSWORD"`
 	Tenant        string `envconfig:"APP_AUDITLOG_TENANT"`
 	EnableSeqHttp bool   `envconfig:"APP_AUDITLOG_ENABLE_SEQ_HTTP"`
-	NewLogConfig  bool   `envconfig: "APP_AUDITLOG_ENABLE_NEW_LOG_CONFIG"`
+	NewLogConfig  bool   `envconfig: "APP_AUDITLOG_NEWLOGCONFIG"`
 }
