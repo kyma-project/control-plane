e83ab4d5e5ea9285ef2cc969b16312e68584df34
Marcin Szwed <marcin.szwed@sap.com>
2021-01-27 10:14:35 +0100

    Implement fake edp client methods

--- components/kyma-environment-broker/internal/edp/client_fake.go
+++ components/kyma-environment-broker/internal/edp/client_fake.go
@@ -1,25 +1,79 @@
 package edp
 
+import "errors"
+
 // FakeClient implements the edp client interface but does not process data nor call real external system
-type FakeClient struct{}
+type FakeClient struct {
+	data map[string]interface{}
+}
 
 // NewFakeClient creates edp fake client
 func NewFakeClient() *FakeClient {
-	return &FakeClient{}
+	return &FakeClient{
+		data: make(map[string]interface{}),
+	}
 }
 
 func (f *FakeClient) CreateDataTenant(data DataTenantPayload) error {
+	err := checkDataTenantPayload(data)
+	if err != nil {
+		return err
+	}
+	f.data["DataTenant"] = struct {
+		Name        string
+		Environment string
+		Secret      string
+	}{
+		Name:        data.Name,
+		Environment: data.Environment,
+		Secret:      data.Secret,
+	}
 	return nil
 }
 
 func (f *FakeClient) CreateMetadataTenant(name, env string, data MetadataTenantPayload) error {
+	err := checkMetadataTenantPayload(data)
+	if err != nil {
+		return err
+	}
+	f.data["MetadataTenant"] = struct {
+		Key   string
+		Value string
+	}{
+		Key:   data.Key,
+		Value: data.Value,
+	}
 	return nil
 }
 
 func (f *FakeClient) DeleteDataTenant(name, env string) error {
+	_, found := f.data["DataTenant"]
+	if !found {
+		return errors.New("datatenant does not exist")
+	}
+	delete(f.data, "DataTenant")
 	return nil
 }
 
 func (f *FakeClient) DeleteMetadataTenant(name, env, key string) error {
+	_, found := f.data["MetadataTenant"]
+	if !found {
+		return errors.New("metadatatenant does not exist")
+	}
+	delete(f.data, "MetadataTenant")
+	return nil
+}
+
+func checkDataTenantPayload(data DataTenantPayload) error {
+	if data.Name == "" || data.Environment == "" || data.Secret == "" {
+		return errors.New("one of the fields in DataTenantPayload is missing")
+	}
+	return nil
+}
+
+func checkMetadataTenantPayload(data MetadataTenantPayload) error {
+	if data.Key == "" || data.Value == "" {
+		return errors.New("one of the fields in MetadataTenantPayload is missing")
+	}
 	return nil
 }
