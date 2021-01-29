c01c0f193e9abad1ab7c909fc5c226f9c5a80b72
Przemyslaw Golicz <przemyslaw.golicz@sap.com>
2021-01-29 13:58:37 +0100

    build fix

--- components/provisioner/internal/operations/stages/provisioning/wait_for_cluster_creation.go
+++ components/provisioner/internal/operations/stages/provisioning/wait_for_cluster_creation.go
@@ -58,8 +58,10 @@ func (s *WaitForClusterCreationStep) Run(cluster model.Cluster, operation model.
 	if lastOperation != nil {
 		if lastOperation.State == gardencorev1beta1.LastOperationStateSucceeded {
 
-			logger.Infof("Saving seed %s to database: %s", shoot.Spec.SeedName)
-
+			if  shoot.Spec.SeedName != nil {
+				logger.Infof("Saving seed %s to database: %s", shoot.Spec.SeedName)
+			}
+			
 			return s.proceedToInstallation(cluster, shoot, operation.ID)
 		}
 
@@ -77,8 +79,9 @@ func (s *WaitForClusterCreationStep) Run(cluster model.Cluster, operation model.
 
 func (s *WaitForClusterCreationStep) proceedToInstallation(cluster model.Cluster, shoot *gardener_types.Shoot, operationId string) (operations.StageResult, error) {
 
-	log.Infof("Saving seed %s to database: %s", shoot.Spec.SeedName)
-
+	if  shoot.Spec.SeedName != nil {
+		log.Infof("Saving seed %s to database: %s", shoot.Spec.SeedName)
+	}
 	kubeconfig, err := s.kubeconfigProvider.FetchRaw(shoot.Name)
 	if err != nil {
 		return operations.StageResult{}, err
