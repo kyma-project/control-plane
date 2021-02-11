9c5d9092d76924a1e9ef6b9716fb1e88e2a483d6
Michal Kempski <michal.kempski@sap.com>
2021-02-11 11:09:30 +0100

    Adjust KEB plans schemas (#452)

--- components/kyma-environment-broker/internal/broker/plans.go
+++ components/kyma-environment-broker/internal/broker/plans.go
@@ -3,8 +3,6 @@ package broker
 import (
 	"encoding/json"
 
-	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/runtime/components"
-
 	"github.com/pivotal-cf/brokerapi/v7/domain"
 )
 
@@ -45,20 +43,41 @@ const (
 
 func AzureRegions() []string {
 	return []string{
-		"centralus",
 		"eastus",
+		"centralus",
 		"westus2",
-		"northeurope",
 		"uksouth",
+		"northeurope",
+		"westeurope",
 		"japaneast",
 		"southeastasia",
-		"westeurope",
 	}
+}
+
+func GCPRegions() []string {
+	return []string{
+		"asia-south1", "asia-southeast1",
+		"asia-east2", "asia-east1",
+		"asia-northeast1", "asia-northeast2", "asia-northeast-3",
+		"australia-southeast1",
+		"europe-west2", "europe-west4", "europe-west5", "europe-west6", "europe-west3",
+		"europe-north1",
+		"us-west1", "us-west2", "us-west3",
+		"us-central1",
+		"us-east4",
+		"northamerica-northeast1", "southamerica-east1"}
 }
 
 type Type struct {
 	Type            string        `json:"type"`
+	Title           string        `json:"title,omitempty"`
+	Description     string        `json:"description,omitempty"`
 	Minimum         int           `json:"minimum,omitempty"`
+	Maximum         int           `json:"maximum,omitempty"`
+	MinLength       int           `json:"minLength,omitempty"`
+	MaxLength       int           `json:"maxLength,omitempty"`
+	Default         interface{}   `json:"default,omitempty"`
+	Example         interface{}   `json:"example,omitempty"`
 	Enum            []interface{} `json:"enum,omitempty"`
 	Items           []Type        `json:"items,omitempty"`
 	AdditionalItems *bool         `json:"additionalItems,omitempty"`
@@ -70,113 +89,80 @@ type RootSchema struct {
 	Type
 	Properties interface{} `json:"properties"`
 	Required   []string    `json:"required"`
+
+	// Specified to true enables form view on website
+	ShowFormView bool `json:"_show_form_view"`
+	// Specifies in what order properties will be displayed on the form
+	ControlsOrder []string `json:"_controlsOrder"`
 }
 
 type ProvisioningProperties struct {
-	Components     Type `json:"components"`
-	Name           Type `json:"name"`
-	DiskType       Type `json:"diskType"`
-	VolumeSizeGb   Type `json:"volumeSizeGb"`
-	MachineType    Type `json:"machineType"`
-	Region         Type `json:"region"`
-	Zones          Type `json:"zones"`
-	AutoScalerMin  Type `json:"autoScalerMin"`
-	AutoScalerMax  Type `json:"autoScalerMax"`
-	MaxSurge       Type `json:"maxSurge"`
-	MaxUnavailable Type `json:"maxUnavailable"`
+	Name          Type  `json:"name"`
+	Region        *Type `json:"region,omitempty"`
+	MachineType   *Type `json:"machineType,omitempty"`
+	AutoScalerMin *Type `json:"autoScalerMin,omitempty"`
+	AutoScalerMax *Type `json:"autoScalerMax,omitempty"`
 }
 
-func GCPSchema(machineTypes []string) []byte {
-	f := new(bool)
-	*f = false
-	t := new(bool)
-	*t = true
+func NameProperty() Type {
+	return Type{
+		Type:      "string",
+		Title:     "Cluster Name",
+		MinLength: 1,
+	}
+}
 
-	rs := RootSchema{
+// NewProvisioningProperties creates a new properties for different plans
+// Note that the order of properties will be the same in the form on the website
+func NewProvisioningProperties(machineTypes []string, regions []string) ProvisioningProperties {
+	return ProvisioningProperties{
+		Name: NameProperty(),
+		Region: &Type{
+			Type: "string",
+			Enum: ToInterfaceSlice(regions),
+		},
+		MachineType: &Type{
+			Type: "string",
+			Enum: ToInterfaceSlice(machineTypes),
+		},
+		AutoScalerMin: &Type{
+			Type:        "integer",
+			Minimum:     2,
+			Default:     2,
+			Description: "Specifies the minimum number of virtual machines to create",
+		},
+		AutoScalerMax: &Type{
+			Type:        "integer",
+			Minimum:     2,
+			Maximum:     40,
+			Default:     10,
+			Description: "Specifies the maximum number of virtual machines to create",
+		},
+	}
+}
+
+func DefaultControlsOrder() []string {
+	return []string{"name", "region", "machineType", "autoScalerMin", "autoScalerMax"}
+}
+
+func NewSchema(properties ProvisioningProperties, controlsOrder []string) RootSchema {
+	return RootSchema{
 		Schema: "http://json-schema.org/draft-04/schema#",
 		Type: Type{
 			Type: "object",
 		},
-		Properties: ProvisioningProperties{
-			Components: Type{
-				Type: "array",
-				Items: []Type{{
-					Type: "string",
-					Enum: ToInterfaceSlice([]string{components.Kiali, components.Tracing}),
-				}},
-				AdditionalItems: f,
-				UniqueItems:     t,
-			},
-			Name: Type{
-				Type: "string",
-			},
-			DiskType: Type{Type: "string"},
-			VolumeSizeGb: Type{
-				Type: "integer",
-			},
-			MachineType: Type{
-				Type: "string",
-				Enum: ToInterfaceSlice(machineTypes),
-			},
-			Region: Type{
-				Type: "string",
-				Enum: ToInterfaceSlice([]string{
-					"asia-south1", "asia-southeast1",
-					"asia-east2", "asia-east1",
-					"asia-northeast1", "asia-northeast2", "asia-northeast-3",
-					"australia-southeast1",
-					"europe-west2", "europe-west4", "europe-west5", "europe-west6", "europe-west3",
-					"europe-north1",
-					"us-west1", "us-west2", "us-west3",
-					"us-central1",
-					"us-east4",
-					"northamerica-northeast1", "southamerica-east1"}),
-			},
-			Zones: Type{
-				Type: "array",
-				Items: []Type{{
-					Type: "string",
-					Enum: ToInterfaceSlice([]string{
-						"asia-south1-a", "asia-south1-b", "asia-south1-c",
-						"asia-southeast1-a", "asia-southeast1-b", "asia-southeast1-c",
-						"asia-east2-a", "asia-east2-b", "asia-east2-c",
-						"asia-east1-a", "asia-east1-b", "asia-east1-c",
-						"asia-northeast1-a", "asia-northeast1-b", "asia-northeast1-c",
-						"asia-northeast2-a", "asia-northeast2-b", "asia-northeast2-c",
-						"asia-northeast-3-a", "asia-northeast-3-b", "asia-northeast-3-c",
-						"australia-southeast1-a", "australia-southeast1-b", "australia-southeast1-c",
-						"europe-west2-a", "europe-west2-b", "europe-west2-c",
-						"europe-west4-a", "europe-west4-b", "europe-west4-c",
-						"europe-west5-a", "europe-west5-b", "europe-west5-c",
-						"europe-west6-a", "europe-west6-b", "europe-west6-c",
-						"europe-west3-a", "europe-west3-b", "europe-west3-c",
-						"europe-north1-a", "europe-north1-b", "europe-north1-c",
-						"us-west1-a", "us-west1-b", "us-west1-c",
-						"us-west2-a", "us-west2-b", "us-west2-c",
-						"us-west3-a", "us-west3-b", "us-west3-c",
-						"us-central1-a", "us-central1-b", "us-central1-c",
-						"us-east4-a", "us-east4-b", "us-east4-c",
-						"northamerica-northeast1-a", "northamerica-northeast1-b", "northamerica-northeast1-c",
-						"southamerica-east1-a", "southamerica-east1-b", "southamerica-east1-c"}),
-				}},
-			},
-			AutoScalerMin: Type{
-				Type: "integer",
-			},
-			AutoScalerMax: Type{
-				Type: "integer",
-			},
-			MaxSurge: Type{
-				Type: "integer",
-			},
-			MaxUnavailable: Type{
-				Type: "integer",
-			},
-		},
-		Required: []string{"name"},
+		Properties:    properties,
+		ShowFormView:  true,
+		Required:      []string{"name"},
+		ControlsOrder: controlsOrder,
 	}
+}
 
-	bytes, err := json.Marshal(rs)
+func GCPSchema(machineTypes []string) []byte {
+	properties := NewProvisioningProperties(machineTypes, GCPRegions())
+	schema := NewSchema(properties, DefaultControlsOrder())
+
+	bytes, err := json.Marshal(schema)
 	if err != nil {
 		panic(err)
 	}
@@ -184,65 +170,10 @@ func GCPSchema(machineTypes []string) []byte {
 }
 
 func AzureSchema(machineTypes []string) []byte {
-	f := new(bool)
-	*f = false
-	t := new(bool)
-	*t = true
-	rs := RootSchema{
-		Schema: "http://json-schema.org/draft-04/schema#",
-		Type: Type{
-			Type: "object",
-		},
-		Properties: ProvisioningProperties{
-			Components: Type{
-				Type: "array",
-				Items: []Type{{
-					Type: "string",
-					Enum: ToInterfaceSlice([]string{components.Kiali, components.Tracing}),
-				}},
-				AdditionalItems: f,
-				UniqueItems:     t,
-			},
-			Name: Type{
-				Type: "string",
-			},
-			DiskType: Type{Type: "string"},
-			VolumeSizeGb: Type{
-				Type:    "integer",
-				Minimum: 50,
-			},
-			MachineType: Type{
-				Type: "string",
-				Enum: ToInterfaceSlice(machineTypes),
-			},
-			Region: Type{
-				Type: "string",
-				Enum: ToInterfaceSlice(AzureRegions()),
-			},
-			Zones: Type{
-				Type: "array",
-				Items: []Type{{
-					Type: "string",
-					//TODO: add enum for zones
-				}},
-			},
-			AutoScalerMin: Type{
-				Type: "integer",
-			},
-			AutoScalerMax: Type{
-				Type: "integer",
-			},
-			MaxSurge: Type{
-				Type: "integer",
-			},
-			MaxUnavailable: Type{
-				Type: "integer",
-			},
-		},
-		Required: []string{"name"},
-	}
+	properties := NewProvisioningProperties(machineTypes, AzureRegions())
+	schema := NewSchema(properties, DefaultControlsOrder())
 
-	bytes, err := json.Marshal(rs)
+	bytes, err := json.Marshal(schema)
 	if err != nil {
 		panic(err)
 	}
@@ -250,34 +181,15 @@ func AzureSchema(machineTypes []string) []byte {
 }
 
 func TrialSchema() []byte {
-	schema := `{
-  "$schema": "http://json-schema.org/draft-04/schema#",
-  "type": "object",
-  "properties": {
-    "name": {
-      "type": "string"
-    },
-    "region": {
-      "type": "string",
-      "enum": [
-        "europe",
-        "us"
-      ]
-    },
-    "provider": {
-      "type": "string",
-      "enum": [
-        "Azure",
-        "GCP"
-      ]
-    }
-  },
-  "required": [
-    "name"
-  ]
-}`
+	schema := NewSchema(
+		ProvisioningProperties{
+			Name: NameProperty(),
+		}, []string{"name"})
 
-	bytes := []byte(schema)
+	bytes, err := json.Marshal(schema)
+	if err != nil {
+		panic(err)
+	}
 	return bytes
 }
 
