e2a70cd952f441f7686d14ac166857c93a8437f7
Rakesh Garimella <rakesh.garimella@sap.com>
2021-03-03 14:56:04 +0100

    try inMem and psql

--- components/kyma-environment-broker/cmd/broker/main.go
+++ components/kyma-environment-broker/cmd/broker/main.go
@@ -184,7 +184,6 @@ func main() {
 	// create storage
 	cipher := storage.NewEncrypter(cfg.Database.SecretKey)
 	var db storage.BrokerStorage
-	cfg.DbInMemory = true
 	if cfg.DbInMemory {
 		db = storage.NewMemoryStorage()
 	} else {
@@ -226,7 +225,8 @@ func main() {
 		fatalOnError(err)
 	}
 	clsClient := cls.NewClient(clsConfig, logs.WithField("service", "clsClient"))
-	clsProvisioner := cls.NewProvisioner(db.CLSInstances(), clsClient, logs.WithField("service", "clsProvisioner"))
+	var clsDb = storage.NewMemoryStorage()
+	clsProvisioner := cls.NewProvisioner(clsDb.CLSInstances(), clsClient, logs.WithField("service", "clsProvisioner"))
 
 	//// LMS
 	//fatalOnError(cfg.LMS.Validate())
@@ -436,7 +436,7 @@ func main() {
 
 	deprovisioningInit := deprovisioning.NewInitialisationStep(db.Operations(), db.Instances(), provisionerClient, accountProvider, serviceManagerClientFactory, cfg.OperationTimeout)
 	deprovisionManager.InitStep(deprovisioningInit)
-	clsDeprovisioner := cls.NewDeprovisioner(db.CLSInstances(), clsClient, logs.WithField("service", "clsDeprovisioner"))
+	clsDeprovisioner := cls.NewDeprovisioner(clsDb.CLSInstances(), clsClient, logs.WithField("service", "clsDeprovisioner"))
 
 	deprovisioningSteps := []struct {
 		disabled bool
