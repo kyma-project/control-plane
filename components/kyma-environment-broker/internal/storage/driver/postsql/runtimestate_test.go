944e9f8c6062998a000e48d13ad15a59db6f39b1
Marcin Szwed <marcin.szwed@sap.com>
2021-03-05 11:25:14 +0100

    Extract runtimestate tests from storage_test.go

---
+++ components/kyma-environment-broker/internal/storage/driver/postsql/runtimestate_test.go
@@ -0,0 +1,64 @@
+package postsql_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal"
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/storage"
+	"github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRuntimeState(t *testing.T) {
+	ctx := context.Background()
+	cleanupNetwork, err := storage.EnsureTestNetworkForDB(t, ctx)
+	require.NoError(t, err)
+	defer cleanupNetwork()
+
+	t.Run("RuntimeStates", func(t *testing.T) {
+		containerCleanupFunc, cfg, err := storage.InitTestDBContainer(t, ctx, "test_DB_1")
+		require.NoError(t, err)
+		defer containerCleanupFunc()
+
+		fixID := "test"
+		givenRuntimeState := internal.RuntimeState{
+			ID:          fixID,
+			CreatedAt:   time.Now(),
+			RuntimeID:   fixID,
+			OperationID: fixID,
+			KymaConfig: gqlschema.KymaConfigInput{
+				Version: fixID,
+			},
+			ClusterConfig: gqlschema.GardenerConfigInput{
+				KubernetesVersion: fixID,
+			},
+		}
+
+		err = storage.InitTestDBTables(t, cfg.ConnectionURL())
+		require.NoError(t, err)
+
+		cipher := storage.NewEncrypter(cfg.SecretKey)
+		brokerStorage, _, err := storage.NewFromConfig(cfg, cipher, logrus.StandardLogger())
+		require.NoError(t, err)
+
+		svc := brokerStorage.RuntimeStates()
+
+		err = svc.Insert(givenRuntimeState)
+		require.NoError(t, err)
+
+		runtimeStates, err := svc.ListByRuntimeID(fixID)
+		require.NoError(t, err)
+		assert.Len(t, runtimeStates, 1)
+		assert.Equal(t, fixID, runtimeStates[0].KymaConfig.Version)
+		assert.Equal(t, fixID, runtimeStates[0].ClusterConfig.KubernetesVersion)
+
+		state, err := svc.GetByOperationID(fixID)
+		require.NoError(t, err)
+		assert.Equal(t, fixID, state.KymaConfig.Version)
+		assert.Equal(t, fixID, state.ClusterConfig.KubernetesVersion)
+	})
+}
