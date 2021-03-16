64a39d3498db8c177123ade7a08ca99186411201
Suleyman Akbas <suleyman.akbas@sap.com>
2021-03-16 10:31:52 +0100

    Improve CLS log messages (#594)

--- components/kyma-environment-broker/internal/cls/deprovisioner.go
+++ components/kyma-environment-broker/internal/cls/deprovisioner.go
@@ -41,7 +41,7 @@ type DeprovisionRequest struct {
 func (d *Deprovisioner) Deprovision(smClient servicemanager.Client, request *DeprovisionRequest) error {
 	instance, exists, err := d.storage.FindByID(request.Instance.InstanceID)
 	if err != nil {
-		return errors.Wrapf(err, "while trying to find the cls instance %s", request.Instance.InstanceID)
+		return errors.Wrapf(err, "while finding CLS instance %s", request.Instance.InstanceID)
 	}
 
 	if !exists {
@@ -49,27 +49,27 @@ func (d *Deprovisioner) Deprovision(smClient servicemanager.Client, request *Dep
 	}
 
 	if !instance.IsReferencedBy(request.SKRInstanceID) {
-		d.log.Warnf("Provided cls instance %s is not referenced by the skr %s", instance.ID, request.SKRInstanceID)
+		d.log.Warnf("Provided CLS instance %s is not referenced by the SKR %s", instance.ID, request.SKRInstanceID)
 		return nil
 	}
 
 	if err := instance.RemoveReference(request.SKRInstanceID); err != nil {
-		return errors.Wrapf(err, "while unreferencing the cls instance %s", instance.ID())
+		return errors.Wrapf(err, "while unreferencing CLS instance %s", instance.ID())
 	}
 
 	if err := d.storage.Update(*instance); err != nil {
-		return errors.Wrapf(err, "while updaing the cls instance %s", instance.ID())
+		return errors.Wrapf(err, "while updating CLS instance %s", instance.ID())
 	}
 
 	if instance.IsBeingRemoved() {
-		d.log.Infof("Removing the cls instance %s", instance.ID)
+		d.log.Infof("Removing CLS instance %s", instance.ID)
 
 		if err := d.remover.RemoveInstance(smClient, request.Instance); err != nil {
-			return errors.Wrapf(err, "while removing the cls instance %s", instance.ID())
+			return errors.Wrapf(err, "while removing CLS instance %s", instance.ID())
 		}
 
 		if err := d.storage.Delete(instance.ID()); err != nil {
-			return errors.Wrapf(err, "while deleting the cls instance %s", instance.ID())
+			return errors.Wrapf(err, "while deleting CLS instance %s", instance.ID())
 		}
 	}
 
