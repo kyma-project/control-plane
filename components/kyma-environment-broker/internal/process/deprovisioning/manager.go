e7eff37e29640f0295a92178feab18497e4bdb5a
Stanislav Khalash <stanislav.khalash@sap.com>
2021-03-22 15:57:56 +0100

    Removed unnecessary debug log

--- components/kyma-environment-broker/internal/process/deprovisioning/manager.go
+++ components/kyma-environment-broker/internal/process/deprovisioning/manager.go
@@ -83,7 +83,6 @@ func (m *Manager) Execute(operationID string) (time.Duration, error) {
 	var when time.Duration
 	logOperation.Info("Start process operation steps")
 	logOperation.Infof("DEBUG: Operation recovered Operation: %#v", operation)
-	logOperation.Infof("DEBUG: Operation recovered CLS: %#v", operation.Cls)
 
 	for _, weightStep := range m.sortWeight() {
 		steps := m.steps[weightStep]
