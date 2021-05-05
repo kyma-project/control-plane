5f7332185fb372a4a4a4aa064d9e0358f4219cf2
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2021-05-05 15:28:35 +0200

    Runtime deprovisioning test (#660)
    * Add DeprovisioningSuite
    
    * Increase tests execution speed

--- components/kyma-environment-broker/cmd/broker/suite_test.go
+++ components/kyma-environment-broker/cmd/broker/suite_test.go
@@ -64,6 +64,7 @@ const (
 	brokerID               = "fake-broker-id"
 	emsOfferingID          = "ems-fake-id"
 	operationID            = "provisioning-op-id"
+	deprovisioningOpID     = "deprovisioning-op-id"
 	instanceID             = "instance-id"
 	smRegion               = "eu"
 	dbSecretKey            = "1234567890123456"
@@ -835,6 +836,7 @@ func fixAccountProvider() *hyperscalerautomock.AccountProvider {
 		},
 		Name: subscriptionNameShared,
 	}, nil)
+	accountProvider.On("MarkUnusedGardenerSecretBindingAsDirty", hyperscaler.Azure, mock.Anything).Return(nil)
 	return &accountProvider
 }
 
