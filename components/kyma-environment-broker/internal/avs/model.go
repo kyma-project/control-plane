83973456bcaab2b6066c45de47ff1f11b7cb6a3d
Kamil Sputo <sputo.kamil@gmail.com>
2021-06-22 01:38:43 +0200

    Use CloudProvider in avs evaluation name generator

--- components/kyma-environment-broker/internal/avs/model.go
+++ components/kyma-environment-broker/internal/avs/model.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal"
-	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/broker"
 )
 
 const (
@@ -121,28 +120,15 @@ func generateNameAndDescription(operation internal.ProvisioningOperation, beType
 	subAccountID := operation.ProvisioningParameters.ErsContext.SubAccountID
 	name := operation.ProvisioningParameters.Parameters.Name
 	shootName := operation.InstanceDetails.ShootName
-	beName := fmt.Sprintf("K8S-%s-Kyma-%s-%s-%s", providerCodeByPlan(operation.ProvisioningParameters.PlanID), beType, subAccountID, name)
+	beName := fmt.Sprintf("K8S-%s-Kyma-%s-%s-%s", providerCodeByPlan(operation), beType, subAccountID, name)
 	beDescription := fmt.Sprintf("SKR instance Name: %s, Global Account: %s, Subaccount: %s, Shoot: %s",
 		name, globalAccountID, subAccountID, shootName)
 
 	return truncateString(beName, 80), truncateString(beDescription, 255)
 }
 
-func providerCodeByPlan(planID string) string {
-	switch planID {
-	case broker.AWSPlanID, broker.AWSHAPlanID:
-		return "AWS"
-	case broker.GCPPlanID:
-		return "GCP"
-	case broker.AzurePlanID, broker.AzureLitePlanID, broker.AzureHAPlanID:
-		return "AZR"
-	case broker.TrialPlanID, broker.FreemiumPlanID:
-		return "AZR"
-	case broker.OpenStackPlanID:
-		return "CC"
-	default:
-		return "AZR"
-	}
+func providerCodeByPlan(operation internal.ProvisioningOperation) string {
+	return string(operation.InputCreator.Provider())
 }
 
 func truncateString(input string, num int) string {
