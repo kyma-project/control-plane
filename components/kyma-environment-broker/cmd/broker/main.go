e4a0cf6d3f30d81c03133acc78dc70f27a0e1944
Radu Fantaziu <radu.fantaziu@sap.com>
2021-07-27 13:26:51 +0200

    Initial commit

--- components/kyma-environment-broker/cmd/broker/main.go
+++ components/kyma-environment-broker/cmd/broker/main.go
@@ -127,10 +127,6 @@ type Config struct {
 	XSUAA struct {
 		Disabled bool `envconfig:"default=true"`
 	}
-	Ems struct {
-		Disabled                              bool `envconfig:"default=true"`
-		SkipDeprovisionAzureEventingAtUpgrade bool `envconfig:"default=false"`
-	}
 	Connectivity struct {
 		Disabled bool `envconfig:"default=true"`
 	}
@@ -555,14 +551,6 @@ func NewProvisioningProcessingQueue(ctx context.Context, provisionManager *provi
 		},
 		{
 			stage: createRuntimeStageName,
-			step: provisioning.NewServiceManagerOfferingStep("EMS_Offering",
-				provisioning.EmsOfferingName, provisioning.EmsPlanName, func(op *internal.ProvisioningOperation) *internal.ServiceManagerInstanceInfo {
-					return &op.Ems.Instance
-				}, db.Operations()),
-			disabled: cfg.Ems.Disabled,
-		},
-		{
-			stage: createRuntimeStageName,
 			step:  provisioning.NewResolveCredentialsStep(db.Operations(), accountProvider),
 		},
 		{
@@ -575,11 +563,6 @@ func NewProvisioningProcessingQueue(ctx context.Context, provisionManager *provi
 				NamespaceAdminRole:  "nar",
 			}),
 			disabled: cfg.XSUAA.Disabled,
-		},
-		{
-			stage:    createRuntimeStageName,
-			step:     provisioning.NewEmsProvisionStep(db.Operations()),
-			disabled: cfg.Ems.Disabled,
 		},
 		{
 			stage:    createRuntimeStageName,
@@ -621,11 +604,6 @@ func NewProvisioningProcessingQueue(ctx context.Context, provisionManager *provi
 			stage:    createRuntimeStageName,
 			step:     provisioning.NewXSUAABindingStep(db.Operations()),
 			disabled: cfg.XSUAA.Disabled,
-		},
-		{
-			stage:    createRuntimeStageName,
-			step:     provisioning.NewEmsBindStep(db.Operations(), cfg.Database.SecretKey),
-			disabled: cfg.Ems.Disabled,
 		},
 		{
 			stage:    createRuntimeStageName,
@@ -724,11 +702,6 @@ func NewDeprovisioningProcessingQueue(ctx context.Context, workersAmount int, de
 		},
 		{
 			weight:   1,
-			step:     deprovisioning.NewEmsUnbindStep(db.Operations()),
-			disabled: cfg.Ems.Disabled,
-		},
-		{
-			weight:   1,
 			step:     deprovisioning.NewConnectivityUnbindStep(db.Operations()),
 			disabled: cfg.Connectivity.Disabled,
 		},
@@ -736,11 +709,6 @@ func NewDeprovisioningProcessingQueue(ctx context.Context, workersAmount int, de
 			weight:   2,
 			step:     deprovisioning.NewXSUAADeprovisionStep(db.Operations()),
 			disabled: cfg.XSUAA.Disabled,
-		},
-		{
-			weight:   2,
-			step:     deprovisioning.NewEmsDeprovisionStep(db.Operations()),
-			disabled: cfg.Ems.Disabled,
 		},
 		{
 			weight: 2,
@@ -783,14 +751,6 @@ func NewKymaOrchestrationProcessingQueue(ctx context.Context, db storage.BrokerS
 	}{
 		{
 			weight: 1,
-			step: upgrade_kyma.NewServiceManagerOfferingStep("EMS_Offering",
-				provisioning.EmsOfferingName, provisioning.EmsPlanName, func(op *internal.UpgradeKymaOperation) *internal.ServiceManagerInstanceInfo {
-					return &op.Ems.Instance
-				}, db.Operations()),
-			disabled: cfg.Ems.Disabled,
-		},
-		{
-			weight: 1,
 			// TODO: Should we skip Connectivity for trial plan? Determine during story productization
 			step: upgrade_kyma.NewServiceManagerOfferingStep("Connectivity_Offering",
 				provisioning.ConnectivityOfferingName, provisioning.ConnectivityPlanName, func(op *internal.UpgradeKymaOperation) *internal.ServiceManagerInstanceInfo {
@@ -812,18 +772,8 @@ func NewKymaOrchestrationProcessingQueue(ctx context.Context, db storage.BrokerS
 		},
 		{
 			weight:   4,
-			step:     upgrade_kyma.NewEmsUpgradeProvisionStep(db.Operations()),
-			disabled: cfg.Ems.Disabled,
-		},
-		{
-			weight:   4,
 			step:     upgrade_kyma.NewConnectivityUpgradeProvisionStep(db.Operations()),
 			disabled: cfg.Connectivity.Disabled,
-		},
-		{
-			weight:   7,
-			step:     upgrade_kyma.NewEmsUpgradeBindStep(db.Operations(), cfg.Database.SecretKey),
-			disabled: cfg.Ems.Disabled,
 		},
 		{
 			weight:   7,
