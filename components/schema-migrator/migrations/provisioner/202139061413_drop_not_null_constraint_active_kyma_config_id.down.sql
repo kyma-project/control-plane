178c9ab3a5d1719216841d4b172f1d6d9ffbbe87
Mariusz Szymanski <mariusz.szymanski@sap.com>
2021-08-09 13:07:58 +0200

    update down.sql

--- components/schema-migrator/migrations/provisioner/202139061413_drop_not_null_constraint_active_kyma_config_id.down.sql
+++ components/schema-migrator/migrations/provisioner/202139061413_drop_not_null_constraint_active_kyma_config_id.down.sql
@@ -1,4 +1,5 @@
 BEGIN;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 UPDATE cluster SET active_kyma_config_id = uuid_generate_v1() WHERE active_kyma_config_id IS NULL;
 ALTER TABLE cluster ALTER COLUMN active_kyma_config_id SET NOT NULL;
 COMMIT;
