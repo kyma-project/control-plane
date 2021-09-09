ae7de031bedf44f29988cd4d7d9e828f342351a4
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2021-09-09 22:27:51 +0200

    Determine Kyma's major version (#914)
    * Add logic to determine Kyma's major version
    
    * Add logic for determining Kyma's major verion in upgrade process
    
    * Add tests and adjust old tests

--- components/kyma-environment-broker/internal/runtimeversion/runtimeversion_test.go
+++ components/kyma-environment-broker/internal/runtimeversion/runtimeversion_test.go
@@ -31,6 +31,7 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, runtimeVer, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
 		require.Equal(t, internal.Parameters, ver.Origin)
 	})
 	t.Run("should return version from Defaults when version not provided", func(t *testing.T) {
@@ -49,6 +50,7 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, runtimeVer, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
 		require.Equal(t, internal.Defaults, ver.Origin)
 	})
 	t.Run("should return version from GlobalAccount mapping when only GlobalAccount mapping provided", func(t *testing.T) {
@@ -71,6 +73,7 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, versionForGA, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
 		require.Equal(t, internal.AccountMapping, ver.Origin)
 	})
 	t.Run("should return version from SubAccount mapping when both GA and SA mapping provided", func(t *testing.T) {
@@ -95,6 +98,7 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, versionForSA, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
 		require.Equal(t, internal.AccountMapping, ver.Origin)
 	})
 	t.Run("should return Kyma Version from ProvisioningParameters even when previewVersion provided", func(t *testing.T) {
@@ -120,6 +124,7 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, runtimeVer, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
 	})
 	t.Run("should return previewVersion when version not provided", func(t *testing.T) {
 		// given
@@ -144,6 +149,7 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, previewVer, ver.Version)
+		require.Equal(t, 2, ver.MajorVersion)
 	})
 	t.Run("should return previewVersion when both GA and SA mapping provided", func(t *testing.T) {
 		// given
@@ -172,6 +178,169 @@ func Test_RuntimeVersionConfigurator_ForProvisioning_FromParameters(t *testing.T
 		// then
 		require.NoError(t, err)
 		require.Equal(t, previewVer, ver.Version)
+		require.Equal(t, 2, ver.MajorVersion)
+	})
+	t.Run("should return custom version from ProvisioningParameters and default Kyma major version", func(t *testing.T) {
+		// given
+		runtimeVer := "1.24.5"
+		previewVer := "2.0.0"
+		customVer := "PR-123"
+		operation := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ProvisioningParameters: internal.ProvisioningParameters{
+					Parameters: internal.ProvisioningParametersDTO{
+						KymaVersion: customVer,
+					},
+				},
+			},
+		}
+
+		rvc := NewRuntimeVersionConfigurator(runtimeVer, previewVer, &AccountVersionMapping{})
+
+		// when
+		ver, err := rvc.ForProvisioning(operation)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, customVer, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
+	})
+	t.Run("should return custom version from ProvisioningParameters and default Kyma preview major version", func(t *testing.T) {
+		// given
+		runtimeVer := "1.24.5"
+		previewVer := "2.0.0"
+		customVer := "PR-123"
+		planID := broker.PreviewPlanID
+		operation := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ProvisioningParameters: internal.ProvisioningParameters{
+					PlanID: planID,
+					Parameters: internal.ProvisioningParametersDTO{
+						KymaVersion: customVer,
+					},
+				},
+			},
+		}
+
+		rvc := NewRuntimeVersionConfigurator(runtimeVer, previewVer, &AccountVersionMapping{})
+
+		// when
+		ver, err := rvc.ForProvisioning(operation)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, customVer, ver.Version)
+		require.Equal(t, 2, ver.MajorVersion)
+	})
+	t.Run("should return custom version from GlobalAccount mapping and default Kyma major version when only GlobalAccount mapping provided", func(t *testing.T) {
+		// given
+		runtimeVer := "1.24.5"
+		previewVer := "2.0.0"
+		customVerGA := "PR-123"
+		operation := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ProvisioningParameters: internal.ProvisioningParameters{
+					ErsContext: internal.ERSContext{GlobalAccountID: fixGlobalAccountID, SubAccountID: versionForSA},
+				},
+			},
+		}
+		rvc := NewRuntimeVersionConfigurator(runtimeVer, previewVer, fixAccountVersionMapping(t, map[string]string{
+			fmt.Sprintf("%s%s", globalAccountPrefix, fixGlobalAccountID): customVerGA,
+		}))
+
+		// when
+		ver, err := rvc.ForProvisioning(operation)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, customVerGA, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
+		require.Equal(t, internal.AccountMapping, ver.Origin)
+	})
+	t.Run("should return version from SubAccount mapping and default Kyma major version when both GA and SA mapping provided", func(t *testing.T) {
+		// given
+		runtimeVer := "1.24.5"
+		previewVer := "2.0.0"
+		customVerGA := "PR-123"
+		customVerSA := "PR-456"
+		operation := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ProvisioningParameters: internal.ProvisioningParameters{
+					ErsContext: internal.ERSContext{GlobalAccountID: fixGlobalAccountID,
+						SubAccountID: fixSubAccountID},
+				},
+			},
+		}
+		rvc := NewRuntimeVersionConfigurator(runtimeVer, previewVer, fixAccountVersionMapping(t, map[string]string{
+			fmt.Sprintf("%s%s", globalAccountPrefix, fixGlobalAccountID): customVerGA,
+			fmt.Sprintf("%s%s", subaccountPrefix, fixSubAccountID):       customVerSA,
+		}))
+
+		// when
+		ver, err := rvc.ForProvisioning(operation)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, customVerSA, ver.Version)
+		require.Equal(t, 1, ver.MajorVersion)
+		require.Equal(t, internal.AccountMapping, ver.Origin)
+	})
+	t.Run("should return preview version and default Kyma preview major version when only custom GlobalAccount mapping provided", func(t *testing.T) {
+		// given
+		runtimeVer := "1.24.5"
+		previewVer := "2.0.0"
+		customVerGA := "PR-123"
+		planID := broker.PreviewPlanID
+		operation := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ProvisioningParameters: internal.ProvisioningParameters{
+					PlanID:     planID,
+					ErsContext: internal.ERSContext{GlobalAccountID: fixGlobalAccountID, SubAccountID: versionForSA},
+				},
+			},
+		}
+		rvc := NewRuntimeVersionConfigurator(runtimeVer, previewVer, fixAccountVersionMapping(t, map[string]string{
+			fmt.Sprintf("%s%s", globalAccountPrefix, fixGlobalAccountID): customVerGA,
+		}))
+
+		// when
+		ver, err := rvc.ForProvisioning(operation)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, previewVer, ver.Version)
+		require.Equal(t, 2, ver.MajorVersion)
+		require.Equal(t, internal.AccountMapping, ver.Origin)
+	})
+	t.Run("should return preview version and Kyma preview major version when both GA and SA mapping provided", func(t *testing.T) {
+		// given
+		runtimeVer := "1.24.5"
+		previewVer := "2.0.0"
+		customVerGA := "PR-123"
+		customVerSA := "PR-456"
+		planID := broker.PreviewPlanID
+		operation := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ProvisioningParameters: internal.ProvisioningParameters{
+					PlanID: planID,
+					ErsContext: internal.ERSContext{GlobalAccountID: fixGlobalAccountID,
+						SubAccountID: fixSubAccountID},
+				},
+			},
+		}
+
+		rvc := NewRuntimeVersionConfigurator(runtimeVer, previewVer, fixAccountVersionMapping(t, map[string]string{
+			fmt.Sprintf("%s%s", globalAccountPrefix, fixGlobalAccountID): customVerGA,
+			fmt.Sprintf("%s%s", subaccountPrefix, fixSubAccountID):       customVerSA,
+		}))
+
+		// when
+		ver, err := rvc.ForProvisioning(operation)
+
+		// then
+		require.NoError(t, err)
+		require.Equal(t, previewVer, ver.Version)
+		require.Equal(t, 2, ver.MajorVersion)
 	})
 }
 
