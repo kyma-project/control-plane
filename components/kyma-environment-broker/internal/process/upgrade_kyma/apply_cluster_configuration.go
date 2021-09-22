ce520f5f59d64c178cc5544076727aa26b86d110
Miskiewicz <piotr.miskiewicz@sap.com>
2021-09-22 08:36:33 +0200

    update diagram

--- components/kyma-environment-broker/internal/process/upgrade_kyma/apply_cluster_configuration.go
+++ components/kyma-environment-broker/internal/process/upgrade_kyma/apply_cluster_configuration.go
@@ -27,7 +27,7 @@ func NewApplyClusterConfigurationStep(os storage.Operations,
 }
 
 func (s *ApplyClusterConfigurationStep) Name() string {
-		return "Apply_Cluster_Configuration"
+	return "Apply_Cluster_Configuration"
 }
 
 func (s *ApplyClusterConfigurationStep) Run(operation internal.UpgradeKymaOperation, log logrus.FieldLogger) (internal.UpgradeKymaOperation, time.Duration, error) {
