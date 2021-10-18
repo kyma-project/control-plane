4bb8fc0eb293b4aa0ede9296757d636a64a661de
Jack Cheng <jack.cheng01@sap.com>
2021-10-18 10:49:28 +0800

    make DNSProvider optional and update into helm charts

--- components/kyma-environment-broker/common/gardener/config.go
+++ components/kyma-environment-broker/common/gardener/config.go
@@ -3,8 +3,8 @@ package gardener
 import "github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
 
 type Config struct {
-	Project        string `envconfig:"default=gardenerProject"`
-	ShootDomain    string `envconfig:"optional"`
-	KubeconfigPath string `envconfig:"default=./dev/kubeconfig.yaml"`
-	DNSProvider    gqlschema.DNSProviderInput
+	Project        string                     `envconfig:"default=gardenerProject"`
+	ShootDomain    string                     `envconfig:"optional"`
+	KubeconfigPath string                     `envconfig:"default=./dev/kubeconfig.yaml"`
+	DNSProvider    gqlschema.DNSProviderInput `envconfig:"optional"`
 }
