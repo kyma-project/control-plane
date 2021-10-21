7007886dbfa456a8dc018f23c31f7315828323d4
Filip Strózik <filip.strozik@outlook.com>
2021-10-21 10:31:30 +0200

    Implement commands which allow users to disable and enable reconciliation for a cluster (#971)
    * Add ownership privilidges to wookiee team for kcp
    
    * Administrate reconciler remotely via CLI using REST API
    
    * integrate oapi client with kcp
    
    * code cleanup
    
    * code cleanup
    
    * remove external api doc
    
    * update client
    
    * update client
    
    * add mothership-api-url global flag
    
    * regenerate mothership client; extend rc cmd  print table with status field
    
    * fix makefile curl
    
    * regenerate oapi client
    
    * go mod tidy
    
    * disable m00g3n functions
    
    * implement draft
    
    * regenerate client
    
    * enable commented code
    
    * add tests; update shole logic
    
    * update commands
    
    * regenerate client
    
    * fix mothershipURL parameter
    
    * remove reconciliations command implementation
    
    * update codeowners
    
    * finish tests
    
    * add manual validation for the mothership-url parameter
    
    * delete manual validation of motherhip url
    
    * change shortcut for the shoot parameter to c
    
    * Improve oapi makefile
    
    * Improve error msg
    
    * Rename the mothership component to the reconciler
    
    * Update missing fields regarding component renaming
    
    * Update README
    
    * Update readme
    
    * Add suggestions
    
    * update readme
    
    * Apply suggestions from code review
    
    Co-authored-by: Małgorzata Świeca <mitorajmalgorzata@gmail.com>
    
    Co-authored-by: Marcin Witalis <marcin.witalis@sap.com>
    Co-authored-by: Małgorzata Świeca <mitorajmalgorzata@gmail.com>

--- tools/cli/go.mod
+++ tools/cli/go.mod
@@ -6,12 +6,14 @@ require (
 	github.com/int128/kubelogin v1.22.0
 	github.com/kyma-project/control-plane/components/kubeconfig-service v0.0.0-20201211152036-9bdabffd55fb
 	github.com/kyma-project/control-plane/components/kyma-environment-broker v0.0.0-20210601124357-126439534aac
+	github.com/kyma-project/control-plane/components/reconciler v0.0.0
 	github.com/liggitt/tabwriter v0.0.0-20181228230101-89fcab3d43de
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/mod v0.4.2
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 	gopkg.in/yaml.v2 v2.4.0
@@ -24,6 +26,7 @@ replace (
 	github.com/gardener/gardener => github.com/gardener/gardener v1.2.3
 	github.com/googleapis/gnostic => github.com/googleapis/gnostic v0.4.0
 	github.com/kyma-incubator/compass/components/director => github.com/kyma-incubator/compass/components/director v0.0.0-20210329081251-209fb6d91e72
+	github.com/kyma-project/control-plane/components/reconciler => ../../components/reconciler
 	k8s.io/api => k8s.io/api v0.17.17
 	k8s.io/apimachinery => k8s.io/apimachinery v0.17.17
 	k8s.io/apiserver => k8s.io/apiserver v0.17.17
---
+++ tools/cli/pkg/command/reconciliations.go
@@ -0,0 +1,43 @@
+package command
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+var (
+	ErrMothershipResponse = errors.New("reconciler error response")
+)
+
+func isErrResponse(statusCode int) bool {
+	return statusCode < http.StatusOK || statusCode >= http.StatusMultipleChoices
+}
+
+func responseErr(resp *http.Response) error {
+	var msg string
+	if err := json.NewDecoder(resp.Body).Decode(&msg); err != nil {
+		msg = errors.Wrap(err, "unexpected error").Error()
+	}
+	return errors.Wrapf(ErrMothershipResponse, "%s %d", msg, resp.StatusCode)
+}
+
+// TODO: Changes in context of NewReconciliationCmd are imlpementing here - https://github.com/kyma-project/control-plane/pull/931
+// NewUpgradeCmd constructs the reconciliation command and all subcommands under the reconciliation command
+func NewReconciliationCmd() *cobra.Command {
+	cobraCmd := &cobra.Command{
+		Use:     "reconciliations",
+		Aliases: []string{"rc"},
+		Short:   "Displays Kyma Reconciliations.",
+		Long: `Displays Kyma Reconciliations and their primary attributes, such as reconciliation-id.
+The command supports filtering Reconciliations based on`,
+	}
+
+	cobraCmd.AddCommand(
+		NewReconciliationEnableCmd(),
+		NewReconciliationDisableCmd(),
+	)
+	return cobraCmd
+}
