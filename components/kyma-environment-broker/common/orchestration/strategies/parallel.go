bd90914990596c3f04f4cfef9665f46b850c5482
vv <weiwei.tang@sap.com>
2021-11-15 14:19:13 +0800

    refactor the keb orchestration operations queuing logic

--- components/kyma-environment-broker/common/orchestration/strategies/parallel.go
+++ components/kyma-environment-broker/common/orchestration/strategies/parallel.go
@@ -2,7 +2,6 @@ package strategies
 
 import (
 	"runtime/debug"
-	"sort"
 	"sync"
 	"time"
 
@@ -20,6 +19,7 @@ type ParallelOrchestrationStrategy struct {
 	mux             sync.RWMutex
 	log             logrus.FieldLogger
 	rescheduleDelay time.Duration
+	processingNum   map[string]int
 }
 
 // NewParallelOrchestrationStrategy returns a new parallel orchestration strategy, which
@@ -31,6 +31,7 @@ func NewParallelOrchestrationStrategy(executor orchestration.OperationExecutor, 
 		wg:              map[string]*sync.WaitGroup{},
 		log:             log,
 		rescheduleDelay: rescheduleDelay,
+		processingNum:   map[string]int{},
 	}
 	if strategy.rescheduleDelay <= 0 {
 		strategy.rescheduleDelay = 24 * time.Hour
@@ -43,30 +44,154 @@ func (p *ParallelOrchestrationStrategy) Execute(operations []orchestration.Runti
 	if len(operations) == 0 {
 		return "", nil
 	}
-	ops := make(chan orchestration.RuntimeOperation, len(operations))
+
 	execID := uuid.New().String()
 	p.mux.Lock()
 	defer p.mux.Unlock()
+	p.processingNum[execID] = len(operations)
 	p.wg[execID] = &sync.WaitGroup{}
 	p.dq[execID] = workqueue.NewDelayingQueue()
 
-	if strategySpec.Schedule == orchestration.MaintenanceWindow {
-		sort.Slice(operations, func(i, j int) bool {
-			return operations[i].MaintenanceWindowBegin.Before(operations[j].MaintenanceWindowBegin)
-		})
-	}
-
-	// Send operations to workers
-	for _, op := range operations {
-		ops <- op
+	for i, op := range operations {
+		duration, err := p.updateMaintenanceWindow(execID, &op, strategySpec)
+		if err != nil {
+			//error when read from storage or update to storage during maintenance window reschedule
+			p.handleRescheduleErrorOperation(execID, &op)
+			p.log.Errorf("while processing operation %s: %v, will reschedule it", op.ID, err)
+		} else {
+			p.dq[execID].AddAfter(&operations[i], duration)
+		}
 	}
 
 	// Create workers
 	for i := 0; i < strategySpec.Parallel.Workers; i++ {
-		p.createWorker(execID, ops, strategySpec)
+		p.createWorker(execID, strategySpec)
 	}
 
+	// p.Wait(execID) // no need as we have waitForCompletion
+
 	return execID, nil
+}
+
+func (p *ParallelOrchestrationStrategy) createWorker(execID string, strategy orchestration.StrategySpec) {
+	p.wg[execID].Add(1)
+
+	go func() {
+		p.scheduleOperationLoop(execID, strategy)
+
+		p.mux.RLock()
+		p.wg[execID].Done()
+		p.mux.RUnlock()
+	}()
+}
+
+func (p *ParallelOrchestrationStrategy) scheduleOperationLoop(execID string, strategy orchestration.StrategySpec) {
+	p.mux.RLock()
+	dq := p.dq[execID]
+	p.mux.RUnlock()
+
+	for {
+		p.mux.RLock()
+		if p.processingNum[execID] <= 0 {
+			p.mux.RUnlock()
+			dq.ShutDown()
+			break
+		}
+		p.mux.RUnlock()
+
+		if dq.ShuttingDown() {
+			break
+		}
+
+		item, shutdown := dq.Get() //other goroutine may hung here
+		if shutdown {
+			p.log.Infof("schedule delaying queue is shutdown")
+			break
+		}
+
+		op := item.(*orchestration.RuntimeOperation)
+
+		// check the window before process
+		duration, err := p.updateMaintenanceWindow(execID, op, strategy)
+		if err != nil {
+			//error when read from storage or update to storage
+			p.handleRescheduleErrorOperation(execID, op)
+			dq.Done(op)
+			continue
+		}
+
+		log := p.log.WithField("operationID", op.ID)
+		if duration <= 0 {
+			log.Infof("operation is scheduled now")
+			p.processOperation(execID, op)
+		} else {
+			log.Infof("operation will be scheduled in %v", duration)
+			dq.AddAfter(op, duration)
+			dq.Done(op)
+		}
+
+	}
+}
+
+func (p *ParallelOrchestrationStrategy) processOperation(execID string, op *orchestration.RuntimeOperation) {
+	id := op.ID
+	log := p.log.WithField("operationID", id)
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Errorf("panic error from process: %v. Stacktrace: %s", err, debug.Stack())
+		}
+		p.dq[execID].Done(op)
+	}()
+
+	when, err := p.executor.Execute(id)
+	if err == nil && when != 0 {
+		log.Infof("Adding %q item after %v", id, when)
+		p.dq[execID].AddAfter(op, when)
+		return
+	}
+	if err != nil {
+		log.Errorf("Error from process: %v", err)
+	}
+
+	p.mux.Lock()
+	p.processingNum[execID]--
+	p.mux.Unlock()
+	log.Infof("Finishing processing operation")
+}
+
+func (p *ParallelOrchestrationStrategy) updateMaintenanceWindow(execID string, op *orchestration.RuntimeOperation, strategy orchestration.StrategySpec) (time.Duration, error) {
+	var duration time.Duration
+	id := op.ID
+
+	switch strategy.Schedule {
+	case orchestration.MaintenanceWindow:
+		// if time window for this operation has finished, we requeue and reprocess on next time window
+		if !op.MaintenanceWindowEnd.IsZero() && op.MaintenanceWindowEnd.Before(time.Now()) {
+			if p.rescheduleDelay > 0 {
+				op.MaintenanceWindowBegin = op.MaintenanceWindowBegin.Add(p.rescheduleDelay)
+				op.MaintenanceWindowEnd = op.MaintenanceWindowEnd.Add(p.rescheduleDelay)
+			} else {
+				currentDay := op.MaintenanceWindowBegin.Weekday()
+				diff := orchestration.NextAvailableDayDiff(currentDay, orchestration.ConvertSliceOfDaysToMap(op.MaintenanceDays))
+				op.MaintenanceWindowBegin = op.MaintenanceWindowBegin.AddDate(0, 0, diff)
+				op.MaintenanceWindowEnd = op.MaintenanceWindowEnd.AddDate(0, 0, diff)
+			}
+
+			err := p.executor.Reschedule(id, op.MaintenanceWindowBegin, op.MaintenanceWindowEnd)
+			//error when read from storage or update to storage
+			if err != nil {
+				errors.Wrap(err, "while rescheduling operation by executor (still continuing with new schedule)")
+				return duration, err
+			}
+		}
+
+		duration = time.Until(op.MaintenanceWindowBegin)
+
+	case orchestration.Immediate:
+	}
+
+	return duration, nil
 }
 
 func (p *ParallelOrchestrationStrategy) Wait(executionID string) {
@@ -91,91 +216,6 @@ func (p *ParallelOrchestrationStrategy) Cancel(executionID string) {
 	}
 }
 
-func (p *ParallelOrchestrationStrategy) createWorker(execID string, ops chan orchestration.RuntimeOperation, strategy orchestration.StrategySpec) {
-	p.wg[execID].Add(1)
-	go func() {
-		moreOperations := true
-		for moreOperations {
-			select {
-			case op := <-ops:
-				err := p.processOperation(op, ops, strategy, execID)
-				if err != nil {
-					p.log.Errorf("while processing operation %s: %v", op.ID, err)
-				}
-			default:
-				p.log.Infof("Idle worker for %s exiting %v", execID)
-				moreOperations = false
-			}
-		}
-		p.mux.RLock()
-		p.wg[execID].Done()
-		p.mux.RUnlock()
-	}()
-}
-
-func (p *ParallelOrchestrationStrategy) processOperation(op orchestration.RuntimeOperation, ops chan orchestration.RuntimeOperation, strategy orchestration.StrategySpec, executionID string) error {
-	exit := false
-	id := op.ID
-	log := p.log.WithField("operationID", id)
-
-	switch strategy.Schedule {
-	case orchestration.MaintenanceWindow:
-		// if time window for this operation has finished, we requeue and reprocess on next time window
-		if !op.MaintenanceWindowEnd.IsZero() && op.MaintenanceWindowEnd.Before(time.Now()) {
-			if p.rescheduleDelay > 0 {
-				op.MaintenanceWindowBegin = op.MaintenanceWindowBegin.Add(p.rescheduleDelay)
-				op.MaintenanceWindowEnd = op.MaintenanceWindowEnd.Add(p.rescheduleDelay)
-			} else {
-				currentDay := op.MaintenanceWindowBegin.Weekday()
-				diff := orchestration.NextAvailableDayDiff(currentDay, orchestration.ConvertSliceOfDaysToMap(op.MaintenanceDays))
-				op.MaintenanceWindowBegin = op.MaintenanceWindowBegin.AddDate(0, 0, diff)
-				op.MaintenanceWindowEnd = op.MaintenanceWindowEnd.AddDate(0, 0, diff)
-			}
-
-			err := p.executor.Reschedule(id, op.MaintenanceWindowBegin, op.MaintenanceWindowEnd)
-			if err != nil {
-				errors.Wrap(err, "while rescheduling operation by executor (still continuing with new schedule)")
-			}
-			ops <- op
-			log.Infof("operation will be rescheduled starting at %v", op.MaintenanceWindowBegin)
-			return err
-		}
-
-		until := time.Until(op.MaintenanceWindowBegin)
-		log.Infof("operation will be scheduled in %v", until)
-		p.dq[executionID].AddAfter(id, until)
-	case orchestration.Immediate:
-		log.Infof("operation is scheduled now")
-		p.dq[executionID].Add(id)
-	}
-
-	for !exit {
-		exit = func() bool {
-			key, quit := p.dq[executionID].Get()
-			if quit {
-				return true
-			}
-			id := key.(string)
-			log = log.WithField("operationID", id)
-			defer func() {
-				if err := recover(); err != nil {
-					log.Errorf("panic error from process: %v. Stacktrace: %s", err, debug.Stack())
-				}
-				p.dq[executionID].Done(key)
-			}()
-
-			when, err := p.executor.Execute(id)
-			if err == nil && when != 0 {
-				log.Infof("Adding %q item after %s", id, when)
-				p.dq[executionID].AddAfter(key, when)
-				return false
-			}
-			if err != nil {
-				log.Errorf("Error from process: %v", err)
-			}
-			return true
-		}()
-	}
-	log.Info("Finishing processing operation")
-	return nil
+func (p *ParallelOrchestrationStrategy) handleRescheduleErrorOperation(execID string, op *orchestration.RuntimeOperation) {
+	p.dq[execID].AddAfter(op, 24*time.Hour)
 }
