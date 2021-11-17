ed9f5a43eae116d1f4ec73b681db42ef2488b33d
Kamil Sputo <sputo.kamil@gmail.com>
2021-11-17 17:35:37 +0100

    Revert "KEB resumes update operations (#1105)"
    This reverts commit e8ce053b0aa4f43c10057e81393e9c8600b708dd.

--- components/kyma-environment-broker/cmd/broker/main.go
+++ components/kyma-environment-broker/cmd/broker/main.go
@@ -347,8 +347,6 @@ func main() {
 		fatalOnError(err)
 		err = processOperationsInProgressByType(internal.OperationTypeDeprovision, db.Operations(), deprovisionQueue, logs)
 		fatalOnError(err)
-		err = processOperationsInProgressByType(internal.OperationTypeUpdate, db.Operations(), updateQueue, logs)
-		fatalOnError(err)
 		err = reprocessOrchestrations(orchestrationExt.UpgradeKymaOrchestration, db.Orchestrations(), db.Operations(), kymaQueue, logs)
 		fatalOnError(err)
 		err = reprocessOrchestrations(orchestrationExt.UpgradeClusterOrchestration, db.Orchestrations(), db.Operations(), clusterQueue, logs)
