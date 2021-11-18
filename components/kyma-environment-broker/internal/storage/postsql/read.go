8f508d0a0cc299de4667575f7bdd97ebde10fb79
Miskiewicz <piotr.miskiewicz@sap.com>
2021-11-18 08:32:12 +0100

    KEB resumes 16 update operations

--- components/kyma-environment-broker/internal/storage/postsql/read.go
+++ components/kyma-environment-broker/internal/storage/postsql/read.go
@@ -235,6 +235,7 @@ func (r readSession) GetNotFinishedOperationsByType(operationType internal.Opera
 		From(OperationTableName).
 		Where(stateCondition).
 		Where(typeCondition).
+		Limit(16).
 		Load(&operations)
 	if err != nil {
 		return nil, dberr.Internal("Failed to get operations: %s", err)
