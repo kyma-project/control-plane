7c58cd5367ea0702916270a6647f7325b78f7c15
Miskiewicz <piotr.miskiewicz@sap.com>
2021-11-22 15:37:25 +0100

    wip

--- components/kyma-environment-broker/cmd/broker/broker_suite_test.go
+++ components/kyma-environment-broker/cmd/broker/broker_suite_test.go
@@ -174,7 +174,6 @@ func NewBrokerSuiteTest(t *testing.T) *BrokerSuiteTest {
 		t:                   t,
 		inputBuilderFactory: inputFactory,
 	}
-	s := smcf.(internal.SMClientFactory)
 
 	ts.CreateAPI(inputFactory, cfg, db, provisioningQueue, deprovisioningQueue, updateQueue, logs)
 
@@ -186,7 +185,7 @@ func NewBrokerSuiteTest(t *testing.T) *BrokerSuiteTest {
 		StatusCheck:        100 * time.Millisecond,
 		UpgradeKymaTimeout: 4 * time.Second,
 	}, 250*time.Millisecond, runtimeVerConfigurator, runtimeResolver, upgradeEvaluationManager,
-		cfg, avs.NewInternalEvalAssistant(cfg.Avs), reconcilerClient, s, inMemoryFs, monitoringClient, logs, cli)
+		cfg, avs.NewInternalEvalAssistant(cfg.Avs), reconcilerClient, smcf, inMemoryFs, monitoringClient, logs, cli)
 
 	clusterQueue := NewClusterOrchestrationProcessingQueue(ctx, db, provisionerClient, eventBroker, inputFactory, &upgrade_cluster.TimeSchedule{
 		Retry:                 10 * time.Millisecond,
@@ -878,16 +877,6 @@ func (s *BrokerSuiteTest) fixExpectedComponentListWithSMProxy(opID string) []rec
 					Value:  false,
 					Secret: false,
 				},
-				//{
-				//	Key:    "grafana.env.GF_AUTH_GENERIC_OAUTH_CLIENT_ID",
-				//	Value:  "cid",
-				//	Secret: true,
-				//},
-				//{
-				//	Key:    "grafana.env.GF_AUTH_GENERIC_OAUTH_CLIENT_SECRET",
-				//	Value:  "csc",
-				//	Secret: true,
-				//},
 			},
 		},
 		{
