85c5a57cc22c657efdad1b299777f193ef1604d8
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2021-11-25 10:56:53 +0100

    Fill KymaConfig after upgrade (#1135)
    * Add method for copying KymaConfig from Cluster struct to RuntimeState
    
    * Fix copying value of *Secret; add test
    
    * Fix format
    
    * Change method to return KymaConfig

--- components/kyma-environment-broker/internal/model.go
+++ components/kyma-environment-broker/internal/model.go
@@ -407,6 +407,45 @@ type RuntimeState struct {
 	ClusterSetup  *reconciler.Cluster           `json:"clusterSetup,omitempty"`
 }
 
+func (r *RuntimeState) GetKymaConfig() gqlschema.KymaConfigInput {
+	if r.ClusterSetup != nil {
+		return r.buildKymaConfigFromClusterSetup()
+	}
+	return r.KymaConfig
+}
+
+func (r *RuntimeState) buildKymaConfigFromClusterSetup() gqlschema.KymaConfigInput {
+	var components []*gqlschema.ComponentConfigurationInput
+	for _, cmp := range r.ClusterSetup.KymaConfig.Components {
+		var config []*gqlschema.ConfigEntryInput
+		for _, cfg := range cmp.Configuration {
+			configEntryInput := &gqlschema.ConfigEntryInput{
+				Key:    cfg.Key,
+				Value:  fmt.Sprint(cfg.Value),
+				Secret: ptr.Bool(cfg.Secret),
+			}
+			config = append(config, configEntryInput)
+		}
+
+		componentConfigurationInput := &gqlschema.ComponentConfigurationInput{
+			Component:     cmp.Component,
+			Namespace:     cmp.Namespace,
+			SourceURL:     &cmp.URL,
+			Configuration: config,
+		}
+		components = append(components, componentConfigurationInput)
+	}
+
+	profile := gqlschema.KymaProfile(r.ClusterSetup.KymaConfig.Profile)
+	kymaConfig := gqlschema.KymaConfigInput{
+		Version:    r.ClusterSetup.KymaConfig.Version,
+		Profile:    &profile,
+		Components: components,
+	}
+
+	return kymaConfig
+}
+
 // OperationStats provide number of operations per type and state
 type OperationStats struct {
 	Provisioning   map[domain.LastOperationState]int
--- components/kyma-environment-broker/internal/orchestration/handlers/orchestration_handler_test.go
+++ components/kyma-environment-broker/internal/orchestration/handlers/orchestration_handler_test.go
@@ -7,9 +7,14 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/common/orchestration"
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal"
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/broker"
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/ptr"
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/reconciler"
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/storage"
+	"github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
 	"github.com/pivotal-cf/brokerapi/v8/domain"
 	"github.com/stretchr/testify/assert"
 
@@ -292,4 +297,179 @@ func TestStatusHandler_AttachRoutes(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, orchestration.Canceling, o.State)
 	})
+
+	t.Run("Kyma 2.0 upgrade operation", func(t *testing.T) {
+		// given
+		db := storage.NewMemoryStorage()
+
+		instanceID := "instanceID"
+		provisioningOp1ID := "provisioningOp1ID"
+
+		provisioningOp1 := internal.ProvisioningOperation{
+			Operation: internal.Operation{
+				ID:         provisioningOp1ID,
+				InstanceID: instanceID,
+			},
+		}
+
+		err := db.Operations().InsertProvisioningOperation(provisioningOp1)
+		require.NoError(t, err)
+
+		orchestration1ID := "ochestration1ID"
+		orchestration1 := internal.Orchestration{
+			OrchestrationID: orchestration1ID,
+			Type:            orchestration.UpgradeKymaOrchestration,
+		}
+
+		err = db.Orchestrations().Insert(orchestration1)
+		require.NoError(t, err)
+
+		upgradeKymaOp1ID := "upgradeKymaOperation1ID"
+		upgradeKymaOp1 := internal.UpgradeKymaOperation{
+			Operation: internal.Operation{
+				ID:              upgradeKymaOp1ID,
+				InstanceID:      instanceID,
+				OrchestrationID: orchestration1ID,
+				ProvisioningParameters: internal.ProvisioningParameters{
+					PlanID: broker.AzurePlanID,
+				},
+			},
+			RuntimeOperation: orchestration.RuntimeOperation{
+				ID: upgradeKymaOp1ID,
+			},
+		}
+
+		err = db.Operations().InsertUpgradeKymaOperation(upgradeKymaOp1)
+		require.NoError(t, err)
+
+		runtimeStateWithClusterSetupID := "runtimeStateWithClusterSetupID"
+		runtimeStateWithClusterSetup := internal.RuntimeState{
+			ID:          runtimeStateWithClusterSetupID,
+			RuntimeID:   uuid.NewString(),
+			OperationID: upgradeKymaOp1ID,
+			ClusterSetup: &reconciler.Cluster{
+				Cluster: uuid.NewString(),
+				KymaConfig: reconciler.KymaConfig{
+					Version: "2.0.0",
+					Profile: string(gqlschema.KymaProfileProduction),
+					Components: []reconciler.Component{
+						{
+							URL:       "component1URL.local",
+							Component: "component1",
+							Namespace: "test",
+							Configuration: []reconciler.Configuration{
+								{
+									Key:    "key1",
+									Value:  "value1",
+									Secret: false,
+								},
+								{
+									Key:    "key2",
+									Value:  "value2",
+									Secret: true,
+								},
+							},
+						},
+					},
+					Administrators: []string{"admin1@test.com", "admin2@test.com"},
+				},
+			},
+		}
+
+		err = db.RuntimeStates().Insert(runtimeStateWithClusterSetup)
+		require.NoError(t, err)
+
+		logs := logrus.New()
+		kymaHandler := NewOrchestrationStatusHandler(db.Operations(), db.Orchestrations(), db.RuntimeStates(), 100, logs)
+
+		urlPath := fmt.Sprintf("/orchestrations/%s/operations", orchestration1ID)
+		req, err := http.NewRequest("GET", urlPath, nil)
+		require.NoError(t, err)
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+		kymaHandler.AttachRoutes(router)
+
+		// when
+		router.ServeHTTP(rr, req)
+
+		// then
+		require.Equal(t, http.StatusOK, rr.Code)
+
+		var opResponseList orchestration.OperationResponseList
+
+		err = json.Unmarshal(rr.Body.Bytes(), &opResponseList)
+		require.NoError(t, err)
+
+		assert.Len(t, opResponseList.Data, 1)
+		assert.Equal(t, 1, opResponseList.TotalCount)
+		assert.Equal(t, 1, opResponseList.Count)
+
+		// given
+		urlPath = fmt.Sprintf("/orchestrations/%s/operations/%s", orchestration1ID, upgradeKymaOp1ID)
+		req, err = http.NewRequest(http.MethodGet, urlPath, nil)
+		require.NoError(t, err)
+
+		rr = httptest.NewRecorder()
+
+		// when
+		router.ServeHTTP(rr, req)
+
+		// then
+		require.Equal(t, http.StatusOK, rr.Code)
+
+		var opDetailResponse orchestration.OperationDetailResponse
+		err = json.Unmarshal(rr.Body.Bytes(), &opDetailResponse)
+		require.NoError(t, err)
+
+		expectedKymaConfig := gqlschema.KymaConfigInput{
+			Version: "2.0.0",
+			Profile: (*gqlschema.KymaProfile)(ptr.String("Production")),
+			Components: []*gqlschema.ComponentConfigurationInput{
+				{
+					Component: "component1",
+					Namespace: "test",
+					SourceURL: ptr.String("component1URL.local"),
+					Configuration: []*gqlschema.ConfigEntryInput{
+						{
+							Key:    "key1",
+							Value:  "value1",
+							Secret: ptr.Bool(false),
+						},
+						{
+							Key:    "key2",
+							Value:  "value2",
+							Secret: ptr.Bool(true),
+						},
+					},
+				},
+			},
+		}
+
+		assert.Equal(t, opDetailResponse.OrchestrationID, orchestration1ID)
+		assert.Equal(t, opDetailResponse.OperationID, upgradeKymaOp1ID)
+		assert.NotNil(t, opDetailResponse.KymaConfig)
+		assertKymaConfigValues(t, expectedKymaConfig, *opDetailResponse.KymaConfig)
+	})
+}
+
+func assertKymaConfigValues(t *testing.T, expected, actual gqlschema.KymaConfigInput) {
+	assert.Equal(t, expected.Version, actual.Version)
+	assert.Equal(t, *expected.Profile, *actual.Profile)
+	if len(expected.Components) > 0 {
+		for i, cmp := range expected.Components {
+			if len(cmp.Configuration) > 0 {
+				for j, cfg := range cmp.Configuration {
+					assert.Equal(t, cfg.Value, actual.Components[i].Configuration[j].Value)
+					assert.Equal(t, cfg.Key, actual.Components[i].Configuration[j].Key)
+					assert.Equal(t, *cfg.Secret, *actual.Components[i].Configuration[j].Secret)
+				}
+			}
+			assert.Equal(t, cmp.Component, actual.Components[i].Component)
+			assert.Equal(t, cmp.Namespace, actual.Components[i].Namespace)
+			if cmp.SourceURL != nil {
+				assert.Equal(t, *cmp.SourceURL, *actual.Components[i].SourceURL)
+			}
+		}
+	}
 }
