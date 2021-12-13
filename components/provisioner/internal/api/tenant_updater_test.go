ee4b951ec6ce926c8edc48c677642018e401091b
Mariusz Szymanski <mariusz.szymanski@sap.com>
2021-12-13 12:13:01 +0100

    fix test

--- components/provisioner/internal/api/tenant_updater_test.go
+++ components/provisioner/internal/api/tenant_updater_test.go
@@ -42,7 +42,7 @@ func TestTenantUpdater_GetAndUpdateTenant(t *testing.T) {
 		rwsMock := &mocks.ReadWriteSession{}
 		tenantUpdater := NewTenantUpdater(rwsMock)
 
-		rwsMock.On("GetTenant", ctx).Return(dbTenant, nil)
+		rwsMock.On("GetTenant", runtimeId).Return(dbTenant, nil)
 		rwsMock.On("UpdateTenant", runtimeId, newTenant).Return(nil)
 
 		err := tenantUpdater.GetAndUpdateTenant(runtimeId, ctx)
@@ -58,7 +58,7 @@ func TestTenantUpdater_GetAndUpdateTenant(t *testing.T) {
 		rwsMock := &mocks.ReadWriteSession{}
 		tenantUpdater := NewTenantUpdater(rwsMock)
 
-		rwsMock.On("GetTenant", ctx).Return(dbTenant, nil)
+		rwsMock.On("GetTenant", runtimeId).Return(dbTenant, nil)
 
 		err := tenantUpdater.GetAndUpdateTenant(runtimeId, ctx)
 		require.NoError(t, err)
