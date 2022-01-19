f4acd70b2d4611f7ca7b400103fc26ffe04984eb
ralikio <74771103+ralikio@users.noreply.github.com>
2022-01-19 12:04:04 +0100

    Removed Unnecessary Import

--- tests/e2e/provisioning/test/test_suite.go
+++ tests/e2e/provisioning/test/test_suite.go
@@ -24,8 +24,6 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
-
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 type Config struct {
