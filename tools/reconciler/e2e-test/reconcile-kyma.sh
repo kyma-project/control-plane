493b6113fa765d87f26093f7b4ddef037b04f9a0
Xin Ruan <xin.ruan@sap.com>
2022-02-02 15:51:28 +0100

    improve wait_until_kyma_installed

--- tools/reconciler/e2e-test/reconcile-kyma.sh
+++ tools/reconciler/e2e-test/reconcile-kyma.sh
@@ -8,9 +8,8 @@
 set -e
 
 readonly RECONCILER_HOST="http://reconciler-mothership-reconciler.reconciler"
-readonly RECONCILER_TIMEOUT=1200 # in secs
 readonly RECONCILER_DELAY=15 # in secs
-
+readonly RECONCILER_STATUS_RETRY=80
 readonly RECONCILE_API="${RECONCILER_HOST}/v1/clusters"
 readonly RECONCILE_PAYLOAD_FILE="/tmp/body.json"
 
@@ -20,7 +19,7 @@ readonly RECONCILE_PAYLOAD_FILE="/tmp/body.json"
 
 # Waits until Kyma reconciliation is in ready state
 function wait_until_kyma_installed() {
-  iterationsLeft=$(( RECONCILER_TIMEOUT/RECONCILER_DELAY ))
+  iterationsLeft=${RECONCILER_STATUS_RETRY}
   while : ; do
     reconcileStatusResponse=$(curl -sL "${RECONCILE_STATUS_URL}")
     status=$(echo "${reconcileStatusResponse}" | jq -r .status)
@@ -36,7 +35,7 @@ function wait_until_kyma_installed() {
       exit 1
     fi
 
-    if [ "$RECONCILER_TIMEOUT" -ne 0 ] && [ "$iterationsLeft" -le 0 ]; then
+    if [ "$iterationsLeft" -le 0 ]; then
       echo "reconcileStatusResponse: ${reconcileStatusResponse}"
       echo "Timeout reached on Kyma reconciliation. Exiting"
       exit 1
