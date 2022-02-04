6166b5c6126885da3630acaf94977cc27a31deb0
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2022-02-04 11:20:04 +0100

    Log incoming requests in KEB (#1324)
    * Log incoming requests in KEB
    
    * bump
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

---
+++ components/kyma-environment-broker/internal/broker/logging_test.go
@@ -0,0 +1,29 @@
+package broker
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHideSensitiveDataFromContext(t *testing.T) {
+	// This test is just a way to show, how the hideSensitiveDataFromContext works
+	in := map[string]interface{}{
+		"password": "pa2345",
+		"username": "johnsmith",
+		"subobject": map[string]interface{}{
+			"secret": "val",
+			"sm_url": "https://sm.url.com",
+		},
+		"isValid": true,
+	}
+
+	// when
+	out := hideSensitiveDataFromContext(in)
+
+	d, err := json.Marshal(out)
+	require.NoError(t, err)
+	assert.Equal(t, `{"isValid":true,"password":"*****","subobject":{"secret":"*****","sm_url":"https://sm.url.com"},"username":"*****"}`, string(d))
+}
