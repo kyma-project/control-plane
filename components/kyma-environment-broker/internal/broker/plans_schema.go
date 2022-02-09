3881b1e246d595f597c18519b6f4dac2d74b0ed8
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2022-02-09 09:23:02 +0100

    Control Order (#1305)
    * Control Order
    
    * Missing Tests
    
    * Corrected Properties Assignment
    
    * Tests Correction
    
    * Moved Control Fnc & Refactoring
    
    * Code Review Remarks & Refactoring
    
    * Corrected JSON Tests
    
    * Corrected "type" Order
    
    * Corrected Empty Update Parameters
    
    * New Test Output - Different Marshaling Ordering
    
    * Corrected Invalid Name
    
    * Removing Defaults on Update
    
    * Removed Invalid Require Fields from Test Data

--- components/kyma-environment-broker/internal/broker/plans_schema.go
+++ components/kyma-environment-broker/internal/broker/plans_schema.go
@@ -1,5 +1,7 @@
 package broker
 
+import "encoding/json"
+
 type RootSchema struct {
 	Schema string `json:"$schema"`
 	Type
@@ -13,14 +15,12 @@ type RootSchema struct {
 }
 
 type ProvisioningProperties struct {
-	Name           NameType  `json:"name"`
-	Region         *Type     `json:"region,omitempty"`
-	MachineType    *Type     `json:"machineType,omitempty"`
-	AutoScalerMin  *Type     `json:"autoScalerMin,omitempty"`
-	AutoScalerMax  *Type     `json:"autoScalerMax,omitempty"`
-	ZonesCount     *Type     `json:"zonesCount,omitempty"`
-	OIDC           *OIDCType `json:"oidc,omitempty"`
-	Administrators *Type     `json:"administrators,omitempty"`
+	UpdateProperties
+
+	Name        NameType `json:"name"`
+	Region      *Type    `json:"region,omitempty"`
+	MachineType *Type    `json:"machineType,omitempty"`
+	ZonesCount  *Type    `json:"zonesCount,omitempty"`
 }
 
 type UpdateProperties struct {
@@ -28,6 +28,11 @@ type UpdateProperties struct {
 	AutoScalerMax  *Type     `json:"autoScalerMax,omitempty"`
 	OIDC           *OIDCType `json:"oidc,omitempty"`
 	Administrators *Type     `json:"administrators,omitempty"`
+}
+
+func (up *UpdateProperties) IncludeAdditional() {
+	up.OIDC = NewOIDCSchema()
+	up.Administrators = AdministratorsProperty()
 }
 
 type OIDCProperties struct {
@@ -92,8 +97,24 @@ func NameProperty() NameType {
 
 // NewProvisioningProperties creates a new properties for different plans
 // Note that the order of properties will be the same in the form on the website
-func NewProvisioningProperties(machineTypes []string, regions []string) ProvisioningProperties {
-	return ProvisioningProperties{
+func NewProvisioningProperties(machineTypes []string, regions []string, update bool) ProvisioningProperties {
+
+	properties := ProvisioningProperties{
+		UpdateProperties: UpdateProperties{
+			AutoScalerMin: &Type{
+				Type:        "integer",
+				Minimum:     2,
+				Default:     2,
+				Description: "Specifies the minimum number of virtual machines to create",
+			},
+			AutoScalerMax: &Type{
+				Type:        "integer",
+				Minimum:     2,
+				Maximum:     40,
+				Default:     10,
+				Description: "Specifies the maximum number of virtual machines to create",
+			},
+		},
 		Name: NameProperty(),
 		Region: &Type{
 			Type: "string",
@@ -103,24 +124,18 @@ func NewProvisioningProperties(machineTypes []string, regions []string) Provisio
 			Type: "string",
 			Enum: ToInterfaceSlice(machineTypes),
 		},
-		AutoScalerMin: &Type{
-			Type:        "integer",
-			Minimum:     2,
-			Default:     2,
-			Description: "Specifies the minimum number of virtual machines to create",
-		},
-		AutoScalerMax: &Type{
-			Type:        "integer",
-			Minimum:     2,
-			Maximum:     40,
-			Default:     10,
-			Description: "Specifies the maximum number of virtual machines to create",
-		},
 	}
+
+	if update {
+		properties.AutoScalerMax.Default = nil
+		properties.AutoScalerMin.Default = nil
+	}
+
+	return properties
 }
 
-func NewOIDCSchema() OIDCType {
-	return OIDCType{
+func NewOIDCSchema() *OIDCType {
+	return &OIDCType{
 		Type: Type{Type: "object", Description: "OIDC configuration"},
 		Properties: OIDCProperties{
 			ClientID:       Type{Type: "string", Description: "The client ID for the OpenID Connect client."},
@@ -140,34 +155,37 @@ func NewOIDCSchema() OIDCType {
 	}
 }
 
-func NewSchema(properties ProvisioningProperties, controlsOrder []string) RootSchema {
-	return RootSchema{
+func NewSchema(properties interface{}, update bool) *RootSchema {
+	schema := &RootSchema{
 		Schema: "http://json-schema.org/draft-04/schema#",
 		Type: Type{
 			Type: "object",
 		},
-		Properties:    properties,
-		ShowFormView:  true,
-		Required:      []string{"name"},
-		ControlsOrder: controlsOrder,
+		Properties:   properties,
+		ShowFormView: true,
+		Required:     []string{"name"},
 	}
+
+	if update {
+		schema.Required = []string{}
+	}
+
+	return schema
 }
 
-func NewUpdateSchema(properties UpdateProperties) RootSchema {
-	return RootSchema{
-		Schema: "http://json-schema.org/draft-04/schema#",
-		Type: Type{
-			Type: "object",
-		},
-		Properties:    properties,
-		ShowFormView:  true,
-		Required:      []string{},
-		ControlsOrder: []string{},
+func unmarshalOrPanic(from, to interface{}) interface{} {
+	if from != nil {
+		marshaled := Marshal(from)
+		err := json.Unmarshal(marshaled, to)
+		if err != nil {
+			panic(err)
+		}
 	}
+	return to
 }
 
 func DefaultControlsOrder() []string {
-	return []string{"name", "region", "machineType", "autoScalerMin", "autoScalerMax"}
+	return []string{"name", "region", "machineType", "autoScalerMin", "autoScalerMax", "zonesCount", "oidc", "administrators"}
 }
 
 func ToInterfaceSlice(input []string) []interface{} {
--- components/kyma-environment-broker/internal/broker/services_test.go
+++ components/kyma-environment-broker/internal/broker/services_test.go
@@ -79,6 +79,42 @@ func TestServices_Services(t *testing.T) {
 		assertPlansContainPropertyInSchemas(t, services[0], "oidc")
 		assertPlansContainPropertyInSchemas(t, services[0], "administrators")
 	})
+
+	t.Run("should return sync control orders", func(t *testing.T) {
+		// given
+		var (
+			name       = "testServiceName"
+			supportURL = "example.com/support"
+		)
+
+		cfg := broker.Config{
+			EnablePlans:                     []string{"gcp", "azure", "openstack", "aws", "free", "azure_ha", "aws_ha"},
+			IncludeAdditionalParamsInSchema: true,
+		}
+		servicesConfig := map[string]broker.Service{
+			broker.KymaServiceName: {
+				Metadata: broker.ServiceMetadata{
+					DisplayName: name,
+					SupportUrl:  supportURL,
+				},
+			},
+		}
+		servicesEndpoint := broker.NewServices(cfg, servicesConfig, logrus.StandardLogger())
+
+		// when
+		services, err := servicesEndpoint.Services(context.TODO())
+
+		// then
+		require.NoError(t, err)
+		assert.Len(t, services, 1)
+		assert.Len(t, services[0].Plans, 7)
+
+		assert.Equal(t, name, services[0].Metadata.DisplayName)
+		assert.Equal(t, supportURL, services[0].Metadata.SupportUrl)
+
+		assertPlansContainPropertyInSchemas(t, services[0], "oidc")
+		assertPlansContainPropertyInSchemas(t, services[0], "administrators")
+	})
 }
 
 func assertPlansContainPropertyInSchemas(t *testing.T, service domain.Service, property string) {
@@ -89,7 +125,7 @@ func assertPlansContainPropertyInSchemas(t *testing.T, service domain.Service, p
 }
 
 func assertPlanContainsPropertyInCreateSchema(t *testing.T, plan domain.ServicePlan, property string) {
-	properties := plan.Schemas.Instance.Create.Parameters["properties"]
+	properties := plan.Schemas.Instance.Create.Parameters[broker.PropertiesKey]
 	propertiesMap := properties.(map[string]interface{})
 	if _, exists := propertiesMap[property]; !exists {
 		t.Errorf("plan %s does not contain %s property in Create schema", plan.Name, property)
@@ -97,7 +133,7 @@ func assertPlanContainsPropertyInCreateSchema(t *testing.T, plan domain.ServiceP
 }
 
 func assertPlanContainsPropertyInUpdateSchema(t *testing.T, plan domain.ServicePlan, property string) {
-	properties := plan.Schemas.Instance.Update.Parameters["properties"]
+	properties := plan.Schemas.Instance.Update.Parameters[broker.PropertiesKey]
 	propertiesMap := properties.(map[string]interface{})
 	if _, exists := propertiesMap[property]; !exists {
 		t.Errorf("plan %s does not contain %s property in Update schema", plan.Name, property)
