1934b1a72ed92ac5a3e30df0806264b3fee27f3d
Miskiewicz <piotr.miskiewicz@sap.com>
2022-02-11 08:54:10 +0100

    Initial commit of the ers cli tool

---
+++ tools/cli/cmd/ers/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/kyma-project/control-plane/tools/cli/pkg/ers/command"
+)
+
+
+/*
+
+Run:
+go run cmd/ers/main.go
+ */
+func main() {
+	setupCloseHandler()
+	cmd := command.New()
+
+	err := cmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+
+}
+
+func setupCloseHandler() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-c
+		fmt.Printf("\r- Signal '%v' received from Terminal. Exiting...\n ", sig)
+		os.Exit(0)
+	}()
+}
---
+++ tools/cli/pkg/ers/command/instances.go
@@ -0,0 +1,42 @@
+package command
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+)
+
+func NewInstancesCommand() *cobra.Command {
+
+	cmd := &cobra.Command {
+		Use: "instances",
+		Short:   "Displays ERS instances.",
+		Long: `Displays most important information about ERS instances.`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// TODO: this is an example of a call to ERS
+
+			url := GlobalOpts.ErsUrl() + "provisioning/v1/kyma/environments?page=0&size=5"
+
+			resp, err := ErsHttpClient.Get(url)
+			if err != nil {
+				fmt.Println("Request error: ", err)
+				return err
+			}
+			defer func() {
+				resp.Body.Close()
+			}()
+
+			fmt.Println("status: ", resp.StatusCode, resp.Status)
+
+			d, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println("Read error:", err)
+				return err
+			}
+			fmt.Println(string(d))
+			return nil
+		},
+	}
+
+	return cmd
+}
