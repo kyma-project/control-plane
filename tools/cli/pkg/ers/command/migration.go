e8e4f77c05d8aa7c434810cf851af9602b865fe2
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2022-02-15 10:31:10 +0100

    Initial commit of the ers cli tool (#1350)
    * Initial commit of the ers cli tool
    
    * wip

---
+++ tools/cli/pkg/ers/command/migration.go
@@ -0,0 +1,13 @@
+package command
+
+import "github.com/spf13/cobra"
+
+func NewMigrationCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "migrate",
+		Short: "Triggers SC migration.",
+		Long:  `Triggers SC migration.`,
+	}
+
+	return cmd
+}
---
+++ tools/cli/pkg/ers/command/root.go
@@ -0,0 +1,71 @@
+package command
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/kyma-project/control-plane/tools/cli/pkg/logger"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+func New() *cobra.Command {
+	cobra.OnInitialize(initConfig)
+	cmd := &cobra.Command{
+		Use:          "ers",
+		Short:        "ERS operations tool for Kyma Runtimes.",
+		Long:         "The ers tool provides commands to play with ERS API for Service Catalog migration.",
+		Version:      "N/A",
+		SilenceUsage: true,
+	}
+
+	cmd.AddCommand(NewInstancesCommand(), NewMigrationCommand())
+
+	cmd.PersistentFlags().StringVar(&configPath, "config", os.Getenv(configEnv), "Path to the KCP CLI config file. Can also be set using the KCPCONFIG environment variable. Defaults to $HOME/.kcp/config.yaml .")
+	SetGlobalOpts(cmd)
+	logger.AddFlags(cmd.PersistentFlags())
+
+	return cmd
+}
+
+func initConfig() {
+	// If config file is set via flags or ENV, use that path,
+	// otherwise try to load the config from $HOME/{configDir}/config.yaml
+	if configPath != "" {
+		viper.SetConfigFile(configPath)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		configPath = fmt.Sprintf("%s/%s", home, configDir)
+		viper.AddConfigPath(configPath)
+		viper.SetConfigName("config")
+	}
+	viper.SetConfigType("yaml")
+	replacer := strings.NewReplacer("-", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.SetEnvPrefix("ERS")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+
+	// Ignore when config file is not found to allow config parameters being passed as flags or environment variables
+	// Panic otherwise
+	if _, ok := err.(viper.ConfigFileNotFoundError); err != nil && !ok {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	config := clientcredentials.Config{
+		ClientID:     GlobalOpts.ClientID(),
+		ClientSecret: GlobalOpts.ClientSecret(),
+		TokenURL:     GlobalOpts.OauthUrl(),
+	}
+
+	// create a shared ERS HTTP client which does the oauth flow
+	ErsHttpClient = config.Client(context.Background())
+}
