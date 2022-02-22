5a650716f070e3b7b15961a6f059996662409244
Raymond Pinto <45314732+raypinto@users.noreply.github.com>
2022-02-22 14:14:50 +0100

    Bump dependencies and fix tests on KMC (#1372)
    * Bump gardener-extension-provider aws and azure
    
    * Updated k8s client version
    
    * Updated other dependencies
    
    * Fix fake dynamicClient issue
    
    * Use 0.22 version of client-go
    
    * Fix imports
    
    * Bump golang version
    
    * Update values.yaml

--- components/kyma-metrics-collector/pkg/skr/svc/fake.go
+++ components/kyma-metrics-collector/pkg/skr/svc/fake.go
@@ -3,6 +3,7 @@ package svc
 import (
 	"github.com/kyma-project/control-plane/components/kyma-metrics-collector/pkg/gardener/commons"
 	kmctesting "github.com/kyma-project/control-plane/components/kyma-metrics-collector/pkg/testing"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 )
 
@@ -15,7 +16,11 @@ func (fakeSvcClient FakeSvcClient) NewClient(string) (*Client, error) {
 		return nil, err
 	}
 
-	dynamicClient := dynamicfake.NewSimpleDynamicClient(scheme, nodeList)
+	dynamicClient := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(scheme,
+		map[schema.GroupVersionResource]string{
+			{Group: "core", Version: "v1", Resource: "Node"}: "NodeList",
+		}, nodeList)
+
 	nsResourceClient := dynamicClient.Resource(GroupVersionResource())
 	return &Client{Resource: nsResourceClient}, nil
 }
