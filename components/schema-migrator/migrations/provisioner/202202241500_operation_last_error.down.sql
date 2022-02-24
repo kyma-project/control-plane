d0b9621a5028224355e4ffa74a57bb68fe5ca105
vv <weiwei.tang@sap.com>
2022-02-24 15:31:59 +0800

    correct table name

--- components/schema-migrator/migrations/provisioner/202202241500_operation_last_error.down.sql
+++ components/schema-migrator/migrations/provisioner/202202241500_operation_last_error.down.sql
@@ -1,4 +1,4 @@
-ALTER TABLE operations
+ALTER TABLE operation
     DROP COLUMN error_msg,
     DROP COLUMN error_reason,
     DROP COLUMN error_component;
--- components/schema-migrator/migrations/provisioner/202202241500_operation_last_error.up.sql
+++ components/schema-migrator/migrations/provisioner/202202241500_operation_last_error.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE operations
+ALTER TABLE operation
     ADD COLUMN error_msg text,
     ADD COLUMN error_reason text,
     ADD COLUMN error_component text;
