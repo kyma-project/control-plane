7a2edf56bb8b30124981ce9069d6f48b343e89e0
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2022-03-04 11:09:02 +0100

    Resuse cluster name for reconciler calls (#1418)
    * Resuse cluster name for reconciler calls
    
    * wip

--- components/kyma-environment-broker/internal/process/input/input.go
+++ components/kyma-environment-broker/internal/process/input/input.go
@@ -61,6 +61,7 @@ type RuntimeInput struct {
 	kubeconfig        string
 	shootDomain       string
 	shootDnsProviders gardener.DNSProvidersData
+	clusterName       string
 }
 
 func (r *RuntimeInput) EnableOptionalComponent(componentName string) internal.ProvisionerInputCreator {
@@ -111,6 +112,13 @@ func (r *RuntimeInput) SetRuntimeID(runtimeID string) internal.ProvisionerInputC
 
 func (r *RuntimeInput) SetKubeconfig(kubeconfig string) internal.ProvisionerInputCreator {
 	r.kubeconfig = kubeconfig
+	return r
+}
+
+func (r *RuntimeInput) SetClusterName(name string) internal.ProvisionerInputCreator {
+	if name != "" {
+		r.clusterName = name
+	}
 	return r
 }
 
@@ -574,6 +582,11 @@ func (r *RuntimeInput) applyGlobalConfigurationForUpgradeRuntime() error {
 }
 
 func (r *RuntimeInput) adjustRuntimeName() error {
+	// if the cluster name was created before, it must be used instead of generating one
+	if r.clusterName != "" {
+		r.provisionRuntimeInput.RuntimeInput.Name = r.clusterName
+		return nil
+	}
 
 	reg, err := regexp.Compile("[^a-zA-Z0-9\\-\\.]+")
 	if err != nil {
