f390a63d9ec129df9cc7278e075d4a5c81bbcdb4
VladislavPaskar <vladislav.paskar@sap.com>
2022-03-16 13:22:34 +0100

    use mounted secret for EDP token instead of using the env var

--- components/kyma-metrics-collector/pkg/edp/config.go
+++ components/kyma-metrics-collector/pkg/edp/config.go
@@ -3,8 +3,8 @@ package edp
 import "time"
 
 type Config struct {
-	URL               string        `envconfig:"EDP_URL" default:"https://input.yevents.io" required:"true"`
-	Token             string        `envconfig:"EDP_TOKEN" required:"true"`
+	URL               string `envconfig:"EDP_URL" default:"https://input.yevents.io" required:"true"`
+	Token             string
 	Namespace         string        `envconfig:"EDP_NAMESPACE" default:"kyma-dev" required:"true"`
 	DataStreamName    string        `envconfig:"EDP_DATASTREAM_NAME" default:"consumption-metrics" required:"true"`
 	DataStreamVersion string        `envconfig:"EDP_DATASTREAM_VERSION" default:"1" required:"true"`
