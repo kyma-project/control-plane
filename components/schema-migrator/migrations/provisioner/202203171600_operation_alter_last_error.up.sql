71221f5c802a826dc00be2ce8b189708d3ea7c3d
vv <weiwei.tang@sap.com>
2022-03-17 16:43:50 +0800

    update the old last error to empty ''

--- components/schema-migrator/migrations/provisioner/202203171600_operation_alter_last_error.up.sql
+++ components/schema-migrator/migrations/provisioner/202203171600_operation_alter_last_error.up.sql
@@ -2,3 +2,7 @@ ALTER TABLE operation
     ALTER COLUMN err_message SET DEFAULT '',
     ALTER COLUMN reason SET DEFAULT '',
     ALTER COLUMN component SET DEFAULT '';
+
+UPDATE operation SET err_message = '' WHERE err_message IS NULL;
+UPDATE operation SET reason = '' WHERE reason IS NULL;
+UPDATE operation SET component = '' WHERE component IS NULL;
