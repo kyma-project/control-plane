842f1449574deda2f306b467114ea5f197a7c4d6
Wojciech Nawa <wojciech.nawa@sap.com>
2022-03-23 12:43:23 +0100

    Correct DNS Config migration
    `NOT NULL` instead of `DEFAULT`

--- components/schema-migrator/migrations/provisioner/202203222101_add_dns_config.up.sql
+++ components/schema-migrator/migrations/provisioner/202203222101_add_dns_config.up.sql
@@ -13,7 +13,7 @@ CREATE TABLE dns_providers
     `id` uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
     `dns_config_id` uuid NOT NULL,
     `domains_include` varchar(256) NOT NULL,
-    `is_primary` boolean DEFAULT false,
+    `is_primary` boolean NOT NULL,
     `secret_name` varchar(256) NOT NULL,
     `type` varchar(256) NOT NULL,
     foreign key (dns_config_id) REFERENCES dns_config (id) ON DELETE CASCADE
