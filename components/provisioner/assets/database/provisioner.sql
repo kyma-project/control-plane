0a6f4b3425257e28aefbfb40ee8c4fd160d6c9c0
Wojciech Nawa <wojciech.nawa@sap.com>
2022-03-23 13:18:02 +0100

    Remove backticks
    Something is not yes, and I am trying stuff out

--- components/provisioner/assets/database/provisioner.sql
+++ components/provisioner/assets/database/provisioner.sql
@@ -199,9 +199,9 @@ type DNSProvider struct {
 -- DNS config
 CREATE TABLE dns_config
 (
-    `id` uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
-    `domain` varchar(256) NOT NULL,
-    `gardener_config_id` uuid NOT NULL,
+    id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
+    domain varchar(256) NOT NULL,
+    gardener_config_id uuid NOT NULL,
     unique(gardener_config_id),
     foreign key (gardener_config_id) REFERENCES gardener_config (id) ON DELETE CASCADE
 );
@@ -209,11 +209,11 @@ CREATE TABLE dns_config
 -- DNS config
 CREATE TABLE dns_providers
 (
-    `id` uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
-    `dns_config_id` uuid NOT NULL,
-    `domains_include` varchar(256) NOT NULL,
-    `is_primary` boolean NOT NULL,
-    `secret_name` varchar(256) NOT NULL,
-    `type` varchar(256) NOT NULL,
+    id uuid PRIMARY KEY CHECK (id <> '00000000-0000-0000-0000-000000000000'),
+    dns_config_id uuid NOT NULL,
+    domains_include varchar(256) NOT NULL,
+    is_primary boolean NOT NULL,
+    secret_name varchar(256) NOT NULL,
+    type varchar(256) NOT NULL,
     foreign key (dns_config_id) REFERENCES dns_config (id) ON DELETE CASCADE
 );
