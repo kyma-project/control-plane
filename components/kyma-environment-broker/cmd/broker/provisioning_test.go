645366106abd3617e2d8232e5782186ff92d0184
Marcin Szwed <marcin.szwed@sap.com>
2022-05-02 08:43:08 +0200

    Remove unnecessary extractShootNameAndDomain() method and tests

--- components/kyma-environment-broker/cmd/broker/provisioning_test.go
+++ components/kyma-environment-broker/cmd/broker/provisioning_test.go
@@ -426,27 +426,6 @@ func TestProvisioning_ClusterParameters(t *testing.T) {
 	}
 }
 
-func TestUnsuspensionWithoutShootName(t *testing.T) {
-	// given
-	suite := NewProvisioningSuite(t)
-
-	// when
-	// Create an instance, succeeded suspension operation in the past and a pending unsuspension operation
-	unsuspensionOperationID := suite.CreateUnsuspension(RuntimeOptions{})
-
-	// then
-	suite.WaitForProvisioningState(unsuspensionOperationID, domain.InProgress)
-	suite.AssertProvisionerStartedProvisioning(unsuspensionOperationID)
-
-	// when
-	suite.FinishProvisioningOperationByProvisioner(unsuspensionOperationID)
-
-	// then
-	suite.WaitForProvisioningState(unsuspensionOperationID, domain.Succeeded)
-	suite.AssertAllStagesFinished(unsuspensionOperationID)
-	suite.AssertProvisioningRequest()
-}
-
 func TestProvisioning_RuntimeOverrides(t *testing.T) {
 
 	t.Run("should apply overrides to default runtime version", func(t *testing.T) {
