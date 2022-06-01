a516dc9714ba2ae5e37b61568c246b1574af31f2
Jan Wozniak <wozniak.jan@gmail.com>
2022-06-01 10:31:46 +0200

    Configure enterprise nework filter for SKRs from ERS context (#1735)
    * Bump kyma_environment_broker images to PR-1735
    
    * Configure enterprise nework filter for SKRs from ERS context
    
    * Add tests for network filter

--- components/kyma-environment-broker/internal/process/update/upgrade_shoot_step.go
+++ components/kyma-environment-broker/internal/process/update/upgrade_shoot_step.go
@@ -54,6 +54,7 @@ func (s *UpgradeShootStep) Run(operation internal.UpdatingOperation, log logrus.
 	if err != nil {
 		return s.operationManager.OperationFailed(operation, "invalid operation data - cannot create upgradeShoot input", err, log)
 	}
+	input.GardenerConfig.ShootNetworkingFilterDisabled = operation.ProvisioningParameters.ErsContext.DisableEnterprisePolicyFilter()
 
 	var provisionerResponse gqlschema.OperationStatus
 	if operation.ProvisionerOperationID == "" {
@@ -142,6 +143,9 @@ func gardenerUpgradeInputToConfigInput(input gqlschema.UpgradeShootInput) *gqlsc
 	}
 	if input.GardenerConfig.MaxUnavailable != nil {
 		result.MaxUnavailable = *input.GardenerConfig.MaxUnavailable
+	}
+	if input.GardenerConfig.ShootNetworkingFilterDisabled != nil {
+		result.ShootNetworkingFilterDisabled = input.GardenerConfig.ShootNetworkingFilterDisabled
 	}
 
 	return result
