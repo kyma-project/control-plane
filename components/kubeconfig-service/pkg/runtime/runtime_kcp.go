78e41ddb36cad0e47f3a78167787c2af739775d5
AdrianPei <85731814+AdrianPei@users.noreply.github.com>
2022-06-01 19:17:46 +0800

    feature: kcp runtime functions (#1736)

---
+++ components/kubeconfig-service/pkg/runtime/runtime_kcp.go
@@ -0,0 +1,227 @@
+package runtime
+
+import (
+	"context"
+	"encoding/json"
+	"path/filepath"
+	"time"
+
+	"github.com/kyma-project/control-plane/components/kubeconfig-service/pkg/caller"
+	"github.com/kyma-project/control-plane/components/kubeconfig-service/pkg/env"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	v1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+/*
+config map example:
+Name:        "userID",
+Namespace:   "kcp-system",
+Label:       "service=kubeconfig",
+Annotation:map[string]string
+    "role=L2L3ROLE"
+     "tenant=tenantID"
+Data:map[string]string
+    "runtimeid-a" : "starttime"
+    "runtimeid-b" : "starttime"
+*/
+
+const KcpNamespace string = "kcp-system"
+const ExpireTime time.Duration = 7 * 24 * time.Hour
+
+type JsonPatchType struct {
+	Op    string `json:"op"`
+	Path  string `json:"path"`
+	Value string `json:"value"`
+}
+
+func SetupConfigMap() error {
+	configMapList, err := getConfigMapList()
+	if configMapList == nil && err == nil {
+		log.Info("No Timer will be setup.")
+		return nil
+	} else if err != nil {
+		log.Errorf("Failed to setup timer.")
+		return err
+	}
+
+	for _, configMap := range (*configMapList).Items {
+		userID := configMap.ObjectMeta.Name
+		role := configMap.ObjectMeta.Annotations["role"]
+		tenantID := configMap.ObjectMeta.Annotations["tenant"]
+		for runtimeID, startTimeString := range configMap.Data {
+			c := caller.NewCaller(env.Config.GraphqlURL, tenantID)
+			status, err := c.RuntimeStatus(runtimeID)
+			if err != nil {
+				log.Errorf("Failed to get runtime status.")
+				return err
+			}
+			rawConfig := *status.RuntimeConfiguration.Kubeconfig
+			rtc, err := NewRuntimeClient([]byte(rawConfig), userID, role, tenantID)
+			if err != nil {
+				log.Errorf("Failed to create runtime client.")
+				return err
+			}
+			startTime, err := time.Parse("2006-01-02 15:04:05 +0000 UTC", startTimeString)
+			if err != nil {
+				log.Errorf("Failed to convert start time.")
+				return err
+			}
+			endTime := startTime.Add(ExpireTime)
+			duration := time.Until(endTime)
+			go rtc.SetupTimer(duration, runtimeID)
+		}
+	}
+
+	return nil
+}
+
+func GetK8sConfig() (*restclient.Config, error) {
+	k8sConfig, err := restclient.InClusterConfig()
+	if err != nil {
+		log.Warnf("Failed to read in cluster config: %s", err.Error())
+		log.Info("Trying to initialize with local config")
+		home := homedir.HomeDir()
+		k8sConfPath := filepath.Join(home, ".kube", "config")
+		k8sConfig, err = clientcmd.BuildConfigFromFlags("", k8sConfPath)
+		if err != nil {
+			return nil, errors.Errorf("Failed to read k8s in-cluster configuration, %s", err.Error())
+		}
+	}
+	return k8sConfig, nil
+}
+
+func GetK8sClient() (kubernetes.Interface, error) {
+	k8sconfig, err := GetK8sConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(k8sconfig)
+	if err != nil {
+		return nil, errors.Errorf("Failed to create k8s core client, %s", err.Error())
+	}
+	return clientset, err
+}
+
+func (rtc *RuntimeClient) SetupTimer(duration time.Duration, runtimeID string) {
+	userID := rtc.User.ServiceAccountName
+	if duration >= 0 {
+		timer := time.AfterFunc(duration, func() {
+			log.Printf("The timer for runtime %s user %s finished.", runtimeID, userID)
+		})
+		defer timer.Stop()
+	}
+
+	//After timer, clean up SA and ConfigMap
+	err := rtc.Cleaner()
+	if err != nil {
+		log.Warnf("Failed to clean runtime %s for user %s.", runtimeID, userID)
+	}
+
+	err = rtc.UpdateConfigMap(runtimeID)
+	if err != nil {
+		log.Warnf("Failed to clean config map for runtime %s user %s", runtimeID, userID)
+	}
+}
+
+func (rtc *RuntimeClient) UpdateConfigMap(runtimeID string) error {
+	log.Info("Trying to remove expired information.")
+	userID := rtc.User.ServiceAccountName
+
+	var patches []*JsonPatchType
+	path := "/data/" + runtimeID
+	patch := &JsonPatchType{
+		Op:   "remove",
+		Path: path,
+	}
+	patches = append(patches, patch)
+	payload, err := json.Marshal(patches)
+	if err != nil {
+		return errors.Errorf("Failed to marshal patch, %s", err.Error())
+	}
+	_, err = rtc.KcpK8s.CoreV1().ConfigMaps(KcpNamespace).Patch(context.Background(), userID, types.JSONPatchType, payload, metav1.PatchOptions{})
+	if err != nil {
+		return errors.Errorf("Failed to update config map, %s", err.Error())
+	}
+	return nil
+}
+
+func (rtc *RuntimeClient) DeployConfigMap(runtimeID string, L2L3OperatorRole string) error {
+	userID := rtc.User.ServiceAccountName
+	tenantID := rtc.User.TenantID
+
+	log.Info("Checking if the user exists")
+	startTime := time.Now().String()
+	cm, err := rtc.KcpK8s.CoreV1().ConfigMaps(KcpNamespace).Get(context.Background(), userID, metav1.GetOptions{})
+	if k8serrors.IsNotFound(err) {
+		log.Info("User doens't exist. Trying to create configmap.")
+		configmap := &v1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        userID,
+				Namespace:   KcpNamespace,
+				Labels:      map[string]string{"service": "kubeconfig"},
+				Annotations: map[string]string{"role": L2L3OperatorRole, "tenant": tenantID},
+			},
+			Data: map[string]string{runtimeID: startTime},
+		}
+		_, err = rtc.KcpK8s.CoreV1().ConfigMaps(KcpNamespace).Create(context.Background(), configmap, metav1.CreateOptions{})
+		if err != nil && !k8serrors.IsAlreadyExists(err) {
+			return errors.Errorf("Failed to create config map for user %s runtime %s", userID, runtimeID)
+		}
+	} else if err != nil {
+		return errors.Errorf("Failed to get config map for user %s runtime %s", userID, runtimeID)
+	} else {
+		log.Info("User already exist. Trying to update configmap.")
+		var patches []*JsonPatchType
+		var patch *JsonPatchType
+		path := "/data/" + runtimeID
+		if len(cm.Data[runtimeID]) != 0 {
+			log.Info("Runtime already exist. Trying to update expire time.")
+			patch = &JsonPatchType{
+				Op:    "replace",
+				Path:  path,
+				Value: startTime,
+			}
+		} else {
+			log.Info("Runtime not exiss. Trying to create new entry.")
+			patch = &JsonPatchType{
+				Op:    "add",
+				Path:  path,
+				Value: startTime,
+			}
+		}
+		patches = append(patches, patch)
+		payload, err := json.Marshal(patches)
+		if err != nil {
+			return errors.Errorf("Failed to marshal patch, %s", err.Error())
+		}
+		_, err = rtc.KcpK8s.CoreV1().ConfigMaps(KcpNamespace).Patch(context.Background(), userID, types.JSONPatchType, payload, metav1.PatchOptions{})
+		if err != nil {
+			return errors.Errorf("Failed to update config map, %s", err.Error())
+		}
+	}
+
+	return nil
+}
+
+func getConfigMapList() (*v1.ConfigMapList, error) {
+	coreClientset, err := GetK8sClient()
+	if err != nil {
+		return nil, errors.Errorf("Failed to get kcp k8s client.")
+	}
+	cmlist, err := coreClientset.CoreV1().ConfigMaps(KcpNamespace).List(context.Background(), metav1.ListOptions{LabelSelector: "service=kubeconfig"})
+	if k8serrors.IsNotFound(err) {
+		log.Info("All configmaps cleaned up.")
+		return nil, nil
+	} else if err != nil {
+		return nil, errors.Errorf("Failed to get config map list: %s", err.Error())
+	}
+	return cmlist, nil
+}
--- components/kubeconfig-service/pkg/runtime/runtime_test.go
+++ components/kubeconfig-service/pkg/runtime/runtime_test.go
@@ -17,23 +17,25 @@ var sa1 = corev1.ServiceAccount{
 	},
 }
 
-func NewRuntimeClientTest(kubeConfig []byte, userID string, L2L3OperatiorRole string) (*RuntimeClient, error) {
+func NewRuntimeClientTest(kubeConfig []byte, userID string, L2L3OperatiorRole string, tenant string) (*RuntimeClient, error) {
 	clientset := fake.NewSimpleClientset()
+	coreClientset := fake.NewSimpleClientset()
 
 	user := SAInfo{
 		ServiceAccountName:     userID,
 		ClusterRoleName:        userID,
 		ClusterRoleBindingName: userID,
 		Namespace:              "default",
+		TenantID:               tenant,
 	}
 	rollbackE := RollbackE{}
-	return &RuntimeClient{clientset, user, L2L3OperatiorRole, rollbackE}, nil
+	return &RuntimeClient{clientset, coreClientset, user, L2L3OperatiorRole, rollbackE}, nil
 }
 
 func TestCreateserviceaccount(t *testing.T) {
-	var expectedSaName, expectedNamespace = "sa1", "default"
+	var expectedSaName, expectedNamespace, expectedTenantID = "sa1", "default", "tenantID"
 	t.Run("If no service account exists one is created", func(t *testing.T) {
-		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeAdmin")
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeAdmin", "tenantID")
 		assert.NoError(t, err)
 		err = rtc.createServiceAccount()
 		assert.Nil(t, err)
@@ -43,10 +45,11 @@ func TestCreateserviceaccount(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, expectedSaName, sa.Name)
 		assert.Equal(t, expectedNamespace, sa.Namespace)
+		assert.Equal(t, expectedTenantID, rtc.User.TenantID)
 	})
 
 	t.Run("If service account already exists nothing is created", func(t *testing.T) {
-		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeAdmin")
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeAdmin", "tenantID")
 		assert.NoError(t, err)
 
 		_, err = rtc.K8s.CoreV1().ServiceAccounts(sa1.Namespace).Create(context.TODO(), &sa1, v1.CreateOptions{})
@@ -62,11 +65,12 @@ func TestCreateserviceaccount(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, expectedSaName, sa.Name)
 		assert.Equal(t, expectedNamespace, sa.Namespace)
+		assert.Equal(t, expectedTenantID, rtc.User.TenantID)
 	})
 
 	t.Run("If no clusterrole exists one is created", func(t *testing.T) {
 		var clusterRoleName = "sa1"
-		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator")
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator", "tenantID")
 		assert.NoError(t, err)
 
 		err = rtc.createClusterRole()
@@ -77,11 +81,12 @@ func TestCreateserviceaccount(t *testing.T) {
 		assert.NotNil(t, cr)
 		assert.NoError(t, err)
 		assert.Equal(t, clusterRoleName, cr.Name)
+		assert.Equal(t, expectedTenantID, rtc.User.TenantID)
 	})
 
 	t.Run("If input clusterrole not supported no one is created", func(t *testing.T) {
 		//`unSupportedOperation` not belong to `runtimeAdmin`/`runtimeOperator`
-		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "unSupportedOperation")
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "unSupportedOperation", "tenantID")
 		assert.NoError(t, err)
 
 		err = rtc.createClusterRole()
@@ -95,7 +100,7 @@ func TestCreateserviceaccount(t *testing.T) {
 
 	t.Run("If no clusterrole exists one is created", func(t *testing.T) {
 		var clusterRoleBindingName = "sa1"
-		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator")
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator", "tenantID")
 		assert.NoError(t, err)
 
 		err = rtc.createClusterRoleBinding()
@@ -106,5 +111,120 @@ func TestCreateserviceaccount(t *testing.T) {
 		assert.NotNil(t, crb)
 		assert.NoError(t, err)
 		assert.Equal(t, clusterRoleBindingName, crb.Name)
+		assert.Equal(t, expectedTenantID, rtc.User.TenantID)
 	})
+
+	t.Run("If kcp config map is updated with remove method", func(t *testing.T) {
+		var runtimeName = "runtime2"
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator", "tenantID")
+		assert.NoError(t, err)
+
+		configmap := &corev1.ConfigMap{
+			ObjectMeta: v1.ObjectMeta{
+				Name:        "sa1",
+				Namespace:   "kcp-system",
+				Labels:      map[string]string{"service": "kubeconfig"},
+				Annotations: map[string]string{"role": "runtimeOperator", "tenant": "tenantID"},
+			},
+			Data: map[string]string{"runtime1": "startTime1", "runtime2": "startTime2"},
+		}
+		cm, err := rtc.KcpK8s.CoreV1().ConfigMaps("kcp-system").Create(context.Background(), configmap, v1.CreateOptions{})
+		assert.NoError(t, err)
+		assert.NotNil(t, cm)
+
+		err = rtc.UpdateConfigMap(runtimeName)
+		assert.NoError(t, err)
+
+		cm, err = rtc.KcpK8s.CoreV1().ConfigMaps("kcp-system").Get(context.Background(), "sa1", v1.GetOptions{})
+		assert.NotNil(t, cm)
+		assert.NoError(t, err)
+		assert.Equal(t, "startTime1", cm.Data["runtime1"])
+		assert.Empty(t, cm.Data["runtime2"])
+	})
+
+	t.Run("If kcp config map deployed when no user exists", func(t *testing.T) {
+		var runtimeName, namespaceName = "runtime1", "kcp-system"
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator", "tenantID")
+		assert.NoError(t, err)
+
+		err = rtc.DeployConfigMap(runtimeName, "runtimeOperator")
+		assert.NoError(t, err)
+
+		cm, err := rtc.KcpK8s.CoreV1().ConfigMaps(namespaceName).Get(context.Background(), "sa1", v1.GetOptions{})
+		assert.NotNil(t, cm)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedSaName, cm.Name)
+		assert.Equal(t, namespaceName, cm.Namespace)
+		assert.Equal(t, "kubeconfig", cm.Labels["service"])
+		assert.Equal(t, "runtimeOperator", cm.Annotations["role"])
+		assert.Equal(t, expectedTenantID, cm.Annotations["tenant"])
+		assert.NotEmpty(t, cm.Data["runtime1"])
+	})
+
+	t.Run("If kcp config map deployed when user and runtime existed", func(t *testing.T) {
+		var runtimeName, namespaceName = "runtime1", "kcp-system"
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator", "tenantID")
+		assert.NoError(t, err)
+
+		configmap := &corev1.ConfigMap{
+			ObjectMeta: v1.ObjectMeta{
+				Name:        "sa1",
+				Namespace:   "kcp-system",
+				Labels:      map[string]string{"service": "kubeconfig"},
+				Annotations: map[string]string{"role": "runtimeOperator", "tenant": "tenantID"},
+			},
+			Data: map[string]string{"runtime1": "startTime1"},
+		}
+		cm, err := rtc.KcpK8s.CoreV1().ConfigMaps("kcp-system").Create(context.Background(), configmap, v1.CreateOptions{})
+		assert.NoError(t, err)
+		assert.NotNil(t, cm)
+
+		err = rtc.DeployConfigMap(runtimeName, "runtimeOperator")
+		assert.NoError(t, err)
+
+		cm, err = rtc.KcpK8s.CoreV1().ConfigMaps("kcp-system").Get(context.Background(), "sa1", v1.GetOptions{})
+		assert.NotNil(t, cm)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedSaName, cm.Name)
+		assert.Equal(t, namespaceName, cm.Namespace)
+		assert.Equal(t, "kubeconfig", cm.Labels["service"])
+		assert.Equal(t, "runtimeOperator", cm.Annotations["role"])
+		assert.Equal(t, expectedTenantID, cm.Annotations["tenant"])
+		assert.NotEmpty(t, cm.Data["runtime1"])
+		assert.NotEqual(t, "startTime1", cm.Data["runtime1"])
+	})
+
+	t.Run("If kcp config map deployed when user existed but runtime not", func(t *testing.T) {
+		var runtimeName, namespaceName = "runtime2", "kcp-system"
+		rtc, err := NewRuntimeClientTest([]byte("kubeconfig"), "sa1", "runtimeOperator", "tenantID")
+		assert.NoError(t, err)
+
+		configmap := &corev1.ConfigMap{
+			ObjectMeta: v1.ObjectMeta{
+				Name:        "sa1",
+				Namespace:   "kcp-system",
+				Labels:      map[string]string{"service": "kubeconfig"},
+				Annotations: map[string]string{"role": "runtimeOperator", "tenant": "tenantID"},
+			},
+			Data: map[string]string{"runtime1": "startTime1"},
+		}
+		cm, err := rtc.KcpK8s.CoreV1().ConfigMaps("kcp-system").Create(context.Background(), configmap, v1.CreateOptions{})
+		assert.NoError(t, err)
+		assert.NotNil(t, cm)
+
+		err = rtc.DeployConfigMap(runtimeName, "runtimeOperator")
+		assert.NoError(t, err)
+
+		cm, err = rtc.KcpK8s.CoreV1().ConfigMaps("kcp-system").Get(context.Background(), "sa1", v1.GetOptions{})
+		assert.NotNil(t, cm)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedSaName, cm.Name)
+		assert.Equal(t, namespaceName, cm.Namespace)
+		assert.Equal(t, "kubeconfig", cm.Labels["service"])
+		assert.Equal(t, "runtimeOperator", cm.Annotations["role"])
+		assert.Equal(t, expectedTenantID, cm.Annotations["tenant"])
+		assert.Equal(t, "startTime1", cm.Data["runtime1"])
+		assert.NotEmpty(t, cm.Data["runtime2"])
+	})
+
 }
