48646ed476aab932d397fe44760a67e3c9b702db
Jan Wozniak <wozniak.jan@gmail.com>
2022-06-24 08:35:47 +0200

    Introduce license_type aggregated metric
    compass_keb_ers_context_license_type_total will offer aggregated metric
    counting all instances grouped by license_type from ERSContext last
    operation

--- components/kyma-environment-broker/internal/metrics/instances_collector.go
+++ components/kyma-environment-broker/internal/metrics/instances_collector.go
@@ -10,8 +10,10 @@ import (
 //   (instance exists but the cluster was removed manually from the gardener):
 // - compass_keb_instances_total - total number of all instances
 // - compass_keb_global_account_id_instances_total - total number of all instances per global account
+// - compass_keb_ers_context_license_type_total - count of instances grouped by license types
 type InstancesStatsGetter interface {
 	GetInstanceStats() (internal.InstanceStats, error)
+	GetERSContextStats() (internal.ERSContextStats, error)
 }
 
 type InstancesCollector struct {
@@ -19,6 +21,7 @@ type InstancesCollector struct {
 
 	instancesDesc        *prometheus.Desc
 	instancesPerGAIDDesc *prometheus.Desc
+	licenseTypeDesc      *prometheus.Desc
 }
 
 func NewInstancesCollector(statsGetter InstancesStatsGetter) *InstancesCollector {
@@ -35,12 +38,18 @@ func NewInstancesCollector(statsGetter InstancesStatsGetter) *InstancesCollector
 			"The total number of instances by Global Account ID",
 			[]string{"global_account_id"},
 			nil),
+		licenseTypeDesc: prometheus.NewDesc(
+			prometheus.BuildFQName(prometheusNamespace, prometheusSubsystem, "ers_context_license_type_total"),
+			"count of instances grouped by license types",
+			[]string{"license_type"},
+			nil),
 	}
 }
 
 func (c *InstancesCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- c.instancesDesc
 	ch <- c.instancesPerGAIDDesc
+	ch <- c.licenseTypeDesc
 }
 
 // Collect implements the prometheus.Collector interface.
@@ -48,11 +57,20 @@ func (c *InstancesCollector) Collect(ch chan<- prometheus.Metric) {
 	stats, err := c.statsGetter.GetInstanceStats()
 	if err != nil {
 		logrus.Error(err)
+	} else {
+		collect(ch, c.instancesDesc, stats.TotalNumberOfInstances)
+
+		for globalAccountID, num := range stats.PerGlobalAccountID {
+			collect(ch, c.instancesPerGAIDDesc, num, globalAccountID)
+		}
+	}
+
+	stats2, err := c.statsGetter.GetERSContextStats()
+	if err != nil {
+		logrus.Error(err)
 		return
 	}
-	collect(ch, c.instancesDesc, stats.TotalNumberOfInstances)
-
-	for globalAccountID, num := range stats.PerGlobalAccountID {
-		collect(ch, c.instancesPerGAIDDesc, num, globalAccountID)
+	for t, num := range stats2.LicenseType {
+		collect(ch, c.licenseTypeDesc, num, t)
 	}
 }
