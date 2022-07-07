d6d24bef31dce3a8fb91e21e9f8968bda93c53f4
Marcin Szwed <marcin.szwed@sap.com>
2022-07-07 16:16:47 +0200

    Add ConfigReader

--- components/kyma-environment-broker/internal/config.go
+++ components/kyma-environment-broker/internal/config.go
@@ -1,7 +1,24 @@
 package internal
 
-import "github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/runtime"
+import (
+	"context"
+
+	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type ConfigReader struct {
+	ctx       context.Context
+	k8sClient client.Client
+}
 
 type ConfigForPlan struct {
 	AdditionalComponents []runtime.KymaComponent `json:"additional-components"`
+}
+
+func NewConfigReader(ctx context.Context, k8sClient client.Client) *ConfigReader {
+	return &ConfigReader{
+		ctx:       ctx,
+		k8sClient: k8sClient,
+	}
 }
