3021acaaf8d0e9e6e9832f2788355fdabedee85d
Marcin Szwed <marcin.szwed@sap.com>
2022-07-08 14:54:25 +0200

    Implement ReadConfig()

--- components/kyma-environment-broker/internal/config.go
+++ components/kyma-environment-broker/internal/config.go
@@ -2,23 +2,102 @@ package internal
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/runtime"
+	"github.com/sirupsen/logrus"
+	coreV1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	namespace                 = "kcp-system"
+	runtimeVersionLabelPrefix = "runtime-version-"
+	kebConfigLabel            = "keb-config"
 )
 
 type ConfigReader struct {
 	ctx       context.Context
 	k8sClient client.Client
+	logger    logrus.FieldLogger
 }
 
 type ConfigForPlan struct {
 	AdditionalComponents []runtime.KymaComponent `json:"additional-components"`
 }
 
-func NewConfigReader(ctx context.Context, k8sClient client.Client) *ConfigReader {
+func NewConfigReader(ctx context.Context, k8sClient client.Client, logger logrus.FieldLogger) *ConfigReader {
 	return &ConfigReader{
 		ctx:       ctx,
 		k8sClient: k8sClient,
+		logger:    logger,
 	}
+}
+
+func (r *ConfigReader) ReadConfig(kymaVersion RuntimeVersionData, planName string) (string, error) {
+	cfgMapList, err := r.getConfigMapList(kymaVersion)
+	if err != nil {
+		return "", err
+	}
+
+	if err = r.verifyConfigMapExistence(cfgMapList); err != nil {
+		return "", fmt.Errorf("while verifying configuration configmap existence for Kyma version %v: %w",
+			kymaVersion.Version, err)
+	}
+	r.logger.Infof("found configmap with configuration for Kyma version: %v. Checking plan existence...",
+		kymaVersion.Version)
+
+	cfgMap := cfgMapList.Items[0]
+	cfgString, err := r.verifyPlanEntryExistence(&cfgMap, planName)
+	if err != nil {
+		return "", fmt.Errorf("while verifying configuration existence for Kyma version %v and plan %v"+
+			": %w", kymaVersion.Version, planName, err)
+	}
+	r.logger.Infof("found configuration for plan %v", planName)
+
+	return cfgString, nil
+}
+
+func (r *ConfigReader) getConfigMapList(kymaVersion RuntimeVersionData) (*coreV1.ConfigMapList, error) {
+	cfgMapList := &coreV1.ConfigMapList{}
+	listOptions := configMapListOptions(kymaVersion.Version)
+	if err := r.k8sClient.List(r.ctx, cfgMapList, listOptions...); err != nil {
+		return nil, fmt.Errorf("while fetching configmaps with configuration for Kyma version %v: %w",
+			kymaVersion.Version, err)
+	}
+	return cfgMapList, nil
+}
+
+func configMapListOptions(version string) []client.ListOption {
+	versionLabel := runtimeVersionLabelPrefix + strings.ToLower(version)
+
+	labels := map[string]string{
+		versionLabel:   "true",
+		kebConfigLabel: "true",
+	}
+
+	return []client.ListOption{
+		client.InNamespace(namespace),
+		client.MatchingLabels(labels),
+	}
+}
+
+func (r *ConfigReader) verifyConfigMapExistence(cfgMapList *coreV1.ConfigMapList) error {
+	switch n := len(cfgMapList.Items); n {
+	case 1:
+		return nil
+	case 0:
+		return fmt.Errorf("configmap with configuration does not exist")
+	default:
+		return fmt.Errorf("allowed number of configuration configmaps: 1, found: %d", n)
+	}
+}
+
+func (r *ConfigReader) verifyPlanEntryExistence(cfgMap *coreV1.ConfigMap, planName string) (string, error) {
+	cfgString, exists := cfgMap.Data[planName]
+	if !exists {
+		return "", fmt.Errorf("configuration for plan %v does not exist", planName)
+	}
+	return cfgString, nil
 }
