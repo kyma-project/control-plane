4e7713b5ee41b812cb33215180474ce20735d508
ralikio <74771103+ralikio@users.noreply.github.com>
2022-07-27 11:04:08 +0200

    Removed Unnecessary Error

--- components/schema-migrator/main.go
+++ components/schema-migrator/main.go
@@ -176,7 +176,8 @@ func quitIstioSidecar() error {
 	}
 
 	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-		return fmt.Errorf("while receiving response: %s", err)
+		fmt.Printf("Quiting istio, response status is: %d", resp.StatusCode)
+		return nil
 	}
 
 	return nil
