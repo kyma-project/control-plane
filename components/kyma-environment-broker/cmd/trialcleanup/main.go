f6853287249e4418e8e581466790dccb66de4c6c
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2022-08-02 11:56:43 +0200

    Trial cleaner job with dummy logic (#1902)
    * initial main and dockerfile
    
    * trial-cleanup-job added to Makefile
    
    * template added
    
    * PR reference in values
    
    * generic makefile updated - creation of trial cleanup image name
    
    * minimal cronjob
    
    * review remark - status removed

---
+++ components/kyma-environment-broker/cmd/trialcleanup/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.Info("Starting dummy trial cleanup job!")
+	log.Info("Dummy trial cleanup job finished successfully after doing nothing!")
+}
