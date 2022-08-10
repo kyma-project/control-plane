4922c93b06fe2ae673952ecfc58be13d35d6199b
Lukasz <lukasz.jezak@sap.com>
2022-08-10 14:47:38 +0200

    Test PR

--- components/kyma-environment-broker/cmd/broker/main.go
+++ components/kyma-environment-broker/cmd/broker/main.go
@@ -80,6 +80,7 @@ func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
+//Test
 // Config holds configuration for the whole application
 type Config struct {
 	// DbInMemory allows to use memory storage instead of the postgres one.
