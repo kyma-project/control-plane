ce75d6b21035942a44f3a4edee10a7fd8e02d8cf
jaroslaw-pieszka <jaroslaw.pieszka@sap.com>
2022-09-12 11:50:31 +0200

    happy path covered

--- components/kyma-environment-broker/internal/process/deprovisioning/remove_instance_step_test.go
+++ components/kyma-environment-broker/internal/process/deprovisioning/remove_instance_step_test.go
@@ -11,18 +11,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRemoveInstanceStep_HappyPath(t *testing.T) {
+func TestRemoveInstanceStep_HappyPathForPermanentRemoval(t *testing.T) {
 	// given
 	log := logrus.New()
 	memoryStorage := storage.NewMemoryStorage()
 
 	operation := fixture.FixDeprovisioningOperationAsOperation(operationID, instanceID)
-	instance := fixGCPInstance(operation.InstanceID)
+	instance := fixture.FixInstance(instanceID)
 
 	err := memoryStorage.Instances().Insert(instance)
 	assert.NoError(t, err)
 
-	step := NewRemoveInstanceStep(memoryStorage.Instances(), nil)
+	err = memoryStorage.Operations().InsertOperation(operation)
+	assert.NoError(t, err)
+
+	step := NewRemoveInstanceStep(memoryStorage.Instances(), memoryStorage.Operations())
 
 	// when
 	operation, repeat, err := step.Run(operation, log)
@@ -30,7 +33,46 @@ func TestRemoveInstanceStep_HappyPath(t *testing.T) {
 	assert.NoError(t, err)
 
 	// then
+	operationFromStorage, err := memoryStorage.Operations().GetOperationByID(operationID)
 	assert.NoError(t, err)
+	assert.Equal(t, 0, len(operationFromStorage.ProvisioningParameters.ErsContext.UserID))
+
+	_, err = memoryStorage.Instances().GetByID(instanceID)
+	assert.ErrorContains(t, err, "not exist")
+
+	assert.Equal(t, time.Duration(0), repeat)
+	assert.Equal(t, domain.Succeeded, operation.State)
+}
+
+func TestRemoveInstanceStep_HappyPathForSuspension(t *testing.T) {
+	// given
+	log := logrus.New()
+	memoryStorage := storage.NewMemoryStorage()
+
+	operation := fixture.FixSuspensionOperationAsOperation(operationID, instanceID)
+	instance := fixture.FixInstance(instanceID)
+
+	err := memoryStorage.Instances().Insert(instance)
+	assert.NoError(t, err)
+
+	err = memoryStorage.Operations().InsertOperation(operation)
+	assert.NoError(t, err)
+
+	step := NewRemoveInstanceStep(memoryStorage.Instances(), memoryStorage.Operations())
+
+	// when
+	operation, repeat, err := step.Run(operation, log)
+
+	assert.NoError(t, err)
+
+	// then
+	operationFromStorage, err := memoryStorage.Operations().GetOperationByID(operationID)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(operationFromStorage.RuntimeID))
+
+	instanceFromStorage, err := memoryStorage.Instances().GetByID(instanceID)
+	assert.Equal(t, 0, len(instanceFromStorage.RuntimeID))
+
 	assert.Equal(t, time.Duration(0), repeat)
 	assert.Equal(t, domain.Succeeded, operation.State)
 }
