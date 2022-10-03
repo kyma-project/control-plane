d062f3383118be975875e4b3af7c27c068b2c3d3
Miskiewicz <piotr.miskiewicz@sap.com>
2022-10-03 06:42:05 +0200

    Use regular mutex instead of nsync

--- components/kyma-environment-broker/internal/process/input/input.go
+++ components/kyma-environment-broker/internal/process/input/input.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	reconcilerApi "github.com/kyma-incubator/reconciler/pkg/keb"
@@ -14,7 +15,6 @@ import (
 	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/runtime"
 	"github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
 	"github.com/pkg/errors"
-	"github.com/vburenin/nsync"
 )
 
 const (
@@ -40,13 +40,17 @@ type Config struct {
 }
 
 type RuntimeInput struct {
+	muOptionalComponents sync.Mutex
+	muLabels             sync.Mutex
+	muOverrides          sync.Mutex
+
 	provisionRuntimeInput gqlschema.ProvisionRuntimeInput
 	upgradeRuntimeInput   gqlschema.UpgradeRuntimeInput
 	upgradeShootInput     gqlschema.UpgradeShootInput
-	mutex                 *nsync.NamedMutex
-	overrides             map[string][]*gqlschema.ConfigEntryInput
-	labels                map[string]string
-	globalOverrides       []*gqlschema.ConfigEntryInput
+	//mutex                 *nsync.NamedMutex
+	overrides       map[string][]*gqlschema.ConfigEntryInput
+	labels          map[string]string
+	globalOverrides []*gqlschema.ConfigEntryInput
 
 	config                    *internal.ConfigForPlan
 	hyperscalerInputProvider  HyperscalerInputProvider
@@ -73,15 +77,15 @@ func (r *RuntimeInput) Configuration() *internal.ConfigForPlan {
 }
 
 func (r *RuntimeInput) EnableOptionalComponent(componentName string) internal.ProvisionerInputCreator {
-	r.mutex.Lock("enabledOptionalComponents")
-	defer r.mutex.Unlock("enabledOptionalComponents")
+	r.muOptionalComponents.Lock()
+	defer r.muOptionalComponents.Unlock()
 	r.enabledOptionalComponents[componentName] = struct{}{}
 	return r
 }
 
 func (r *RuntimeInput) DisableOptionalComponent(componentName string) internal.ProvisionerInputCreator {
-	r.mutex.Lock("enabledOptionalComponents")
-	defer r.mutex.Unlock("enabledOptionalComponents")
+	r.muOptionalComponents.Lock()
+	defer r.muOptionalComponents.Unlock()
 
 	r.optionalComponentsService.AddComponentToDisable(componentName, runtime.NewGenericComponentDisabler(componentName))
 	delete(r.enabledOptionalComponents, componentName)
@@ -135,13 +139,13 @@ func (r *RuntimeInput) SetOIDCLastValues(oidcConfig gqlschema.OIDCConfigInput) i
 	return r
 }
 
-// AppendOverrides sets the overrides for the given component and discard the previous ones.
+// SetOverrides sets the overrides for the given component and discard the previous ones.
 //
 // Deprecated: use AppendOverrides
 func (r *RuntimeInput) SetOverrides(component string, overrides []*gqlschema.ConfigEntryInput) internal.ProvisionerInputCreator {
 	// currently same as in AppendOverrides function, as we working on the same underlying object.
-	r.mutex.Lock("AppendOverrides")
-	defer r.mutex.Unlock("AppendOverrides")
+	r.muOverrides.Lock()
+	defer r.muOverrides.Lock()
 
 	r.overrides[component] = overrides
 	return r
@@ -149,8 +153,8 @@ func (r *RuntimeInput) SetOverrides(component string, overrides []*gqlschema.Con
 
 // AppendOverrides appends overrides for the given components, the existing overrides are preserved.
 func (r *RuntimeInput) AppendOverrides(component string, overrides []*gqlschema.ConfigEntryInput) internal.ProvisionerInputCreator {
-	r.mutex.Lock("AppendOverrides")
-	defer r.mutex.Unlock("AppendOverrides")
+	r.muOverrides.Lock()
+	defer r.muOverrides.Lock()
 
 	for _, o2 := range overrides {
 		found := false
@@ -170,8 +174,8 @@ func (r *RuntimeInput) AppendOverrides(component string, overrides []*gqlschema.
 
 // AppendGlobalOverrides appends overrides, the existing overrides are preserved.
 func (r *RuntimeInput) AppendGlobalOverrides(overrides []*gqlschema.ConfigEntryInput) internal.ProvisionerInputCreator {
-	r.mutex.Lock("AppendGlobalOverrides")
-	defer r.mutex.Unlock("AppendGlobalOverrides")
+	r.muOverrides.Lock()
+	defer r.muOverrides.Lock()
 
 	for _, o2 := range overrides {
 		found := false
@@ -190,8 +194,8 @@ func (r *RuntimeInput) AppendGlobalOverrides(overrides []*gqlschema.ConfigEntryI
 }
 
 func (r *RuntimeInput) SetLabel(key, value string) internal.ProvisionerInputCreator {
-	r.mutex.Lock("Labels")
-	defer r.mutex.Unlock("Labels")
+	r.muLabels.Lock()
+	defer r.muLabels.Lock()
 
 	if r.provisionRuntimeInput.RuntimeInput.Labels == nil {
 		r.provisionRuntimeInput.RuntimeInput.Labels = gqlschema.Labels{}
@@ -495,8 +499,8 @@ func (r *RuntimeInput) applyProvisioningParametersForUpgradeShoot() error {
 }
 
 func (r *RuntimeInput) resolveOptionalComponentsForProvisionRuntime() error {
-	r.mutex.Lock("enabledOptionalComponents")
-	defer r.mutex.Unlock("enabledOptionalComponents")
+	r.muOptionalComponents.Lock()
+	defer r.muOptionalComponents.Unlock()
 
 	componentsToInstall := []string{}
 	componentsToInstall = append(componentsToInstall, r.provisioningParameters.Parameters.OptionalComponentsToInstall...)
@@ -516,8 +520,8 @@ func (r *RuntimeInput) resolveOptionalComponentsForProvisionRuntime() error {
 }
 
 func (r *RuntimeInput) resolveOptionalComponentsForUpgradeRuntime() error {
-	r.mutex.Lock("enabledOptionalComponents")
-	defer r.mutex.Unlock("enabledOptionalComponents")
+	r.muOptionalComponents.Lock()
+	defer r.muOptionalComponents.Unlock()
 
 	componentsToInstall := []string{}
 	componentsToInstall = append(componentsToInstall, r.provisioningParameters.Parameters.OptionalComponentsToInstall...)
