06814a228087dd3f19759bf7c0e0898639bc226c
Domokos János <Jani19966@gmail.com>
2022-10-04 09:09:13 +0200

    Created AWS cleanup script (#1840)
    * testing
    
    * Created AWS volume cleanup script
    
    * Removed AWS credentials from commit
    
    * Update README.md
    
    Co-authored-by: Benjamin Somhegyi <somhegyi.b@gmail.com>
    
    * Refractored code
    
    * Adding back removed empty line
    
    * Bumping old PR version to 1840
    
    The following PR has completed AWS cleanup script logic: https://github.com/kyma-project/control-plane/pull/1840
    
    * Ran go mod tidy
    
    Co-authored-by: i332322 <hoang.hai.nguyen@sap.com>
    Co-authored-by: i332322 <107933914+i332322@users.noreply.github.com>
    Co-authored-by: Benjamin Somhegyi <somhegyi.b@gmail.com>

--- cmd/subscriptioncleanup/cloudprovider/aws.go
+++ cmd/subscriptioncleanup/cloudprovider/aws.go
@@ -1,18 +1,117 @@
 package cloudprovider
 
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
 type awsResourceCleaner struct {
+	credentials awsCredentialsConfig
 }
 
-type awsConfig struct {
+type awsCredentialsConfig struct {
 	accessKeyID     string
 	secretAccessKey string
 }
 
 func NewAwsResourcesCleaner(secretData map[string][]byte) (ResourceCleaner, error) {
-	return awsResourceCleaner{}, nil
+	awsResourceCleaner := awsResourceCleaner{}
+	awsConfig, err := awsResourceCleaner.toAwsConfig(secretData)
+	if err != nil {
+		return nil, err
+	}
+
+	awsResourceCleaner.credentials = awsConfig
+	return awsResourceCleaner, nil
 }
 
 func (ac awsResourceCleaner) Do() error {
-	// TODO: clean up resources if required...
+	all_regions, err := ac.getAllRegions()
+	if err != nil {
+		return err
+	}
+
+	for _, region := range all_regions.Regions {
+		logrus.Printf("Switching to region %v", *region.RegionName)
+		ec2Client, err := ac.newAwsEC2Client(ac.credentials, *region.RegionName)
+		if err != nil {
+			return err
+		}
+
+		err = ac.deleteVolumes(ec2Client)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func (ac awsResourceCleaner) deleteVolumes(ec2Client ec2.Client) error {
+	volumes, err := ec2Client.DescribeVolumes(context.TODO(), &ec2.DescribeVolumesInput{})
+	if err != nil {
+		return err
+	}
+
+	for _, volume := range volumes.Volumes {
+		if volume.State == types.VolumeStateInUse {
+			return errors.New(fmt.Sprintf("There is an EC2 instance which uses this volume with id: %v", *volume.VolumeId))
+		}
+	}
+
+	for _, volume := range volumes.Volumes {
+		logrus.Printf("Deleting volume with id %v", *volume.VolumeId)
+		ec2Client.DeleteVolume(context.TODO(), &ec2.DeleteVolumeInput{
+			VolumeId: volume.VolumeId,
+		})
+	}
+
+	return nil
+}
+
+func (ac awsResourceCleaner) getAllRegions() (ec2.DescribeRegionsOutput, error) {
+	allRegions := false
+	ec2Client, err := ac.newAwsEC2Client(ac.credentials, "eu-central-1")
+	if err != nil {
+		return ec2.DescribeRegionsOutput{}, err
+	}
+
+	regionOutput, err := ec2Client.DescribeRegions(context.TODO(), &ec2.DescribeRegionsInput{AllRegions: &allRegions})
+	if err != nil {
+		return ec2.DescribeRegionsOutput{}, err
+	}
+
+	return *regionOutput, nil
+}
+
+func (ac awsResourceCleaner) toAwsConfig(secretData map[string][]byte) (awsCredentialsConfig, error) {
+	accessKeyID, exists := secretData["accessKeyID"]
+	if !exists {
+		return awsCredentialsConfig{}, errors.New("AccessKeyID was not provided in secret!")
+	}
+
+	secretAccessKey, exists := secretData["secretAccessKey"]
+	if !exists {
+		return awsCredentialsConfig{}, errors.New("SecretAccessKey was not provided in secret!")
+	}
+
+	return awsCredentialsConfig{
+		accessKeyID:     string(accessKeyID),
+		secretAccessKey: string(secretAccessKey),
+	}, nil
+}
+
+func (ac awsResourceCleaner) newAwsEC2Client(awsCredentialConfig awsCredentialsConfig, region string) (ec2.Client, error) {
+	return *ec2.New(ec2.Options{
+		Region:      region,
+		Credentials: aws.NewCredentialsCache(credentials.NewStaticCredentialsProvider(awsCredentialConfig.accessKeyID, awsCredentialConfig.secretAccessKey, "")),
+	}), nil
 }
