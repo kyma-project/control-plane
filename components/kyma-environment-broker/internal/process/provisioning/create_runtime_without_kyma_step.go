8c282ca6b3656d3c1dd4b94131c9c6699b3aa843
jaroslaw-pieszka <jaroslaw.pieszka@sap.com>
2022-12-14 15:58:48 +0100

    extra logging provisioning parameters

--- components/kyma-environment-broker/internal/process/provisioning/create_runtime_without_kyma_step.go
+++ components/kyma-environment-broker/internal/process/provisioning/create_runtime_without_kyma_step.go
@@ -58,11 +58,12 @@ func (s *CreateRuntimeWithoutKymaStep) Run(operation internal.Operation, log log
 		return s.operationManager.OperationFailed(operation, "invalid operation data - cannot create provisioning input", err, log)
 	}
 
-	log.Infof("call ProvisionRuntime: kubernetesVersion=%s, region=%s, provider=%s, name=%s",
+	log.Infof("call ProvisionRuntime: kubernetesVersion=%s, region=%s, provider=%s, name=%s, euAccess=%t",
 		requestInput.ClusterConfig.GardenerConfig.KubernetesVersion,
 		requestInput.ClusterConfig.GardenerConfig.Region,
 		requestInput.ClusterConfig.GardenerConfig.Provider,
-		requestInput.ClusterConfig.GardenerConfig.Name)
+		requestInput.ClusterConfig.GardenerConfig.Name,
+		requestInput.ClusterConfig.GardenerConfig.EuAccess)
 
 	provisionerResponse, err := s.provisionerClient.ProvisionRuntime(operation.ProvisioningParameters.ErsContext.GlobalAccountID, operation.ProvisioningParameters.ErsContext.SubAccountID, requestInput)
 	switch {
