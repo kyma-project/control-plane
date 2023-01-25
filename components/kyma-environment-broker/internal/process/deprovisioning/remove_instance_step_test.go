8eb427cc033f21df5dce87fcd088cec7c82e5824
Marek Michali <marek.michali@sap.com>
2023-01-25 11:41:37 +0100

    Add test case for instance removing

--- components/kyma-environment-broker/internal/process/deprovisioning/remove_instance_step_test.go
+++ components/kyma-environment-broker/internal/process/deprovisioning/remove_instance_step_test.go
@@ -71,6 +71,7 @@ func TestRemoveInstanceStep_HappyPathForSuspension(t *testing.T) {
 
 	operation := fixture.FixSuspensionOperationAsOperation(operationID, instanceID)
 	instance := fixture.FixInstance(instanceID)
+	instance.DeletedAt = time.Time{}
 
 	err := memoryStorage.Instances().Insert(instance)
 	assert.NoError(t, err)
@@ -92,6 +93,42 @@ func TestRemoveInstanceStep_HappyPathForSuspension(t *testing.T) {
 
 	instanceFromStorage, err := memoryStorage.Instances().GetByID(instanceID)
 	assert.Equal(t, 0, len(instanceFromStorage.RuntimeID))
+	assert.Equal(t, time.Time{}, instanceFromStorage.DeletedAt)
+
+	assert.Equal(t, time.Duration(0), backoff)
+}
+
+func TestRemoveInstanceStep_InstanceHasExecutedButNotCompletedOperationSteps(t *testing.T) {
+	// given
+	log := logrus.New()
+	memoryStorage := storage.NewMemoryStorage()
+
+	operation := fixture.FixDeprovisioningOperationAsOperation(operationID, instanceID)
+	operation.ExcutedButNotCompleted = append(operation.ExcutedButNotCompleted, "De-provision_AVS_Evaluations")
+	instance := fixture.FixInstance(instanceID)
+	instance.DeletedAt = time.Time{}
+
+	err := memoryStorage.Instances().Insert(instance)
+	assert.NoError(t, err)
+
+	err = memoryStorage.Operations().InsertOperation(operation)
+	assert.NoError(t, err)
+
+	step := NewRemoveInstanceStep(memoryStorage.Instances(), memoryStorage.Operations())
+
+	// when
+	_, backoff, err := step.Run(operation, log)
+
+	assert.NoError(t, err)
+
+	// then
+	operationFromStorage, err := memoryStorage.Operations().GetOperationByID(operationID)
+	assert.NoError(t, err)
+	assert.Equal(t, false, operationFromStorage.Temporary)
+
+	instanceFromStorage, err := memoryStorage.Instances().GetByID(instanceID)
+	assert.NoError(t, err)
+	assert.NotEqual(t, time.Time{}, instanceFromStorage.DeletedAt)
 
 	assert.Equal(t, time.Duration(0), backoff)
 }
