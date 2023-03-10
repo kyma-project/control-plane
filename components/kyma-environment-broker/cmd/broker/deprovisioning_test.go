88e1aa18e16648801e875bf5760dd806216e6d84
Marek Michali <56163696+MarekMichali@users.noreply.github.com>
2023-03-10 15:52:43 +0100

    Add unit tests for redeprovisioning (#2570)
    * Add unit tests
    
    * Fix typo
    
    * Bump KEB image

--- components/kyma-environment-broker/cmd/broker/deprovisioning_test.go
+++ components/kyma-environment-broker/cmd/broker/deprovisioning_test.go
@@ -19,7 +19,7 @@ func TestKymaDeprovision(t *testing.T) {
 	instanceId := suite.CreateProvisionedRuntime(runtimeOptions)
 
 	// when
-	deprovisioningOperationID := suite.CreateDeprovisioning(instanceId)
+	deprovisioningOperationID := suite.CreateDeprovisioning(deprovisioningOpID, instanceId)
 
 	// then
 	suite.WaitForDeprovisioningState(deprovisioningOperationID, domain.InProgress)
@@ -30,4 +30,86 @@ func TestKymaDeprovision(t *testing.T) {
 
 	// then
 	suite.WaitForDeprovisioningState(deprovisioningOperationID, domain.Succeeded)
+	suite.AssertInstanceRemoved(instanceId)
+}
+
+func TestKymaReDeprovisionSucceeded(t *testing.T) {
+	// given
+	runtimeOptions := RuntimeOptions{
+		GlobalAccountID: globalAccountID,
+		SubAccountID:    badSubAccountID,
+		Provider:        internal.AWS,
+	}
+
+	suite := NewDeprovisioningSuite(t)
+	instanceId := suite.CreateProvisionedRuntime(runtimeOptions)
+
+	// when
+	deprovisioningOperationID := suite.CreateDeprovisioning(deprovisioningOpID, instanceId)
+
+	// then
+	suite.WaitForDeprovisioningState(deprovisioningOperationID, domain.InProgress)
+	suite.AssertProvisionerStartedDeprovisioning(deprovisioningOperationID)
+
+	// when
+	suite.FinishDeprovisioningOperationByProvisioner(deprovisioningOperationID)
+
+	// then
+	suite.WaitForDeprovisioningState(deprovisioningOperationID, domain.Succeeded)
+	suite.AssertInstanceNotRemoved(instanceId)
+
+	// when
+	runtimeOptions.SubAccountID = subAccountID
+	suite.updateSubAccountIDForDeprovisioningOperation(runtimeOptions, instanceId)
+	reDeprovisioningOperationID := suite.CreateDeprovisioning(reDeprovisioningOpID, instanceId)
+
+	// then
+	suite.WaitForDeprovisioningState(reDeprovisioningOperationID, domain.InProgress)
+	suite.AssertProvisionerStartedDeprovisioning(reDeprovisioningOperationID)
+
+	// when
+	suite.FinishDeprovisioningOperationByProvisioner(reDeprovisioningOperationID)
+
+	// then
+	suite.WaitForDeprovisioningState(reDeprovisioningOperationID, domain.Succeeded)
+	suite.AssertInstanceRemoved(instanceId)
+}
+
+func TestKymaReDeprovisionFailed(t *testing.T) {
+	// given
+	runtimeOptions := RuntimeOptions{
+		GlobalAccountID: globalAccountID,
+		SubAccountID:    badSubAccountID,
+		Provider:        internal.AWS,
+	}
+
+	suite := NewDeprovisioningSuite(t)
+	instanceId := suite.CreateProvisionedRuntime(runtimeOptions)
+	// when
+	deprovisioningOperationID := suite.CreateDeprovisioning(deprovisioningOpID, instanceId)
+
+	// then
+	suite.WaitForDeprovisioningState(deprovisioningOperationID, domain.InProgress)
+	suite.AssertProvisionerStartedDeprovisioning(deprovisioningOperationID)
+
+	// when
+	suite.FinishDeprovisioningOperationByProvisioner(deprovisioningOperationID)
+
+	// then
+	suite.WaitForDeprovisioningState(deprovisioningOperationID, domain.Succeeded)
+	suite.AssertInstanceNotRemoved(instanceId)
+
+	// when
+	reDeprovisioningOperationID := suite.CreateDeprovisioning(reDeprovisioningOpID, instanceId)
+
+	// then
+	suite.WaitForDeprovisioningState(reDeprovisioningOperationID, domain.InProgress)
+	suite.AssertProvisionerStartedDeprovisioning(reDeprovisioningOperationID)
+
+	// when
+	suite.FinishDeprovisioningOperationByProvisioner(reDeprovisioningOperationID)
+
+	// then
+	suite.WaitForDeprovisioningState(reDeprovisioningOperationID, domain.Succeeded)
+	suite.AssertInstanceNotRemoved(instanceId)
 }
