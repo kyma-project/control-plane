4547b8212d03786600e5c7c1cfc3bfc6ea694984
Rafał Foks <r.fok74@gmail.com>
2023-04-21 13:03:43 +0200

    Revert original file

--- tests/provisioner-tests/scripts/entrypoint.sh
+++ tests/provisioner-tests/scripts/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]
   	echo "Test cases regexp not provided, running default test cases: $TEST_CASES"
 fi
 
-echo "Run Provisioner tests mv"
+echo "Run Provisioner tests"
 ./provisioner.test -test.v -test.timeout 5h -test.run ${TEST_CASES} -test.parallel 3
 res=$((res+$?))
 
