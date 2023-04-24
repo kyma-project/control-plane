dd7025ccd4f94afa19122e5acbd0287c119fb3dd
Miskiewicz <piotr.miskiewicz@sap.com>
2023-04-24 15:37:09 +0200

    fml

--- tests/e2e/provisioning/test/test_suite.go
+++ tests/e2e/provisioning/test/test_suite.go
@@ -18,6 +18,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/vrischmann/envconfig"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
