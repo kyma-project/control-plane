a22cd007be935bb6c2e310258de4e338c8f157bc
Miskiewicz <piotr.miskiewicz@sap.com>
2023-04-24 15:16:27 +0200

    wip

--- tests/e2e/provisioning/test/test_suite.go
+++ tests/e2e/provisioning/test/test_suite.go
@@ -17,6 +17,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/vrischmann/envconfig"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
