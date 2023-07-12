a331da8a4e2acc42c990b3c88a5e48dcbae39ddb
Piotr Miśkiewicz <piotr.miskiewicz@sap.com>
2023-07-12 12:48:06 +0200

    Fix failing cronjobs (#2917)

--- utils/kyma-environments-cleanup-job/apply.sh
+++ utils/kyma-environments-cleanup-job/apply.sh
@@ -10,18 +10,13 @@ deploymentName=kcp-kyma-environment-broker
 namespace=kcp-system
 kebContainerName=kyma-environment-broker
 cloudsqlProxyContainerName=cloudsql-proxy
-brokerUrlEnv=APP_BROKER_URL
 host=kyma-env-broker
 
-SCRIPT_BROKER_URL=$(kubectl get deployment $deploymentName -n $namespace -o jsonpath=\
-"{.spec.template.spec.containers[?(@.name==\"$kebContainerName\")].env[?(@.name==\"$brokerUrlEnv\")]}" \
-| jq -r '.value')
 SCRIPT_DOMAIN=${SCRIPT_BROKER_URL#"$host."}
 
 SCRIPT_CLOUDSQL_PROXY_COMMAND=$(kubectl get deployment $deploymentName -n $namespace -o jsonpath=\
 "{.spec.template.spec.containers[?(@.name==\"$cloudsqlProxyContainerName\")].command}")
 
-export SCRIPT_BROKER_URL
 export SCRIPT_DOMAIN
 export SCRIPT_CLOUDSQL_PROXY_COMMAND
 
