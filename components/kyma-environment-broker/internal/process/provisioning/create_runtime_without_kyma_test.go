285424634db61de74259df99c7637e96b15f5d10
Miskiewicz <piotr.miskiewicz@sap.com>
2023-08-29 13:17:16 +0200

    wip

--- components/kyma-environment-broker/internal/process/provisioning/create_runtime_without_kyma_test.go
+++ components/kyma-environment-broker/internal/process/provisioning/create_runtime_without_kyma_test.go
@@ -1,200 +1,200 @@
 package provisioning
 
 import (
-	"fmt"
-	"reflect"
-	"testing"
+    "fmt"
+    "reflect"
+    "testing"
 
-	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/broker"
-	provisionerAutomock "github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/provisioner/automock"
-	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/ptr"
-	"github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/storage"
-	"github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
-	"github.com/pivotal-cf/brokerapi/v8/domain"
-	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
+    "github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/broker"
+    provisionerAutomock "github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/provisioner/automock"
+    "github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/ptr"
+    "github.com/kyma-project/control-plane/components/kyma-environment-broker/internal/storage"
+    "github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
+    "github.com/pivotal-cf/brokerapi/v8/domain"
+    "github.com/sirupsen/logrus"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/mock"
 )
 
 func TestCreateRuntimeWithoutKyma_Run(t *testing.T) {
-	// given
-	log := logrus.New()
-	memoryStorage := storage.NewMemoryStorage()
+    // given
+    log := logrus.New()
+    memoryStorage := storage.NewMemoryStorage()
 
-	operation := fixOperationCreateRuntime(t, broker.GCPPlanID, "europe-west3")
-	operation.ShootDomain = "kyma.org"
-	err := memoryStorage.Operations().InsertOperation(operation)
-	assert.NoError(t, err)
+    operation := fixOperationCreateRuntime(t, broker.GCPPlanID, "europe-west3")
+    operation.ShootDomain = "kyma.org"
+    err := memoryStorage.Operations().InsertOperation(operation)
+    assert.NoError(t, err)
 
-	err = memoryStorage.Instances().Insert(fixInstance())
-	assert.NoError(t, err)
+    err = memoryStorage.Instances().Insert(fixInstance())
+    assert.NoError(t, err)
 
-	disabled := false
-	provisionerInput := fixProvisionerInput(disabled, false)
+    disabled := false
+    provisionerInput := fixProvisionerInput(disabled, false)
 
-	provisionerClient := &provisionerAutomock.Client{}
-	provisionerClient.On("ProvisionRuntime", globalAccountID, subAccountID, mock.MatchedBy(
-		func(input gqlschema.ProvisionRuntimeInput) bool {
-			return reflect.DeepEqual(input.RuntimeInput.Labels, provisionerInput.RuntimeInput.Labels) &&
-				input.KymaConfig == nil && reflect.DeepEqual(input.ClusterConfig, provisionerInput.ClusterConfig)
-		},
-	)).Return(gqlschema.OperationStatus{
-		ID:        ptr.String(provisionerOperationID),
-		Operation: "",
-		State:     "",
-		Message:   nil,
-		RuntimeID: ptr.String(runtimeID),
-	}, nil)
+    provisionerClient := &provisionerAutomock.Client{}
+    provisionerClient.On("ProvisionRuntime", globalAccountID, subAccountID, mock.MatchedBy(
+        func(input gqlschema.ProvisionRuntimeInput) bool {
+            return reflect.DeepEqual(input.RuntimeInput.Labels, provisionerInput.RuntimeInput.Labels) &&
+                input.KymaConfig == nil && reflect.DeepEqual(input.ClusterConfig, provisionerInput.ClusterConfig)
+        },
+    )).Return(gqlschema.OperationStatus{
+        ID:        ptr.String(provisionerOperationID),
+        Operation: "",
+        State:     "",
+        Message:   nil,
+        RuntimeID: ptr.String(runtimeID),
+    }, nil)
 
-	step := NewCreateRuntimeWithoutKymaStep(memoryStorage.Operations(), memoryStorage.RuntimeStates(), memoryStorage.Instances(), provisionerClient)
+    step := NewCreateRuntimeWithoutKymaStep(memoryStorage.Operations(), memoryStorage.RuntimeStates(), memoryStorage.Instances(), provisionerClient)
 
-	// when
-	entry := log.WithFields(logrus.Fields{"step": "TEST"})
-	operation, repeat, err := step.Run(operation, entry)
+    // when
+    entry := log.WithFields(logrus.Fields{"step": "TEST"})
+    operation, repeat, err := step.Run(operation, entry)
 
-	// then
-	assert.NoError(t, err)
-	assert.Zero(t, repeat)
-	assert.Equal(t, provisionerOperationID, operation.ProvisionerOperationID)
+    // then
+    assert.NoError(t, err)
+    assert.Zero(t, repeat)
+    assert.Equal(t, provisionerOperationID, operation.ProvisionerOperationID)
 
-	instance, err := memoryStorage.Instances().GetByID(operation.InstanceID)
-	assert.NoError(t, err)
-	assert.Equal(t, instance.RuntimeID, runtimeID)
+    instance, err := memoryStorage.Instances().GetByID(operation.InstanceID)
+    assert.NoError(t, err)
+    assert.Equal(t, instance.RuntimeID, runtimeID)
 }
 
 func TestCreateRuntimeWithoutKyma_RunWithEuAccess(t *testing.T) {
-	// given
-	log := logrus.New()
-	memoryStorage := storage.NewMemoryStorage()
+    // given
+    log := logrus.New()
+    memoryStorage := storage.NewMemoryStorage()
 
-	operation := fixOperationCreateRuntimeWithPlatformRegion(t, broker.GCPPlanID, "europe-west3", "cf-ch20")
-	operation.ShootDomain = "kyma.org"
-	err := memoryStorage.Operations().InsertOperation(operation)
-	assert.NoError(t, err)
+    operation := fixOperationCreateRuntimeWithPlatformRegion(t, broker.GCPPlanID, "europe-west3", "cf-ch20")
+    operation.ShootDomain = "kyma.org"
+    err := memoryStorage.Operations().InsertOperation(operation)
+    assert.NoError(t, err)
 
-	err = memoryStorage.Instances().Insert(fixInstance())
-	assert.NoError(t, err)
+    err = memoryStorage.Instances().Insert(fixInstance())
+    assert.NoError(t, err)
 
-	disabled := false
-	provisionerInput := fixProvisionerInput(disabled, true)
+    disabled := false
+    provisionerInput := fixProvisionerInput(disabled, true)
 
-	provisionerClient := &provisionerAutomock.Client{}
-	provisionerClient.On("ProvisionRuntime", globalAccountID, subAccountID, mock.MatchedBy(
-		func(input gqlschema.ProvisionRuntimeInput) bool {
-			return reflect.DeepEqual(input.RuntimeInput.Labels, provisionerInput.RuntimeInput.Labels) &&
-				input.KymaConfig == nil && reflect.DeepEqual(input.ClusterConfig, provisionerInput.ClusterConfig)
-		},
-	)).Return(gqlschema.OperationStatus{
-		ID:        ptr.String(provisionerOperationID),
-		Operation: "",
-		State:     "",
-		Message:   nil,
-		RuntimeID: ptr.String(runtimeID),
-	}, nil)
+    provisionerClient := &provisionerAutomock.Client{}
+    provisionerClient.On("ProvisionRuntime", globalAccountID, subAccountID, mock.MatchedBy(
+        func(input gqlschema.ProvisionRuntimeInput) bool {
+            return reflect.DeepEqual(input.RuntimeInput.Labels, provisionerInput.RuntimeInput.Labels) &&
+                input.KymaConfig == nil && reflect.DeepEqual(input.ClusterConfig, provisionerInput.ClusterConfig)
+        },
+    )).Return(gqlschema.OperationStatus{
+        ID:        ptr.String(provisionerOperationID),
+        Operation: "",
+        State:     "",
+        Message:   nil,
+        RuntimeID: ptr.String(runtimeID),
+    }, nil)
 
-	step := NewCreateRuntimeWithoutKymaStep(memoryStorage.Operations(), memoryStorage.RuntimeStates(), memoryStorage.Instances(), provisionerClient)
+    step := NewCreateRuntimeWithoutKymaStep(memoryStorage.Operations(), memoryStorage.RuntimeStates(), memoryStorage.Instances(), provisionerClient)
 
-	// when
-	entry := log.WithFields(logrus.Fields{"step": "TEST"})
-	operation, repeat, err := step.Run(operation, entry)
+    // when
+    entry := log.WithFields(logrus.Fields{"step": "TEST"})
+    operation, repeat, err := step.Run(operation, entry)
 
-	// then
-	assert.NoError(t, err)
-	assert.Zero(t, repeat)
-	assert.Equal(t, provisionerOperationID, operation.ProvisionerOperationID)
+    // then
+    assert.NoError(t, err)
+    assert.Zero(t, repeat)
+    assert.Equal(t, provisionerOperationID, operation.ProvisionerOperationID)
 
-	instance, err := memoryStorage.Instances().GetByID(operation.InstanceID)
-	assert.NoError(t, err)
-	assert.Equal(t, instance.RuntimeID, runtimeID)
+    instance, err := memoryStorage.Instances().GetByID(operation.InstanceID)
+    assert.NoError(t, err)
+    assert.Equal(t, instance.RuntimeID, runtimeID)
 }
 
 func fixProvisionerInput(disabled bool, euAccess bool) gqlschema.ProvisionRuntimeInput {
-	administrator := ""
-	return gqlschema.ProvisionRuntimeInput{
-		RuntimeInput: &gqlschema.RuntimeInput{
-			Name:        "dummy",
-			Description: nil,
-			Labels: gqlschema.Labels{
-				"broker_instance_id":   instanceID,
-				"global_subaccount_id": subAccountID,
-				"operator_grafanaUrl":  "https://grafana.kyma.org",
-			},
-		},
-		ClusterConfig: &gqlschema.ClusterConfigInput{
-			GardenerConfig: &gqlschema.GardenerConfigInput{
-				Name:                                shootName,
-				KubernetesVersion:                   k8sVersion,
-				DiskType:                            ptr.String("pd-standard"),
-				VolumeSizeGb:                        ptr.Integer(50),
-				MachineType:                         "n2-standard-4",
-				Region:                              "europe-west3",
-				Provider:                            "gcp",
-				Purpose:                             &shootPurpose,
-				LicenceType:                         nil,
-				WorkerCidr:                          "10.250.0.0/19",
-				AutoScalerMin:                       3,
-				AutoScalerMax:                       20,
-				MaxSurge:                            3,
-				MaxUnavailable:                      0,
-				TargetSecret:                        "",
-				ControlPlaneFailureTolerance:        ptr.String("zone"),
-				EnableKubernetesVersionAutoUpdate:   ptr.Bool(autoUpdateKubernetesVersion),
-				EnableMachineImageVersionAutoUpdate: ptr.Bool(autoUpdateMachineImageVersion),
-				ProviderSpecificConfig: &gqlschema.ProviderSpecificInput{
-					GcpConfig: &gqlschema.GCPProviderConfigInput{
-						Zones: []string{"europe-west3-b", "europe-west3-c"},
-					},
-				},
-				Seed: nil,
-				OidcConfig: &gqlschema.OIDCConfigInput{
-					ClientID:       "9bd05ed7-a930-44e6-8c79-e6defeb7dec9",
-					GroupsClaim:    "groups",
-					IssuerURL:      "https://kymatest.accounts400.ondemand.com",
-					SigningAlgs:    []string{"RS256"},
-					UsernameClaim:  "sub",
-					UsernamePrefix: "-",
-				},
-				DNSConfig: &gqlschema.DNSConfigInput{
-					Domain: "kyma.org",
-					Providers: []*gqlschema.DNSProviderInput{
-						&gqlschema.DNSProviderInput{
-							DomainsInclude: []string{"devtest.kyma.ondemand.com"},
-							Primary:        true,
-							SecretName:     "aws_dns_domain_secrets_test_intest",
-							Type:           "route53_type_test",
-						},
-					},
-				},
-				ShootNetworkingFilterDisabled: &disabled,
-				EuAccess:                      &euAccess,
-			},
-			Administrators: []string{administrator},
-		},
-		KymaConfig: nil,
-	}
+    administrator := ""
+    return gqlschema.ProvisionRuntimeInput{
+        RuntimeInput: &gqlschema.RuntimeInput{
+            Name:        "dummy",
+            Description: nil,
+            Labels: gqlschema.Labels{
+                "broker_instance_id":   instanceID,
+                "global_subaccount_id": subAccountID,
+                "operator_grafanaUrl":  "https://grafana.kyma.org",
+            },
+        },
+        ClusterConfig: &gqlschema.ClusterConfigInput{
+            GardenerConfig: &gqlschema.GardenerConfigInput{
+                Name:                                shootName,
+                KubernetesVersion:                   k8sVersion,
+                DiskType:                            ptr.String("pd-standard"),
+                VolumeSizeGb:                        ptr.Integer(50),
+                MachineType:                         "n2-standard-4",
+                Region:                              "europe-west3",
+                Provider:                            "gcp",
+                Purpose:                             &shootPurpose,
+                LicenceType:                         nil,
+                WorkerCidr:                          "10.250.0.0/22",
+                AutoScalerMin:                       3,
+                AutoScalerMax:                       20,
+                MaxSurge:                            3,
+                MaxUnavailable:                      0,
+                TargetSecret:                        "",
+                ControlPlaneFailureTolerance:        ptr.String("zone"),
+                EnableKubernetesVersionAutoUpdate:   ptr.Bool(autoUpdateKubernetesVersion),
+                EnableMachineImageVersionAutoUpdate: ptr.Bool(autoUpdateMachineImageVersion),
+                ProviderSpecificConfig: &gqlschema.ProviderSpecificInput{
+                    GcpConfig: &gqlschema.GCPProviderConfigInput{
+                        Zones: []string{"europe-west3-b", "europe-west3-c"},
+                    },
+                },
+                Seed: nil,
+                OidcConfig: &gqlschema.OIDCConfigInput{
+                    ClientID:       "9bd05ed7-a930-44e6-8c79-e6defeb7dec9",
+                    GroupsClaim:    "groups",
+                    IssuerURL:      "https://kymatest.accounts400.ondemand.com",
+                    SigningAlgs:    []string{"RS256"},
+                    UsernameClaim:  "sub",
+                    UsernamePrefix: "-",
+                },
+                DNSConfig: &gqlschema.DNSConfigInput{
+                    Domain: "kyma.org",
+                    Providers: []*gqlschema.DNSProviderInput{
+                        &gqlschema.DNSProviderInput{
+                            DomainsInclude: []string{"devtest.kyma.ondemand.com"},
+                            Primary:        true,
+                            SecretName:     "aws_dns_domain_secrets_test_intest",
+                            Type:           "route53_type_test",
+                        },
+                    },
+                },
+                ShootNetworkingFilterDisabled: &disabled,
+                EuAccess:                      &euAccess,
+            },
+            Administrators: []string{administrator},
+        },
+        KymaConfig: nil,
+    }
 }
 
 func TestCreateRuntimeWithoutKymaStep_RunWithBadRequestError(t *testing.T) {
-	// given
-	log := logrus.New()
-	memoryStorage := storage.NewMemoryStorage()
+    // given
+    log := logrus.New()
+    memoryStorage := storage.NewMemoryStorage()
 
-	operation := fixOperationCreateRuntime(t, broker.AzurePlanID, "westeurope")
-	err := memoryStorage.Operations().InsertOperation(operation)
-	assert.NoError(t, err)
+    operation := fixOperationCreateRuntime(t, broker.AzurePlanID, "westeurope")
+    err := memoryStorage.Operations().InsertOperation(operation)
+    assert.NoError(t, err)
 
-	err = memoryStorage.Instances().Insert(fixInstance())
-	assert.NoError(t, err)
+    err = memoryStorage.Instances().Insert(fixInstance())
+    assert.NoError(t, err)
 
-	provisionerClient := &provisionerAutomock.Client{}
-	provisionerClient.On("ProvisionRuntime", globalAccountID, subAccountID, mock.Anything).Return(gqlschema.OperationStatus{}, fmt.Errorf("some permanent error"))
+    provisionerClient := &provisionerAutomock.Client{}
+    provisionerClient.On("ProvisionRuntime", globalAccountID, subAccountID, mock.Anything).Return(gqlschema.OperationStatus{}, fmt.Errorf("some permanent error"))
 
-	step := NewCreateRuntimeWithoutKymaStep(memoryStorage.Operations(), memoryStorage.RuntimeStates(), memoryStorage.Instances(), provisionerClient)
+    step := NewCreateRuntimeWithoutKymaStep(memoryStorage.Operations(), memoryStorage.RuntimeStates(), memoryStorage.Instances(), provisionerClient)
 
-	// when
-	entry := log.WithFields(logrus.Fields{"step": "TEST"})
-	operation, _, err = step.Run(operation, entry)
+    // when
+    entry := log.WithFields(logrus.Fields{"step": "TEST"})
+    operation, _, err = step.Run(operation, entry)
 
-	// then
-	assert.Equal(t, domain.Failed, operation.State)
+    // then
+    assert.Equal(t, domain.Failed, operation.State)
 }
