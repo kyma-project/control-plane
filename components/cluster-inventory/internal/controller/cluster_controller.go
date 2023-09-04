7373e19ae6f1066113c14d83a6e2768e5c2e3968
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2023-09-04 15:53:17 +0200

    Rotation test refactored

--- components/cluster-inventory/internal/controller/cluster_controller.go
+++ components/cluster-inventory/internal/controller/cluster_controller.go
@@ -2,9 +2,10 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
@@ -67,6 +68,7 @@ func NewClusterInventoryController(mgr ctrl.Manager, kubeconfigProvider Kubeconf
 func (r *ClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
+	r.log.Info("Starting reconciliation")
 	var cluster clusterinventoryv1beta1.Cluster
 
 	err := r.Client.Get(ctx, req.NamespacedName, &cluster)
@@ -89,23 +91,44 @@ func (r *ClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 }
 
 func (r *ClusterReconciler) rotateOrCreateSecret(cluster clusterinventoryv1beta1.Cluster) error {
-	var secret corev1.Secret
-	key := types.NamespacedName{Name: cluster.Name, Namespace: r.SecretNamespace}
-
-	if err := r.Client.Get(context.Background(), key, &secret); err != nil {
-		if k8serrors.IsNotFound(err) {
-			secret, err := r.createSecret(cluster)
-			if err != nil {
-				return err
-			}
-
-			return r.Client.Create(context.Background(), &secret)
-		}
-
+	r.log.Info("Looking for secret")
+	secret, err := r.getSecret(cluster.Labels["kyma-project.io/shoot-name"])
+	if err != nil {
 		return err
 	}
 
-	return r.rotateSecret(&secret)
+	if secret == nil {
+		r.log.Info("Secret not found, and will be created")
+		return r.createSecret(cluster)
+	}
+
+	r.log.Info("Secret found, and will be rotated if needed")
+	return r.rotateSecret(secret.Name, secret.Annotations)
+}
+
+func (r *ClusterReconciler) getSecret(shootName string) (*corev1.Secret, error) {
+	var secretList corev1.SecretList
+
+	selector := client.MatchingLabels(map[string]string{
+		"kyma-project.io/shoot-name": shootName,
+	})
+
+	err := r.Client.List(context.Background(), &secretList, selector)
+	if err != nil {
+		return nil, err
+	}
+
+	size := len(secretList.Items)
+
+	if size == 0 {
+		return nil, nil
+	}
+
+	if size > 1 {
+		return nil, errors.New(fmt.Sprintf("unexpected numer of secrets found for shoot `%s`", shootName))
+	}
+
+	return &secretList.Items[0], nil
 }
 
 const (
@@ -120,20 +143,21 @@ const (
 	kymaNameLabel        = "operator.kyma-project.io/kyma-name"
 )
 
-func (r *ClusterReconciler) createSecret(cluster clusterinventoryv1beta1.Cluster) (corev1.Secret, error) {
-	clusterInventoryLabels := cluster.Labels
+func (r *ClusterReconciler) createSecret(cluster clusterinventoryv1beta1.Cluster) error {
+	secret, err := r.newSecret(cluster)
+	if err != nil {
+		return err
+	}
 
+	return r.Client.Create(context.Background(), &secret)
+}
+
+func (r *ClusterReconciler) newSecret(cluster clusterinventoryv1beta1.Cluster) (corev1.Secret, error) {
 	labels := map[string]string{}
 
-	labels[instanceIDLabel] = clusterInventoryLabels[instanceIDLabel]
-	labels[runtimeIDLabel] = clusterInventoryLabels[runtimeIDLabel]
-	labels[planIDLabel] = clusterInventoryLabels[planIDLabel]
-	labels[planNameLabel] = clusterInventoryLabels[planNameLabel]
-	labels[globalAccountIDLabel] = clusterInventoryLabels[globalAccountIDLabel]
-	labels[subaccountIDLabel] = clusterInventoryLabels[subaccountIDLabel]
-	labels[shootNameLabel] = clusterInventoryLabels[shootNameLabel]
-	labels[regionLabel] = clusterInventoryLabels[regionLabel]
-	labels[kymaNameLabel] = clusterInventoryLabels[kymaNameLabel]
+	for key, val := range cluster.Labels {
+		labels[key] = val
+	}
 	labels["operator.kyma-project.io/managed-by"] = "lifecycle-manager"
 
 	kubeconfig, err := r.KubeconfigProvider.Fetch(labels[shootNameLabel])
@@ -152,19 +176,40 @@ func (r *ClusterReconciler) createSecret(cluster clusterinventoryv1beta1.Cluster
 	}, nil
 }
 
-func (r *ClusterReconciler) rotateSecret(secret *corev1.Secret) error {
-	_, forceKubeconfigRotation := secret.Annotations[forceRotationAnnotation]
+func (r *ClusterReconciler) rotateSecret(secretName string, annotations map[string]string) error {
+	_, forceKubeconfigRotation := annotations[forceRotationAnnotation]
 
 	if forceKubeconfigRotation {
-		kubeconfig, err := r.KubeconfigProvider.Fetch(secret.Labels[shootNameLabel])
+		r.log.Info("Secret has operator.kyma-project.io/force-kubeconfig-rotation annotation and will be rotated")
+		var secret corev1.Secret
+		key := types.NamespacedName{Name: secretName, Namespace: r.SecretNamespace}
+
+		err := r.Client.Get(context.Background(), key, &secret)
 		if err != nil {
+			r.log.Error(err, "failed to get secret")
 			return err
 		}
+
+		r.log.Info("Fetching dynamic kubeconfig")
+		kubeconfig, err := r.KubeconfigProvider.Fetch(secret.Labels[shootNameLabel])
+		if err != nil {
+			r.log.Error(err, "failed to fetch dynamic kubeconfig")
+			return err
+		}
+
+		r.log.Info("Updating secret with new data")
 		delete(secret.Annotations, forceRotationAnnotation)
 		secret.Annotations[lastKubeconfigSyncAnnotation] = time.Now().UTC().String()
+
 		secret.StringData = map[string]string{"config": kubeconfig}
 
-		return r.Client.Update(context.Background(), secret)
+		r.log.Info(fmt.Sprintf("%v", secret.Name))
+		err = r.Client.Update(context.Background(), &secret)
+		if err != nil {
+			r.log.Error(err, "failed to update secret")
+		}
+
+		return err
 	}
 
 	return nil
--- components/cluster-inventory/internal/controller/cluster_controller_test.go
+++ components/cluster-inventory/internal/controller/cluster_controller_test.go
@@ -12,11 +12,6 @@ import (
 	"time"
 )
 
-const (
-	namespace  = "test"
-	kubeconfig = "kubeconfig"
-)
-
 var _ = Describe("Cluster Inventory controller", func() {
 	Context("Secret with kubeconfig doesn't exist", func() {
 		kymaName := "kymaname1"
@@ -25,8 +20,7 @@ var _ = Describe("Cluster Inventory controller", func() {
 		It("Create secret", func() {
 			By("Create Cluster CR")
 			// TODO: Cluster Inventory CR should have Cluster scope
-			clusterCR := fixClusterInventoryCR(kymaName, kymaName, "shootName1", namespace)
-
+			clusterCR := fixClusterInventoryCR(kymaName, namespace, kymaName, "shootName1")
 			Expect(k8sClient.Create(context.Background(), &clusterCR)).To(Succeed())
 
 			By("Wait for secret creation")
@@ -39,49 +33,53 @@ var _ = Describe("Cluster Inventory controller", func() {
 
 			err := k8sClient.Get(context.Background(), key, &kubeconfigSecret)
 			Expect(err).To(BeNil())
-			expectedSecret := fixSecret(kymaName, kymaName, "shootName1", namespace)
+			expectedSecret := fixNewSecret(kymaName, namespace, kymaName, "shootName1", "kubeconfig1")
 			Expect(kubeconfigSecret.Labels).To(Equal(expectedSecret.Labels))
 			Expect(kubeconfigSecret.Data).To(Equal(expectedSecret.Data))
 			Expect(kubeconfigSecret.Annotations[lastKubeconfigSyncAnnotation]).To(Not(BeEmpty()))
 		})
 	})
 
-	type AnnotationsPredicate func(secret corev1.Secret) bool
-
 	Context("Secret with kubeconfig exists", func() {
-		kymaName := "kymaname2"
 		namespace := "default"
 
-		DescribeTable("Rotate kubeconfig", func(shootName string, secret corev1.Secret, predicate AnnotationsPredicate) {
+		DescribeTable("Create secret when needed", func(clusterCR v1beta1.Cluster, secret corev1.Secret, previousTimestamp, expectedKubeconfig string) {
 			By("Create kubeconfig secret")
 			Expect(k8sClient.Create(context.Background(), &secret)).To(Succeed())
 
 			By("Create Cluster CR")
-			clusterCR := fixClusterInventoryCR(kymaName, kymaName, "shootName2", namespace)
-
 			Expect(k8sClient.Create(context.Background(), &clusterCR)).To(Succeed())
 
 			var kubeconfigSecret corev1.Secret
-			key := types.NamespacedName{Name: kymaName, Namespace: namespace}
+			key := types.NamespacedName{Name: secret.Name, Namespace: namespace}
 
 			Eventually(func() bool {
+
 				err := k8sClient.Get(context.Background(), key, &kubeconfigSecret)
 				if err != nil {
 					return false
 				}
 
-				return predicate(kubeconfigSecret)
+				_, forceAnnotationFound := kubeconfigSecret.Annotations[forceRotationAnnotation]
+				timestampAnnotation := kubeconfigSecret.Annotations[lastKubeconfigSyncAnnotation]
+
+				return !forceAnnotationFound && timestampAnnotation != previousTimestamp
 			}, time.Second*30, time.Second*3).Should(BeTrue())
 
 			err := k8sClient.Get(context.Background(), key, &kubeconfigSecret)
 			Expect(err).To(BeNil())
-			expectedSecret := fixSecret(kymaName, kymaName, "shootName2", namespace)
-			Expect(kubeconfigSecret.Labels).To(Equal(expectedSecret.Labels))
-			Expect(kubeconfigSecret.Data).To(Equal(expectedSecret.Data))
-			Expect(kubeconfigSecret.Annotations[lastKubeconfigSyncAnnotation]).To(Not(BeEmpty()))
+			Expect(string(kubeconfigSecret.Data["config"])).To(Equal(expectedKubeconfig))
 		},
-			Entry("Rotate static kubeconfig", "shootName2", forceRotationAnnotation),
-			Entry("Rotate dynamic kubeconfig", "shootName3", forceRotationAnnotation),
+			Entry("Rotate static kubeconfig",
+				fixClusterInventoryCR("cluster2", "default", "kymaName2", "shootName2"),
+				fixSecretWithForceRotation("static-kubeconfig-secret", namespace, "kymaName2", "shootName2", "kubeconfig2"),
+				"",
+				"kubeconfig2"),
+			//Entry("Rotate dynamic kubeconfig",
+			//	fixClusterInventoryCR("cluster3", "default", "kymaName3", "shootName3"),
+			//	fixNewSecret("dynamic-kubeconfig-secret", namespace, "kymaName3", "shootName3", "kubeconfig3"),
+			//	"oooo",
+			//	"dynamic kubeconfig 2"),
 		)
 
 		Describe("Skip rotation", func() {
@@ -94,47 +92,74 @@ var _ = Describe("Cluster Inventory controller", func() {
 	})
 })
 
-type SecretBuilder struct {
-	name        string
-	namespace   string
-	labels      map[string]string
-	annotations map[string]string
-	data        string
+type TestSecret struct {
+	secret corev1.Secret
 }
 
-func NewSecretBuilder(name, namespace, data string) *SecretBuilder {
-	return &SecretBuilder{
-		name:      name,
-		namespace: namespace,
-		data:      data,
-	}
-}
-
-func (sb *SecretBuilder) WithLabels(labels map[string]string) *SecretBuilder {
-	sb.labels = labels
-
-	return sb
-}
-
-func (sb *SecretBuilder) WithAnnotations(annotations map[string]string) *SecretBuilder {
-	sb.annotations = annotations
-
-	return sb
-}
-
-func (sb SecretBuilder) Build() corev1.Secret {
-	return corev1.Secret{
-		ObjectMeta: v12.ObjectMeta{
-			Name:        sb.name,
-			Namespace:   sb.namespace,
-			Labels:      sb.labels,
-			Annotations: sb.annotations,
+func newTestSecret(name, namespace string) *TestSecret {
+	return &TestSecret{
+		secret: corev1.Secret{
+			ObjectMeta: v12.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
 		},
 	}
-
 }
 
-func fixClusterInventoryCR(name, kymaName, shootName, namespace string) v1beta1.Cluster {
+func (sb *TestSecret) WithLabels(labels map[string]string) *TestSecret {
+	sb.secret.Labels = labels
+
+	return sb
+}
+
+func (sb *TestSecret) WithAnnotations(annotations map[string]string) *TestSecret {
+	sb.secret.Annotations = annotations
+
+	return sb
+}
+
+func (sb *TestSecret) WithData(data string) *TestSecret {
+	sb.secret.Data = (map[string][]byte{"config": []byte(data)})
+
+	return sb
+}
+
+func (sb *TestSecret) ToSecret() corev1.Secret {
+	return sb.secret
+}
+
+type TestClusterInventoryCR struct {
+	cluster v1beta1.Cluster
+}
+
+func newTestClusterInventoryCR(name, namespace string) *TestClusterInventoryCR {
+	return &TestClusterInventoryCR{
+		cluster: v1beta1.Cluster{
+			ObjectMeta: v12.ObjectMeta{
+				Name:      name,
+				Namespace: namespace,
+			},
+		},
+	}
+}
+
+func (sb *TestClusterInventoryCR) WithLabels(labels map[string]string) *TestClusterInventoryCR {
+	sb.cluster.Labels = labels
+
+	return sb
+}
+
+func (sb *TestClusterInventoryCR) ToCluster() v1beta1.Cluster {
+	return sb.cluster
+}
+
+func fixClusterInventoryCR(name, namespace, kymaName, shootName string) v1beta1.Cluster {
+	return newTestClusterInventoryCR(name, namespace).
+		WithLabels(fixClusterInventoryLabels(kymaName, shootName)).ToCluster()
+}
+
+func fixClusterInventoryLabels(kymaName, shootName string) map[string]string {
 
 	labels := map[string]string{}
 
@@ -148,16 +173,10 @@ func fixClusterInventoryCR(name, kymaName, shootName, namespace string) v1beta1.
 	labels["kyma-project.io/region"] = "region"
 	labels["operator.kyma-project.io/kyma-name"] = kymaName
 
-	return v1beta1.Cluster{
-		ObjectMeta: v12.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-			Labels:    labels,
-		},
-	}
+	return labels
 }
 
-func fixSecretLabels(name, kymaName, shootName, namespace string) map[string]string {
+func fixSecretLabels(kymaName, shootName string) map[string]string {
 	labels := map[string]string{}
 
 	labels["kyma-project.io/instance-id"] = "instanceID"
@@ -174,64 +193,38 @@ func fixSecretLabels(name, kymaName, shootName, namespace string) map[string]str
 	return labels
 }
 
-func fixSecret(name, kymaName, shootName, namespace string) corev1.Secret {
-	labels := map[string]string{}
-
-	labels["kyma-project.io/instance-id"] = "instanceID"
-	labels["kyma-project.io/runtime-id"] = "runtimeID"
-	labels["kyma-project.io/broker-plan-id"] = "planID"
-	labels["kyma-project.io/broker-plan-name"] = "planName"
-	labels["kyma-project.io/global-account-id"] = "globalAccountID"
-	labels["kyma-project.io/subaccount-id"] = "subAccountID"
-	labels["kyma-project.io/shoot-name"] = shootName
-	labels["kyma-project.io/region"] = "region"
-	labels["operator.kyma-project.io/kyma-name"] = kymaName
-	labels["operator.kyma-project.io/managed-by"] = "lifecycle-manager"
-
-	return corev1.Secret{
-		ObjectMeta: v12.ObjectMeta{
-			Name:      name,
-			Namespace: namespace,
-			Labels:    labels,
-		},
-		Data: map[string][]byte{"config": []byte(kubeconfig)},
-	}
+func fixDynamicKubeconfigAnnotations(date string) map[string]string {
+	return map[string]string{lastKubeconfigSyncAnnotation: date}
 }
 
-func fixSecretWithForceRotation(name, kymaName, shootName, namespace string, dynamic bool) corev1.Secret {
-	labels := map[string]string{}
+func fixForceKubeconfigAnnotations() map[string]string {
+	return map[string]string{forceRotationAnnotation: "true"}
+}
 
-	labels["kyma-project.io/instance-id"] = "instanceID"
-	labels["kyma-project.io/runtime-id"] = "runtimeID"
-	labels["kyma-project.io/broker-plan-id"] = "planID"
-	labels["kyma-project.io/broker-plan-name"] = "planName"
-	labels["kyma-project.io/global-account-id"] = "globalAccountID"
-	labels["kyma-project.io/subaccount-id"] = "subAccountID"
-	labels["kyma-project.io/shoot-name"] = shootName
-	labels["kyma-project.io/region"] = "region"
-	labels["operator.kyma-project.io/kyma-name"] = kymaName
-	labels["operator.kyma-project.io/managed-by"] = "lifecycle-manager"
+func fixNewSecret(name, namespace, kymaName, shootName, data string) corev1.Secret {
+	labels := fixSecretLabels(kymaName, shootName)
 
-	annotations := map[string]string{}
+	builder := newTestSecret(name, namespace)
+	return builder.WithLabels(labels).WithData(data).ToSecret()
+}
 
-	if dynamic {
-		annotations[lastKubeconfigSyncAnnotation] = "2013-05-01 23:00:00 +0000 UTC"
-	} else {
-		annotations[forceRotationAnnotation] = "true"
-	}
+func fixSecretWithForceRotation(name, namespace, kymaName, shootName, data string) corev1.Secret {
+	labels := fixSecretLabels(kymaName, shootName)
+	annotations := fixForceKubeconfigAnnotations()
 
-	return corev1.Secret{
-		ObjectMeta: v12.ObjectMeta{
-			Name:        name,
-			Namespace:   namespace,
-			Labels:      labels,
-			Annotations: annotations,
-		},
-		StringData: map[string]string{"config": "static kubeconfig"},
-	}
+	return newTestSecret(name, namespace).WithAnnotations(annotations).WithLabels(labels).WithData(data).ToSecret()
+}
+
+func fixSecretWithDynamicKubeconfig(name, namespace, kymaName, shootName string) corev1.Secret {
+	labels := fixSecretLabels(kymaName, shootName)
+	annotations := fixDynamicKubeconfigAnnotations("2022-11-10 23:00:00 +0000")
+
+	builder := newTestSecret(name, namespace)
+	return builder.WithAnnotations(annotations).WithLabels(labels).WithData("dynamic kubeconfig").ToSecret()
 }
 
 func setupKubeconfigProviderMock(kpMock *mocks.KubeconfigProvider) {
-	kpMock.On("Fetch", "shootName1").Return(kubeconfig, nil)
-	kpMock.On("Fetch", "shootName2").Return(kubeconfig, nil)
+	kpMock.On("Fetch", "shootName1").Return("kubeconfig1", nil)
+	kpMock.On("Fetch", "shootName2").Return("kubeconfig2", nil)
+	kpMock.On("Fetch", "shootName3").Return("kubeconfig3", nil)
 }
