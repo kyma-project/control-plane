4c7d0821277f6b5e12f9fde8c0487df332732a1d
Wojciech Wrzalik <74771103+ralikio@users.noreply.github.com>
2023-09-12 16:11:07 +0200

    KEB moved to kyma-project/kyma-environment-broker (#3077)
    * KEB moved to https://github.com/kyma-project/kyma-environment-broker
    
    * Docs Removal

--- components/kyma-environment-broker/internal/networking/cidr.go
+++
@@ -1,9 +0,0 @@
-package networking
-
-const (
-	DefaultNodesCIDR    = "10.250.0.0/22"
-	DefaultPodsCIDR     = "10.96.0.0/13"
-	DefaultServicesCIDR = "10.104.0.0/13"
-)
-
-var GardenerSeedCIDRs = []string{"10.243.128.0/17", "10.242.0.0/16", "10.243.0.0/17", "10.64.0.0/11", "10.254.0.0/16", "10.243.0.0/16"}
