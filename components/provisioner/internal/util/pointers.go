6aa743ae78525ba1542baccf3ed2341c76c65872
void404 <wojciech.nawa@sap.com>
2024-02-19 13:14:07 +0100

    Make pointer utils generic

--- components/provisioner/internal/util/pointers.go
+++ components/provisioner/internal/util/pointers.go
@@ -1,87 +1,30 @@
 package util
 
-import (
-	"k8s.io/apimachinery/pkg/util/intstr"
-)
-
-// BoolPtr returns pointer to given bool
-func BoolPtr(b bool) *bool {
-	return &b
+// PtrTo returns pointer to given value
+func PtrTo[T any](v T) *T {
+	return &v
 }
 
-// IntPtr returns pointer to given int
-func IntPtr(val int) *int {
-	return &val
-}
-
-// StringPtr returns pointer to given string
-func StringPtr(str string) *string {
-	return &str
-}
-
-// IntOrStringPtr returns pointer to given int or string
-func IntOrStringPtr(intOrStr intstr.IntOrString) *intstr.IntOrString {
-	return &intOrStr
-}
-
-// UnwrapInt returns int value from pointer
-func UnwrapInt(intPtr *int) int {
-	if intPtr == nil {
-		return 0
-	}
-	return *intPtr
-}
-
-// UnwrapStr returns string value from pointer
-func UnwrapStr(strPtr *string) string {
-	if strPtr == nil {
-		return ""
-	}
-	return *strPtr
-}
-
-// UnwrapBoolOrDefault returns bool value from pointer or if pointer is nil returns default value
-func UnwrapBoolOrDefault(ptr *bool, def bool) bool {
+// UnwrapOrZero returns value from pointer or zero value if pointer is nil
+func UnwrapOrZero[T any](ptr *T) T {
 	if ptr == nil {
-		return def
+		return *new(T)
 	}
+
 	return *ptr
 }
 
-// UnwrapIntOrDefault returns int value from pointer or if pointer is nil returns default value
-func UnwrapIntOrDefault(ptr *int, def int) int {
+// UnwrapOrDefault returns value from pointer or provided default value if pointer is nil
+func UnwrapOrDefault[T any](ptr *T, def T) T {
 	if ptr == nil {
 		return def
 	}
+
 	return *ptr
 }
 
-// UnwrapStrOrDefault returns string value from pointer or if pointer is nil returns default value
-func UnwrapStrOrDefault(ptr *string, def string) string {
-	if ptr == nil {
-		return def
-	}
-	return *ptr
-}
-
-// DefaultStrIfNil returns default string pointer if passed pointer is nil
-func DefaultStrIfNil(ptr *string, def *string) *string {
-	if ptr == nil {
-		return def
-	}
-	return ptr
-}
-
-// DefaultIntIfNil returns default int pointer if passed pointer is nil
-func DefaultIntIfNil(ptr *int, def *int) *int {
-	if ptr == nil {
-		return def
-	}
-	return ptr
-}
-
-// DefaultBoolIfNil returns default bool pointer if passed pointer is nil
-func DefaultBoolIfNil(ptr *bool, def *bool) *bool {
+// OkOrDefault returns pointer or provided default pointer if pointer is nil
+func OkOrDefault[T any](ptr *T, def *T) *T {
 	if ptr == nil {
 		return def
 	}
