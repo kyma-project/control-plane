7f6e063e2737f03fc2a7fd6de0e0cd16f049d52b
hamdymoussa <41269566+hamdymoussa@users.noreply.github.com>
2024-03-01 04:09:35 -0500

    Remove kcp-cli code (#3392)
    Remove kcp-cli code
    Related to https://github.tools.sap/kyma/backlog/issues/4842
    Kcp-cli code moved to https://github.tools.sap/kyma/kcp-cli

--- tools/cli/pkg/ers/fetcher/api.go
+++
@@ -1,8 +0,0 @@
-package fetcher
-
-import "github.com/kyma-project/control-plane/tools/cli/pkg/ers"
-
-type InstanceFetcher interface {
-	GetAllInstances() ([]ers.Instance, error)
-	GetInstanceById(id string) (*ers.Instance, error)
-}
